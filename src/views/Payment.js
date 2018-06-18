1d8fb01a57f98a43458e09da10222436f01f39cc
Leonardo Costa <leolima.str@gmail.com>
2018-06-17 23:50:42 -0300

    Construct payment form

--- src/views/Payment.js
+++ src/views/Payment.js
@@ -13,36 +13,117 @@ import { connect } from 'react-redux';
  */
 class Payment extends Component {
 
+
     constructor(props) {
         super(props);
-       
-    }
 
+    }
 
     render() {
 
         return (
-            <div>Payment</div>
+            <main>
+                <div className="container">
+                    <nav>
+                        <ul>
+                            <div>Sacola</div>
+                            <div>Pagamento</div>
+                            <div>Confirmação</div>
+                        </ul>
+                    </nav>
 
+                    <div>
+                        <h1>Cartão de Crédito</h1>
+                    </div>
+
+                    <div className="form-payment">
+
+                        <div class="form-group">
+                            <label htmlFor="card-number">Número do Cartão:</label>
+                            <input type="text" class="form-control"
+                             id="card-number" placeholder="____.____.____.____" />
+
+                        </div>
+
+                        <div class="form-group">
+                            <label htmlFor="card-name">Nome do Titular:</label>
+                            <input type="text" class="form-control" id="card-name" placeholder="Como no cartão" />
+
+                        </div>
+
+                        <div class="form-group">
+                            <label htmlFor="card-validate">Validade (mês/ano):</label>
+                            <input type="text" class="form-control" id="card-validate" placeholder="__/____" />
+
+                        </div>
+
+                        <div class="form-group">
+                            <label fohtmlForr="card-cvv">Cvv</label>
+                            <input type="text" class="form-control" id="card-cvv" placeholder="___" />
+
+                        </div>
+
+                    </div>
+
+                    <div className="resume">
+                        <div className="resume-specification">
+                            <label>Produtos</label>
+                            <div className="currency">
+                                {this.props.subTotal}
+                            </div>
+                        </div>
+                        <div className="resume-specification">
+                            <label>Frete</label>
+                            <div className="currency">
+                                {this.props.shippingTotal}
+                            </div>
+                        </div>
+                        <div className="resume-specification">
+                            <label>Desconto</label>
+                            <div className="currency">
+                                {this.props.discount}
+                            </div>
+                        </div>
+                        <div className="resume-specification">
+                            <label>Total</label>
+                            <div className="currency">
+                                {this.props.total}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="btn">Finalizar o pedido</div>
+                </div>
+            </main>
         )
     }
 
 }
 
 // Decorate the component with reducer attributes
-/*const mapStateToProps = state => {
+const mapStateToProps = state => {
 
-    //const usuarioLogado = state.CartReducer.data;
+    const discount = state.CartReducer.discount;
+    const id = state.CartReducer.id;
+    const items = state.CartReducer.items;
+    const shippingTotal = state.CartReducer.shippingTotal;
+    const subTotal = state.CartReducer.subTotal;
+    const total = state.CartReducer.total;
+    const loading_cart = state.CartReducer.loading_cart;
+
     return {
-        botao_atualiza_perfil,
-        carregando_perfil,
-        usuarioLogado,
-        perfil,
-        atualizando_perfil
+        discount,
+        id,
+        items,
+        subTotal,
+        shippingTotal,
+        total,
+        loading_cart
     }
-}*/
+}
+
 
 //use conector redux to decorate component with variables and methods
-export default connect(null, {
-   
+export default connect(mapStateToProps, {
+
 })(Payment);
