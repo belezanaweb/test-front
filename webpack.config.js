a6b644ead7d956bcc36f938d1688e671db4f97c3
Ricardo Han <ricardoshhan@gmail.com>
2018-07-18 07:19:20 -0300

    first commit, setting up config files and folder structure

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
