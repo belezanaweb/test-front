f020d458f1997761f20d608f4622abf0673deb3f
Cristiano Mesquita <cristianomesquita@Cristianos-MacBook-Air.local>
2018-07-27 21:26:32 -0300

    add: <ItemBox /> && styled it

---
+++ src/components/ItemBox/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const ItemBox = ({ children }) => <Container>{children}</Container>
+
+export default ItemBox
---
+++ src/components/ItemBox/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background: #fff;
+  height: 325px;
+  box-shadow: 1px 1px 5px 0 rgba(0, 0, 29, 0.22);
+  padding: 13px;
+  overflow-y: scroll;
+`
