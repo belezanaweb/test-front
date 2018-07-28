785e6a433f8bd676fa6daefbc083fca858ba80e7
Cristiano Mesquita <cristianomesquita@Cristianos-MacBook-Air.local>
2018-07-27 21:24:48 -0300

    update: page cart && styled it

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,23 +1,52 @@
 import React from 'react'
+import _ from 'lodash'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import { Creators as CartActions } from '../../store/ducks/cart'
 
+import { Container } from './styles'
+
+import ItemBox from '../../components/ItemBox'
+import ProductBox from '../../components/ProductBox'
+
 class Cart extends React.Component {
   state = {}
 
-  componentWillMount() {
+  componentDidMount() {
     const { fetchRequest } = this.props
     fetchRequest()
   }
 
   render() {
-    return <h1>Cart page</h1>
+    const { cart } = this.props
+    const {
+      data: { items }
+    } = cart
+
+    return (
+      <Container>
+        <h6 className="title">PRODUTOS</h6>
+        <ItemBox>
+          {items &&
+            items.length &&
+            items.map(item => (
+              <ProductBox
+                key={item.product.sku}
+                description={_.truncate(item.product.name, { length: 63 })}
+                thumbnail={item.product.imageObjects[0].thumbnail}
+                price={item.product.priceSpecification.price}
+              />
+            ))}
+        </ItemBox>
+      </Container>
+    )
   }
 }
 
-const mapStateToProps = ({ cart }) => ({ cart })
+const mapStateToProps = ({ cart }) => ({
+  cart
+})
 const mapDispatchToProps = dispatch => bindActionCreators(CartActions, dispatch)
 
 export default connect(
---
+++ src/pages/cart/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 0 10px 20px 10px;
+
+  .title {
+    height: 17px;
+    color: #999;
+    font-size: 14px;
+    font-weight: 700;
+    letter-spacing: NaNpx;
+    line-height: 17px;
+    margin: 0 21px 5px 10px;
+  }
+`
