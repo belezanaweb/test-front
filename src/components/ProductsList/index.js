d8b3efe6ef5592e1de517410a7a250ec8c173848
Gian Souza <giansouzafreitas@gmail.com>
2018-08-08 20:43:07 -0300

    style in Products List

--- src/components/ProductsList/index.js
+++ src/components/ProductsList/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 /**
  * Styles
  */
 import { SectionTitle } from '../../assets/global';
+import { Products, Product, ProductDetails, ProductName, ProductPrice } from './styles';
 
 const ProductsList = () => (
-  <div>
+  <Fragment>
     <SectionTitle>Produtos</SectionTitle>
-  </div>
+    <Products>
+      <Product>
+        <figure>
+          <img src="//placehold.it/65x65" alt="." />
+        </figure>
+        <ProductDetails>
+          <ProductName>L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium</ProductName>
+          <ProductPrice>R$ 150,00</ProductPrice>
+        </ProductDetails>
+      </Product>
+      <Product>
+        <figure>
+          <img src="//placehold.it/65x65" alt="." />
+        </figure>
+      </Product>
+      <Product>
+        <figure>
+          <img src="//placehold.it/65x65" alt="." />
+        </figure>
+      </Product>
+    </Products>
+  </Fragment>
 );
 
 export default ProductsList;
---
+++ src/components/ProductsList/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Products = styled.div`
+  box-shadow: 1px 1px 5px 0 rgba(0,0,29,0.22);
+  background-color: #FFF;
+  padding: 12px;
+`;
+
+export const Product = styled.div`
+  border: 1px solid #EEE;
+  padding: 14px 10px;
+  display: flex;
+  margin-bottom: 15px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+export const ProductDetails = styled.div`
+  color: #212122;
+`;
+
+export const ProductName = styled.h3`
+  font-size: 13px;
+  line-height: 16px;
+  font-weight: normal;
+  margin-bottom: 15px;
+`;
+
+export const ProductPrice = styled.h4`
+  font-size: 14px;
+  line-height: 17px;
+  font-weight: bold;
+`
