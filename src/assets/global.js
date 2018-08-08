33a0bff47265114b149af5b6c0a26eee091c2d37
Gian Souza <giansouzafreitas@gmail.com>
2018-08-08 20:15:22 -0300

    add Helvetica Neue Font

--- src/assets/global.js
+++ src/assets/global.js
@@ -1,9 +1,20 @@
 import { injectGlobal } from  'styled-components';
 
+/**
+ * Fonts
+ */
+import Helvetica from './fonts/helvetica.ttf';
+
 injectGlobal`
+  @font-face {
+    font-family: 'Helvetica';
+    src: url(${ Helvetica });
+  }
+
   * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: 'Helvetica', sans-serif;
   }
 `;
