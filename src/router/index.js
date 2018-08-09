53aca89bfea615cde2c0188d7263cbe92118167d
Marcos Borges <marcos.abb52@gmail.com>
2018-08-09 13:17:41 -0300

    Adding checkout page container

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Basket from '../pages/basket'
+import Checkout from '../pages/checkout'
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route path='/basket' component={Basket} />
+      <Route path='/checkout' component={Checkout} />
     </Switch>
   </BrowserRouter>
 )
