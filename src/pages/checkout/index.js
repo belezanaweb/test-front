3db0ffa8b632e8f1b920e19efb45fa72eafbce29
Marcos Borges <marcos.abb52@gmail.com>
2018-08-09 19:38:02 -0300

    Adding form component

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
-const Checkout = () => <h1>Checkout page</h1>
+import Content from '../../components/Content'
+import Summary from '../../components/Summary'
+import Button from '../../components/Button'
+
+import { Container } from './styles'
+
+const Checkout = () => (
+  <Container>
+    <Content title='Cartão de Crédito' />
+    <Summary />
+    <Button>Finalizar o pedido</Button>
+  </Container>
+)
 
 export default Checkout
---
+++ src/pages/checkout/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  padding: 10px;
+`
