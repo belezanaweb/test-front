85f3d13f9d23dbb9b009a7a7c524b7bf70ca65e4
Marcos Borges <marcos.abb52@gmail.com>
2018-08-09 13:34:45 -0300

    Configuring route code splitting using react-loadable

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Loadable from 'react-loadable'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import Basket from '../pages/basket'
-import Checkout from '../pages/checkout'
-import Finish from '../pages/finish'
+// import Basket from '../pages/basket'
+// import Checkout from '../pages/checkout'
+// import Finish from '../pages/finish'
+
+const Basket = Loadable({
+  loader: () => import('../pages/basket'),
+  loading: () => <h1>Loading!</h1>
+})
+
+const Checkout = Loadable({
+  loader: () => import('../pages/checkout'),
+  loading: () => <h1>Loading!</h1>
+})
+
+const Finish = Loadable({
+  loader: () => import('../pages/finish'),
+  loading: () => <h1>Loading!</h1>
+})
 
 const Router = () => (
   <BrowserRouter>
