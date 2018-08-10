9583578e4f4e31354ecc736ededce165205c8435
Marcos Borges <marcos.abb52@gmail.com>
2018-08-09 22:26:17 -0300

    Adding isActive rule to steps component

--- src/components/Steps/index.js
+++ src/components/Steps/index.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { withRouter } from 'react-router-dom'
 
 import { Container, List, Item } from './styles'
 
-const Steps = props => (
+const isActive = (path, name) => path.replace('/', '') === name
+
+const Steps = ({ location: { pathname } }) => (
   <Container>
     <List>
-      <Item isActive>Sacola</Item>
-      <Item>Pagamento</Item>
-      <Item>Confirmação</Item>
+      <Item isActive={isActive(pathname, 'basket')}>Sacola</Item>
+      <Item isActive={isActive(pathname, 'checkout')}>Pagamento</Item>
+      <Item isActive={isActive(pathname, 'finish')}>Confirmação</Item>
     </List>
   </Container>
 )
+
+Steps.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string
+  }).isRequired
+}
 
 export default withRouter(Steps)
