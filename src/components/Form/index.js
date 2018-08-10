b9a87481abaaad58c146d0a1dd37139bcb171500
Marcos Borges <marcos.abb52@gmail.com>
2018-08-10 01:23:47 -0300

    Changing input mask component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,50 +1,24 @@
 import React from 'react'
-import Mask from 'react-text-mask'
+import Mask from 'react-maskedinput'
 
 import Input from './Input'
 
 import { Container, Group } from './styles'
 
-const masks = {
-  card: [
-    /[1-9]/,
-    /\d/,
-    /\d/,
-    /\d/,
-    '.',
-    /\d/,
-    /\d/,
-    /\d/,
-    /\d/,
-    '.',
-    /\d/,
-    /\d/,
-    /\d/,
-    /\d/,
-    '.',
-    /\d/,
-    /\d/,
-    /\d/,
-    /\d/
-  ],
-  validity: [/[1-9]/, /\d/, '/', /\d/, /\d/, /\d/, /\d/],
-  code: [/[1-9]/, /\d/, /\d/]
-}
-
 const Form = () => (
   <Container>
     <Input label='Número do cartão:' id='card'>
-      <Mask id='card' mask={masks.card} placeholder='____.____.____.____' />
+      <Mask id='card' mask='1111.1111.1111.1111' placeholder='____.____.____.____' />
     </Input>
     <Input label='Nome do Titular:' id='holder'>
-      <Mask id='holder' placeholder='Como no cartão' />
+      <input id='holder' placeholder='Como no cartão' />
     </Input>
     <Group>
       <Input label='Validade (mês/ano):' id='validity' width={55}>
-        <Mask id='validity' mask={masks.validity} placeholder='__/____' />
+        <Mask id='validity' mask='11/1111' placeholder='__/____' />
       </Input>
       <Input label='CVV:' id='code' width={40}>
-        <Mask id='code' mask={masks.code} placeholder='___' />
+        <Mask id='code' mask='111' placeholder='___' />
       </Input>
     </Group>
   </Container>
