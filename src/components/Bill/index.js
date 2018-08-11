1bd168dad41aec4a75153e62316ebf94ae0d077a
Marcos Borges <marcos.abb52@gmail.com>
2018-08-11 19:59:39 -0300

    Fixing bill component prop types

--- src/components/Bill/index.js
+++ src/components/Bill/index.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import Detail from './Detail'
 
 const Bill = ({ bill }) => (
   <ul>
-    <Detail value={bill.card} />
-    <Detail value={bill.name} />
-    <Detail value={bill.expiration} />
+    <Detail value={bill.card || '****.****.****.1234'} />
+    <Detail value={bill.name || 'John Doe'} />
+    <Detail value={bill.expiration || '01/2020'} />
   </ul>
 )
+
+Bill.propTypes = {
+  bill: PropTypes.shape({
+    card: PropTypes.string,
+    name: PropTypes.string,
+    expiration: PropTypes.string
+  }).isRequired
+}
 
 const mapStateToProps = state => ({
   bill: state.checkout.data
