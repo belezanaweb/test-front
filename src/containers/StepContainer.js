ad2af5c2b2501d2627b86a4217de0ce6f4d77393
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-08-26 02:16:38 -0300

    refactor(stepcontainer): add steps array

--- src/containers/StepContainer.js
+++ src/containers/StepContainer.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
-const StepContainer = () => (
-  <span>steps</span>
+import Step from '../components/Step';
+
+const StepList = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`
+
+const steps = ['sacola', 'pagamento', 'confirmacao']
+
+const StepContainer = ({
+  location
+}) => (
+  <StepList>
+    {steps.map(step =>
+      <Step
+        title={step}
+        isActive={location.pathname === `/${step}`
+        ? true
+        : false } />
+    )}
+  </StepList>
 );
 
-export default StepContainer;
+export default withRouter(StepContainer);
