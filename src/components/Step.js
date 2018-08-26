bcafad0e4c92b243e3aca976c8f985e11f9edef6
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-08-26 04:31:13 -0300

    refactor(step): add variables to css

--- src/components/Step.js
+++ src/components/Step.js
@@ -2,8 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StepItem = styled.span`
-  color: ${({ isActive }) => (isActive ? '#FF7800' : '#CCC' )};
+  color: ${({ isActive }) =>
+    (isActive
+      ? 'var(--color-orange-1)'
+      : 'var(--color-light-gray-1)'
+    )};
   text-transform: uppercase;
+  font-weight: 700;
+  font-size: 1.3rem;
 `
 
 const Step = ({
--- src/containers/StepContainer.js
+++ src/containers/StepContainer.js
@@ -5,8 +5,12 @@ import styled from 'styled-components';
 import Step from '../components/Step';
 
 const StepList = styled.div`
+  height: 4rem;
   display: flex;
   justify-content: space-evenly;
+  align-items: center;
+  background-color: #fff;
+  box-shadow: var(--box-shadow-1);
 `
 
 const steps = ['sacola', 'pagamento', 'confirmacao']
@@ -17,6 +21,7 @@ const StepContainer = ({
   <StepList>
     {steps.map(step =>
       <Step
+        key={step}
         title={step}
         isActive={location.pathname === `/${step}`
         ? true
