ca2a784237954773bbffdcae438b84c683bb967c
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-08-26 01:36:43 -0300

    feat(index): add store and step reducer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const stepReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE_STEP':
+      return {
+        ...state,
+        step: action.step
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(
+  stepReducer
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
