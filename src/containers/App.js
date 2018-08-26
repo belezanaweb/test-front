7137bd715963afa08a641287b9d21cb3c1f08862
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-08-26 01:36:06 -0300

    refactor(app): add router switch and containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import CartContainer from './CartContainer';
+import PaymentContainer from './PaymentContainer';
+import ConfirmationContainer from './ConfirmationContainer';
+import StepContainer from './StepContainer';
 
 const App = () => (
-  <span>prince</span>
+  <Router>
+    <Fragment>
+      <StepContainer />
+      <Switch>
+        <Route exact path='/cart' component={CartContainer}/>
+        <Route path='/payment' component={PaymentContainer} />
+        <Route path='/confirmation' component={ConfirmationContainer} />
+        <Redirect from='/' to='/cart' />
+      </Switch>
+    </Fragment>
+  </Router>
 )
 
 export default App;
