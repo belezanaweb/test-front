5752a6a4ffc7a33ac1e1577cead2c07f2cad9b36
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-08-26 04:32:27 -0300

    refactor(cartContainer): add fetch api and chang to class component

--- src/containers/CartContainer.js
+++ src/containers/CartContainer.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import Box from '../components/Box';
 
-const CartContainer = () => (
-  <span>CART</span>
-);
+class CartContainer extends React.Component {
+
+  componentDidMount() {
+    fetch('https://www.mocky.io/v2/5b15c4923100004a006f3c07')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+        }
+      )
+  }
+
+  render() {
+    return (
+      <Box title="produtos">
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </Box>
+    );
+  }
+};
 
 export default CartContainer;
