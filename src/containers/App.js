d68708a31680d7d5a32b860777aece9d6efa0c79
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-08-27 21:44:27 -0300

    feat(margin): add margin component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,29 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import styled from 'styled-components';
 
 import CartContainer from './CartContainer';
 import PaymentContainer from './PaymentContainer';
 import ConfirmationContainer from './ConfirmationContainer';
 import StepContainer from './StepContainer';
 
+const Margin = styled.div`
+  margin: 2rem 1rem;
+`
+
+
 const App = () => (
   <Router>
     <Fragment>
       <StepContainer />
-      <Switch>
-        <Route exact path='/sacola' component={CartContainer}/>
-        <Route path='/pagamento' component={PaymentContainer} />
-        <Route path='/confirmacao' component={ConfirmationContainer} />
-        <Redirect from='/' to='/sacola' />
-      </Switch>
+      <Margin>
+        <Switch>
+          <Route exact path='/sacola' component={CartContainer}/>
+          <Route path='/pagamento' component={PaymentContainer} />
+          <Route path='/confirmacao' component={ConfirmationContainer} />
+          <Redirect from='/' to='/sacola' />
+        </Switch>
+      </Margin>
     </Fragment>
   </Router>
 )
