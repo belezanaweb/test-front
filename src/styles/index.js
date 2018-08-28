33de4b9719e3b1f97ea7a96043b358f896376d07
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-08-28 02:32:06 -0300

    feat(styles): add button colors

--- src/styles/index.js
+++ src/styles/index.js
@@ -4,6 +4,9 @@ injectGlobal`
 
   :root {
     --color-orange-1: #FF7800;
+    --color-orange-2: #ff6c00;
+    --color-orange-3: #d45a00;
+
     --color-light-gray-1: #CCC;
     --color-dark-gray-2: #EEE;
     --color-dark-gray-3: #999;
@@ -11,6 +14,8 @@ injectGlobal`
     --border-box: 1px solid #ccc;
 
     --box-shadow-1: 0 1px 5px 0 rgba(0, 0, 30, .20);
+    --box-shadow-bottom-1: 0 5px #ff6c00;
+    --box-shadow-bottom-2: 0 4px #d45a00;
   }
 
   * {
