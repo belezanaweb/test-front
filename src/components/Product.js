70223af95f142105df47b57c5e308d2d5fa20bcc
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-08-28 01:44:52 -0300

    refactor(product): add markup and styles

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-const Products = () => (
-  <span>products list</span>
-)
+import { formatToReal } from '../utils/currencyFormat'
 
-export default Products
+const ProductImg = styled.div`
+  margin-right: 1rem;
+`
+
+const ProductItem = styled.li`
+  display: flex;
+  padding: 1rem;
+  margin: 1rem;
+  border: var(--border-box);
+`
+
+const ProductDetails = styled.div`
+  display:flex;
+  flex: 1 1 auto;
+  flex-direction: column;
+  justify-content: space-between;
+  font-size: 1.4rem;
+
+  span:last-child {
+    font-weight: bolder;
+    align-self: flex-end;
+  }
+`
+
+const Product = ({
+  thumbnail,
+  name,
+  price
+}) => (
+  <ProductItem>
+    <ProductImg>
+      <img alt='Product' src={thumbnail} />
+    </ProductImg>
+    <ProductDetails>
+      <span>{name}</span>
+      <span>{formatToReal(price)}</span>
+    </ProductDetails>
+  </ProductItem>
+);
+
+Product.propTypes = {
+  thumbnail: PropTypes.string,
+  name: PropTypes.string,
+  price: PropTypes.number
+}
+
+export default Product
