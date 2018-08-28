e9e0621cdc2d0e127b898ceb389cb1129008ad76
Joaquim Prince <jpqvasconcelos@gmail.com>
2018-08-28 01:45:25 -0300

    refactor(cart container): add product list  styled component

--- src/containers/CartContainer.js
+++ src/containers/CartContainer.js
@@ -1,10 +1,16 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import { saveData } from '../actions';
 
+import Product from '../components/Product';
 import Box from '../components/Box';
 import Summary from '../components/Summary';
+
+const ProductList = styled.ul`
+  padding: 1px 0;
+`
 
 class CartContainer extends React.Component {
 
@@ -32,14 +38,23 @@ class CartContainer extends React.Component {
       discount
     } = data
 
+    const { items } = data;
+    console.log(items)
     return (
       <Fragment>
         <Box title="produtos">
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-          </ul>
+          <ProductList>
+            {items.map(item =>
+              <Product key={item.product.sku}
+                thumbnail={
+                  item.product.imageObjects[0].thumbnail
+                }
+                name={item.product.name}
+                price={
+                  item.product.priceSpecification.price
+                } />
+            )}
+          </ProductList>
         </Box>
         <Summary
           products={subTotal}
