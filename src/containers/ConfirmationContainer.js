dfd5551520044b1668068e0f45d9f15200337dec
princevasconcelos <jpqvasconcelos@gmail.com>
2018-08-31 03:15:20 -0300

    feat(confirmation): add icon and box

--- src/containers/ConfirmationContainer.js
+++ src/containers/ConfirmationContainer.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
-const ConfirmationContainer = () => (
-  <span>CONFIRMATION</span>
+import Box from '../components/Box';
+
+const IconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: var(--color-orange-1);
+
+  i {
+    font-size: 3rem;
+    letter-space: -1px;
+    border: 2px solid var(--color-orange-1);
+    padding: 1rem;
+    border-radius: 100%;
+    margin-bottom: 1rem;
+  }
+
+  span {
+    font-size: 2rem;
+    text-transform: uppercase;
+  }
+`
+
+const PaymentDetils = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  font-size: 1.4rem;
+`
+
+const ConfirmationContainer = ({
+  location,
+}) => (
+  <Fragment>
+    <IconContainer >
+      <i className='fa fa-check' aria-hidden="true"></i>
+      <span>compra efetuada com sucesso</span>
+    </IconContainer>
+    <Box title='pagamento'>
+      <PaymentDetils>
+        <span>//todo: pegar as informacoes vindo da rota ao inves do state</span>
+        {/* <span>{location.state.cardNumber.slice(0,-4).replace(/\d/g, '*')} {location.state.cardNumber.slice(-4)}</span>
+        <span>{location.state.cardName}</span>
+        <span>{location.state.cardDate}</span> */}
+      </PaymentDetils>
+    </Box>
+  </Fragment>
 );
 
-export default ConfirmationContainer;
+export default withRouter(ConfirmationContainer);
