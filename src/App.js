f6c209fdd86cb325b3ce00ddb1795be250eedfaf
Franklin Javier <franklinjalves@gmail.com>
2019-03-27 12:11:02 -0300

    add: cra v2 as bootstrap

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
