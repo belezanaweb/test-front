9b276dcc7dc08b6c567f77378fda674c397b90be
ernandesferreira <ernandes.dev@gmail.com>
2019-04-02 17:27:02 -0300

    instalando dependencias

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+  text-align: center;
+`
+const AppHeader = styled.header`
+  background-color: ${props => props.backgroundColor || "#282c34"};
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
+      <AppContainer className="App">
+        <AppHeader className="App-header" backgroundColor="#6b2477">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          teste front
+        </AppHeader>
+      </AppContainer>
     );
   }
 }
