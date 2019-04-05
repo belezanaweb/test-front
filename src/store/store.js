cb14186e6efff45bdae3a711f890201cf114fcfb
mayconbalves <mayconmustaine@hotmail.com>
2019-04-05 11:33:17 -0300

    feat(*): create store

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk'
+// import reducers from '../reducers/index'
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = applyMiddleware(thunk)(createStore)(devTools)
+
+export default store
