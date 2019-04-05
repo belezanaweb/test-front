f1ca0098d78d39d4db9a32b09f27986b5d5a2813
mayconbalves <mayconmustaine@hotmail.com>
2019-04-05 12:42:14 -0300

    feat(actions): create cart actions

---
+++ src/actions/cart.js
@@ -0,0 +1,28 @@
+import {
+  CART_PRODUCTS_SUCCESS,
+  CART_PRODUCTS_ERROR
+} from '../constants/actionTypes'
+
+import URL from '../constants/api'
+
+const fetchProductsSuccess = data => {
+  console.log(data, 'data')
+  return {
+    type: CART_PRODUCTS_SUCCESS,
+    payload: data
+  }
+}
+
+const fetchProductsError = error => {
+  return {
+    type: CART_PRODUCTS_ERROR,
+    payload: error
+  }
+}
+
+export const fetchProductsCart = () => dispatch => {
+  fetch(URL.PRODUCTS_CART)
+      .then((res) => res.json())
+      .then((response) => dispatch(fetchProductsSuccess((response))))
+      .catch((error) => dispatch(fetchProductsError((error))))
+}
