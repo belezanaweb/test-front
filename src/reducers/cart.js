5430050cd2f551a66fcda8fa46d5872e41344eb2
mayconbalves <mayconmustaine@hotmail.com>
2019-04-05 12:43:37 -0300

    feat(reducers): create cart reducer

---
+++ src/reducers/cart.js
@@ -0,0 +1,35 @@
+import {
+  CART_PRODUCTS_SUCCESS,
+  CART_PRODUCTS_ERROR
+} from '../constants/actionTypes'
+
+const initialState = {
+  cart: {
+    discount: '',
+    id: '',
+    items: [],
+    shippingTotal: 0,
+    subTotal: 0,
+    total: 0,
+  },
+  error: null
+}
+
+export default function cart (state = initialState, action) {
+  switch(action.type) {
+    case CART_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        cart: action.payload,
+        error: null
+      }
+    case CART_PRODUCTS_ERROR:
+      return {
+        ...state,
+        cart: null,
+        error: action.payload
+      }
+    default:
+      return state
+  }
+}
