e71ab9f9980837a95ae1fa21109c978611b61c8a
mayconbalves <mayconmustaine@hotmail.com>
2019-04-07 21:18:09 -0300

    feat(root): add router of payments

--- src/Root.js
+++ src/Root.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import { Provider } from 'react-redux'
 
 import Cart from './containers/Cart'
+import Payment from './containers/Payment'
 
 const Root = ({ store }) => (
 	<Provider store={store}>
 		<Router>
 			<Switch>
+				<Route exact path='/pagamento' component={Payment} />
 				<Route exact path='/' component={Cart} />
 				<Redirect to='/' />
 			</Switch>
