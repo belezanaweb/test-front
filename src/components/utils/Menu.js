a0fe7bf763c22e50b22747da8db6b1dce2105c62
mayconbalves <mayconmustaine@hotmail.com>
2019-04-09 00:06:21 -0300

    refactor(menu): add styled on menu

--- src/components/utils/Menu.js
+++ src/components/utils/Menu.js
@@ -1,32 +1,45 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
-import { Link } from 'react-router-dom'
 import styled from '@emotion/styled'
 
 const MenuBody = styled('nav')`
-  margin: 32px 13px 0 14px;
-  height: 72px;
-  width: 387px;
+  height: 40px;
+  width: 360px;
   border-radius: 3px;
   background-color: #FFF;
   box-shadow: 1px 1px 5px 0 rgba(0,0,29,0.22);
+
 
   ul {
     display: flex;
     justify-content: space-around;
 
     li {
-      margin: 44px 29px 12px 0;
+      margin: 13px 29px 12px 0;
       list-style: none;
 
       .sacola {
+        text-transform: uppercase;
+        text-decoration: none;
         height: 16px;
         width: 61px;
         color: #FF7800;
         font-family: "Helvetica Neue";
         font-size: 13px;
         font-weight: 700;
-        letter-spacing: NaNpx;
+        line-height: 16px;
+        text-align: center;
+      }
+
+      .pagamento, .confirmacao {
+        text-transform: uppercase;
+        text-decoration: none;
+        height: 16px;
+        width: 89px;
+        color: #CCC;
+        font-family: "Helvetica Neue";
+        font-size: 13px;
+        font-weight: 700;
         line-height: 16px;
         text-align: center;
       }
@@ -39,13 +52,13 @@ const Menu = () => {
     <MenuBody>
       <ul>
         <li>
-          <Link className='sacola'>Sacola</Link>
+          <a href='/' className='sacola'>Sacola</a>
         </li>
         <li>
-          <Link>Pagamento</Link>
+          <a href='/' className='pagamento'>Pagamento</a>
         </li>
         <li>
-          <Link>Confirmação</Link>
+          <a href='/' className='confirmacao'>Confirmação</a>
         </li>
       </ul>
     </MenuBody>
