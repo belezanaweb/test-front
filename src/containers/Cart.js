e664fdc0168a05913de6f4dac94c354803f19826
mayconbalves <mayconmustaine@hotmail.com>
2019-04-08 23:47:32 -0300

    refactor(*): change style of button

--- src/containers/Cart.js
+++ src/containers/Cart.js
@@ -34,13 +34,30 @@ const ConfirmButton = styled('button')`
   border-radius: 3px;
   background-color: #FF6C00;
   box-shadow: inset 0 -3px 0 0 #D45A00, 0 2px 4px 0 rgba(0,0,0,0.25);
-  color: #FFF;
+
+  a {
+    height: 24px;
+    width: 339px;
+    color: #FFF;
+    font-family: "Helvetica Neue";
+    font-size: 20px;
+    font-weight: 700;
+    letter-spacing: 0.5px;
+    line-height: 24px;
+    text-decoration: none;
+    text-align: center;
+  }
+`
+
+const PaymentParagraph = styled('p')`
+  height: 17px;
+  width: 319px;
+  color: #999;
   font-family: "Helvetica Neue";
-  font-size: 20px;
+  font-size: 14px;
   font-weight: 700;
-  letter-spacing: 0.5px;
-  line-height: 24px;
-  text-align: center;
+  letter-spacing: NaNpx;
+  line-height: 17px;
 `
 
 class Cart extends Component {
@@ -80,7 +97,7 @@ class Cart extends Component {
     return (
       <Container>
         <Menu />
-        <h1>Produtos</h1>
+        <PaymentParagraph>Produtos</PaymentParagraph>
         <CardContainer>
           {
             cart.items.map((item, index) =>
--- src/containers/Payment.js
+++ src/containers/Payment.js
@@ -73,13 +73,19 @@ const ConfirmButton = styled('button')`
   border-radius: 3px;
   background-color: #FF6C00;
   box-shadow: inset 0 -3px 0 0 #D45A00, 0 2px 4px 0 rgba(0,0,0,0.25);
-  color: #FFF;
-  font-family: "Helvetica Neue";
-  font-size: 20px;
-  font-weight: 700;
-  letter-spacing: 0.5px;
-  line-height: 24px;
-  text-align: center;
+  
+  a {
+    height: 24px;
+    width: 339px;
+    color: #FFF;
+    font-family: "Helvetica Neue";
+    font-size: 20px;
+    font-weight: 700;
+    letter-spacing: 0.5px;
+    line-height: 24px;
+    text-decoration: none;
+    text-align: center;
+  }
 `
 
 class Payment extends Component {
