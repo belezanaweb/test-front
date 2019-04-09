00650d2887e4c58ece4eb794a6b4240968490249
mayconbalves <mayconmustaine@hotmail.com>
2019-04-08 23:53:32 -0300

    fix(cart): resolve bug inside state of reducer

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -43,7 +43,6 @@ export default function cart (state = initialState, action) {
       case CART_DATA_SUCCESS:
         return {
           ...state,
-          cart: action.payload,
           payment: action.payload,
           error: null,
         }
