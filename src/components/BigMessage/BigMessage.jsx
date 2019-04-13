82ed220ca82291ad4cb1d9b20b85b9815cf49ecb
Christian Valentin <chris_valentin@outlook.com>
2019-04-13 20:54:29 -0300

    add: BigMessage component

---
+++ src/components/BigMessage/BigMessage.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import success from '../../assets/images/success-icon.png'
+
+import jss from 'jss'
+import preset from 'jss-preset-default'
+
+jss.setup(preset())
+
+const styles = {
+  container: {
+    margin: { bottom: 20 },
+  },
+  icon: {
+    display: 'block',
+    margin: {
+      top: 0,
+      left: 'auto',
+      right: 'auto',
+      bottom: 10,
+    },
+  },
+  text: {
+    display: 'block',
+    color: '#FF7800',
+    fontWeight: 'bold',
+    fontSize: 14,
+    lineHeight: '17px',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+  },
+}
+
+const { classes } = jss.createStyleSheet(styles).attach()
+
+const BigMessage = ({
+  message,
+}) =>
+  <div className={classes.container}>
+    <img src={success} className={classes.icon} />
+    <span className={classes.text}>{message}</span>
+  </div>
+
+export default BigMessage
