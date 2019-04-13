2772d38ee03e5442ff6f58a90020e1d3b9ba4eea
Christian Valentin <chris_valentin@outlook.com>
2019-04-13 15:48:40 -0300

    add: initial dependencies

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Libs
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+// JSS
+import jss from 'jss'
+import preset from 'jss-preset-default'
+jss.setup(preset())
+
+const store = createStore(state => state)
+const app =
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
