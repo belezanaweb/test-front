62cd5dcbb4bb2de91a11ceb1925ff310eb68c05b
Christian Valentin <chris_valentin@outlook.com>
2019-04-13 19:47:34 -0300

    add: cart item list mocked component

---
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+import jss from 'jss'
+import preset from 'jss-preset-default'
+
+jss.setup(preset())
+
+const styles = {
+  card: {
+    display: 'flex',
+    padding: [12, 5],
+    borderRadius: 3,
+    margin: { bottom: 15 },
+    border: {
+      width: 1,
+      style: 'solid',
+      color: '#EEE',
+    },
+    '&:last-child': {
+      margin: { bottom: 0 },
+    }
+  },
+  column: {
+    flex: 'auto',
+    padding: [0, 5],
+  },
+  thumb: {
+    height: 65,
+    width: 65,
+  },
+  productName: {
+    display: 'block',
+    lineHeight: '16px',
+    fontSize: 13,
+    color: '#212122',
+    height: 32,
+    overflow: 'hidden',
+    margin: { bottom: 15 },
+  },
+  productPrice: {
+    display: 'block',
+    color: '#212122',
+    lineHeight: '17px',
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'right',
+  },
+}
+
+const { classes } = jss.createStyleSheet(styles).attach()
+
+const CartItem = ({
+  name,
+  thumb,
+  price,
+}) =>
+  <div className={classes.card}>
+    <div className={classes.column}>
+      <img src={thumb} title={name} className={classes.thumb} />
+    </div>
+    <div className={classes.column}>
+      <span className={classes.productName}>{name}</span>
+      <span className={classes.productPrice}>R$ {price.toFixed(2).replace('.', ',')}</span>
+    </div>
+  </div>
+
+export default CartItem
