cacfb4335ce83cfee5533b27aff825afd9ba53f9
Christian Valentin <chris_valentin@outlook.com>
2019-04-13 18:31:06 -0300

    add: Title component

---
+++ src/components/UI/Title/Title.js
@@ -0,0 +1,19 @@
+import styled from 'styled-jss'
+
+const Title = styled('h2')({
+  color: '#999',
+  fontWeight: 'bold',
+  fontSize: 14,
+  lineHeight: '20px',
+  textTransform: 'uppercase',
+  display: 'block',
+  padding: [0, 10],
+  margin: {
+    top: 0,
+    bottom: 5,
+    left: 0,
+    right: 0,
+  },
+})
+
+export default Title
