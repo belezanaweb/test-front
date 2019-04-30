745bd5095bbcf8a7f4e136dfcae4aa7396eb7570
Felipe Chama <felipechama42@gmail.com>
2019-04-29 22:19:05 -0300

    Improve style link component

--- src/components/Link/styles.js
+++ src/components/Link/styles.js
@@ -4,6 +4,7 @@ export const ButtonLink = styled.button`
   height: 60px;
   width: 100%;
   border: 0px;
+  cursor: pointer;
   font-size: 20px;
   font-weight: bold;
   letter-spacing: 0.5px;
@@ -12,4 +13,8 @@ export const ButtonLink = styled.button`
   color: ${props => props.theme.color.white};
   background-color: ${props => props.theme.color.orangeMedium};
   box-shadow: inset 0 -3px 0 0 ${props => props.theme.color.orangeDark}, 0 2px 4px 0 rgba(0, 0, 0, 0.25);
+
+  &:hover {
+    background-color: ${props => props.theme.color.orangeDark};
+  }
 `;
