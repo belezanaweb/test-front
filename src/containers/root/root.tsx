940ebe4eb7d865fa55af5d6101133d8a25f15420
Thiago Capelo <thiagokpelo@gmail.com>
2019-05-27 13:55:51 -0300

    Create a theme object - theme_provider

--- src/containers/root/root.tsx
+++ src/containers/root/root.tsx
@@ -3,13 +3,55 @@ import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
 import CheckoutContainer from '../checkout/checkout';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+
+  breakpoints: {},
+
+  palette: {
+
+    common: {
+      white: '#FFF',
+      black: '#000',
+    },
+
+    primary: {
+      main: '#FF6C00',
+      dark: '#D45A00',
+      contrastText: '#FFF',
+    },
+
+    secondary: {
+      main: '#A43287',
+    },
+
+    neutral: {
+      main: '#212122',
+      light: '#E0E7EE',
+      lightest: '#E7E7E7',
+      medium: '#CCC',
+      dark: '#212121',
+    },
+
+    support: {
+      error: '#F30',
+      success: '',
+    },
+
+  },
+
+  spacing: {},
+}
 
 const Root: React.FC<any> = ({ store }) => (
   <Provider store={store}>
-    <Switch>
-      <Route path="/checkout" component={CheckoutContainer} />
-      <Route from="*" render={() => <div>Error 404</div>} />
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route path="/checkout" component={CheckoutContainer} />
+        <Route from="*" render={() => <div>Error 404</div>} />
+      </Switch>
+    </ThemeProvider>
   </Provider>
 );
 
