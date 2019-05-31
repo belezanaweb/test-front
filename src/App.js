4128762ad4fca0a39641bc9cb703c81e90d4c354
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-05-31 01:59:38 -0300

    initial configurations; routing system setted up; main scene created;

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Checkout from './scenes/Checkout';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
+      <BrowserRouter>
+        <Route path="/" component={ Checkout }/>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/scenes/Checkout/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Checkout extends Component {
+  render() {
+    return <div>Main scene</div>;
+  }
+}
