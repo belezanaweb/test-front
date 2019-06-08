2914c4fc29e5717ead16ca5d3a6bdf5771ced913
luisCarvalhoJr <carvalholuis@bitbucket.com>
2019-06-08 13:14:25 -0300

    initial config

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
       </div>
     );
   }
