d6a729b5f08bdca6c953494ece565504ed01ba15
luisCarvalhoJr <carvalholuis@bitbucket.com>
2019-06-09 18:43:07 -0300

    global css

---
+++ src/styles/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+  *{
+    margin: 0;
+    padding:0;
+  }
+
+  body{
+    background: #eee;
+  }
+
+`;
+
+export default GlobalStyle;
