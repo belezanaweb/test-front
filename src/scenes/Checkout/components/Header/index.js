fd3c690e060ffa44f113fb2518f2c805daffa7c4
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-06-10 16:19:53 -0300

    dynamic header

--- src/scenes/Checkout/components/Header/index.js
+++ src/scenes/Checkout/components/Header/index.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import { Container, Item } from './styles';
+import { Container, Item } from './styles'
 
-export default class components extends Component {
+import { connect } from 'react-redux'
+
+class Header extends Component {
   render() {
     return (
-        <Container>
-          <Item active>Sacola</Item>
-          <Item>Pagamento</Item>
-          <Item>Confirmação</Item>
-        </Container>
-    );
+      <Container>
+        <Item active={this.props.page == 'Cart' ? true : false}>Sacola</Item>
+        <Item active={this.props.page == 'Payment' ? true : false}>Pagamento</Item>
+        <Item active={this.props.page == 'Conclusion' ? true : false}>Confirmação</Item>
+      </Container>
+    )
   }
 }
+
+export default connect(state => ({
+  page: state.page.page
+}))(Header)
