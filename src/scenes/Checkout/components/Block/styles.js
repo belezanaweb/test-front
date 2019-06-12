7eff44a17d136283feb87126a3871d9299558a68
Samuel Levy <samuellevy@Samuels-iMac.local>
2019-06-12 03:23:15 -0300

    fix on edges

--- src/scenes/Checkout/components/Block/styles.js
+++ src/scenes/Checkout/components/Block/styles.js
@@ -1,89 +1,90 @@
-import styled from 'styled-components';
-import React from 'react';
+import styled from 'styled-components'
+import React from 'react'
 
-import colors from '../../../../configs/colors';
-import metrics from '../../../../configs/metrics';
+import colors from '../../../../configs/colors'
+import metrics from '../../../../configs/metrics'
 
 export const Container = styled.div`
-    display: relative;
-    width: 100%;
-    margin: 5px 0;
-    background-color: ${colors.white};
-    border-radius: ${metrics.borderRadius};
-    box-shadow: 1px 1px 5px 0 rgba(0,0,29,0.22);
+  display: relative;
+  width: 100%;
+  margin: 5px 0;
+  background-color: ${colors.white};
+  border-radius: ${metrics.borderRadius};
+  box-shadow: 1px 1px 5px 0 rgba(0, 0, 29, 0.22);
 
-    padding: 5px 10px; //fixing padding because layout with little padding out of default
-    &.cart{
-        padding: 5px 12px; //fixing padding because layout with little padding out of default
-    }
-`;
+  padding: 5px 10px; //fixing padding because layout with little padding out of default
+  &.cart {
+    padding: 5px 12px; //fixing padding because layout with little padding out of default
+  }
+`
 
 export const Item = styled.li`
-    width: 100%;
-    height: 90px;
-    border: 1px #EEE solid;
-    border-radius: ${metrics.borderRadius};
-    display: inline-flex;
-    margin: 7px 0 8px;
-    padding: 4px 0px;
-    line-height: 16px;
-    // align-items: center;
-    &.condensed{
-        margin: 15px 0 6px 0;
-        &:first-child{
-            margin: 10px 0 0 0;
-        }
-        height: 64px;
-        div > img{
-            width: 45px;
-            height: 45px;
-        }
+  width: 100%;
+  height: 90px;
+  border: 1px #eee solid;
+  border-radius: ${metrics.borderRadius};
+  display: inline-flex;
+  margin: 7px 0 8px;
+  padding: 4px 0px;
+  line-height: 16px;
+  // align-items: center;
+  &.condensed {
+    margin: 15px 0 6px 0;
+    &:first-child {
+      margin: 10px 0 0 0;
     }
-`;
+    height: 64px;
+    div > img {
+      width: 45px;
+      height: 45px;
+    }
+  }
+`
 
 export const BoxImage = styled.div`
-    padding: 0 9px 1px;
-    align-self: center;
+  padding: 0 9px 1px;
+  align-self: center;
 `
 
 export const BoxInfo = styled.div`
-    padding: 7px 0px;
-    position: relative;
+  padding: 7px 0px;
+  position: relative;
+  width: 100%;
 `
 
 export const Image = styled.img`
-    width:65px;
-    height:65px;
+  width: 65px;
+  height: 65px;
 `
 export const DescriptionText = styled.p`
-    font-size: ${metrics.fonts.size.small};
-    min-height: 32px;
-    max-height: 32px;
-    padding: 0 2px;
+  font-size: ${metrics.fonts.size.small};
+  min-height: 32px;
+  max-height: 32px;
+  padding: 0 2px;
 `
 
 export const PriceText = styled.p`
-    font-size: ${metrics.fonts.size.small};
-    text-align: right;
-    padding: 0 9px 0 0;
-    font-size: ${metrics.fonts.size.normal};
-    font-weight: bold;
-    margin: 16px 0 0 0px;
-    width: 100%;
-    // position: absolute;
-    // bottom: 11px;
-    // right: -8px;
+  font-size: ${metrics.fonts.size.small};
+  text-align: right;
+  padding: 0 9px 0 0;
+  font-size: ${metrics.fonts.size.normal};
+  font-weight: bold;
+  margin: 16px 0 0 0px;
+  width: 100%;
+  // position: absolute;
+  // bottom: 11px;
+  // right: -8px;
 `
 
 export const Figure = ({ className, src, children }) => (
-    <BoxImage>
-        <Image src={src} alt="" />
-    </BoxImage>
-);
+  <BoxImage>
+    <Image src={src} alt="" />
+  </BoxImage>
+)
 
-export const Description = ({ text, price, showPrice=true, children }) => (
-    <BoxInfo>
-        <DescriptionText>{text}</DescriptionText>
-        <PriceText>{showPrice?price:""}</PriceText>
-    </BoxInfo>
-);
+export const Description = ({ text, price, showPrice = true, children }) => (
+  <BoxInfo>
+    <DescriptionText>{text}</DescriptionText>
+    <PriceText>{showPrice ? price : ''}</PriceText>
+  </BoxInfo>
+)
--- src/scenes/Checkout/components/Header/styles.js
+++ src/scenes/Checkout/components/Header/styles.js
@@ -1,28 +1,28 @@
-import styled from 'styled-components';
-import colors from '../../../../configs/colors';
-import metrics from '../../../../configs/metrics';
+import styled from 'styled-components'
+import colors from '../../../../configs/colors'
+import metrics from '../../../../configs/metrics'
 
 export const Container = styled.nav`
   display: flex;
   flex-direction: row;
-  width: 360px;
+  width: 100%;
   justify-content: space-between;
   height: 40px;
   align-items: center;
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   border-radius: ${metrics.borderRadius};
-  box-shadow: 1px 1px 5px 0 rgba(0,0,29,0.22);
+  box-shadow: 1px 1px 5px 0 rgba(0, 0, 29, 0.22);
   padding: 0px 16px 0px 21px;
   letter-spacing: 0px;
-`;
+`
 
 export const Item = styled.div`
   display: block;
-  color: ${props => props.active ? colors.primary : colors.graylight};
+  color: ${props => (props.active ? colors.primary : colors.graylight)};
   font-weight: bold;
   font-size: ${metrics.fonts.size.small};
   line-height: 16px;
   text-transform: uppercase;
   font-family: 'Helvetica Neue';
   text-align: center;
-`;
+`
