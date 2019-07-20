257728fabd85797e9a9305a020e95329e2d1d490
Gabriel Bibiano <gabriel.bibiano.a@gmail.com>
2019-07-19 21:13:06 -0300

    Style steps items

--- src/components/Steps.vue
+++ src/components/Steps.vue
@@ -1,7 +1,12 @@
 <template>
   <section class="steps">
     <ul>
-      <router-link tag="li" v-for="(item, key) in items" :key="key" :to="item.to">
+      <router-link 
+        tag="li" 
+        v-for="(item, key) in items" 
+        :key="key" 
+        :to="item.to"
+      >
         {{ item.label }}
       </router-link>
     </ul>
@@ -21,5 +26,14 @@ export default {
 </script>
 
 <style scoped lang="stylus">
-
+ul 
+  display flex
+  list-style none
+  margin 0
+  padding 0
+  justify-content space-around
+  color #FF7800
+  font-weight bold
+  text-transform uppercase
+  font-size 13px
 </style>
