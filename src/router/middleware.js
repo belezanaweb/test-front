e3f49e72ab61b936010ca63ed22e2ca960e4d4cf
Gabriel Bibiano <gabriel.bibiano.a@gmail.com>
2019-07-21 15:36:47 -0300

    Add validation when 'from' route is confirmation and 'to' is payment

--- src/router/middleware.js
+++ src/router/middleware.js
@@ -1,6 +1,14 @@
 export default ({ router, store }) => {
   router.beforeEach((to, from, next) => {
-    if (to.matched.some(record => record.meta.paymentIsValid)) {
+    
+    const fromConfirmationRoute = from.name === 'confirmation'
+    const isPaymentRoute = to.matched.some(record => record.name === 'payment')
+
+    if (store.state.paymentFormIsValid && fromConfirmationRoute && isPaymentRoute) {
+      next({
+        path: '/',
+      })
+    } else if (to.matched.some(record => record.meta.paymentIsValid)) {
       if (!store.state.paymentFormIsValid) {
         next({
           path: from.path,
