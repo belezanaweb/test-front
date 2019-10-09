7ded51b3fb9b4036e5f58a25de8fb3900bfd068d
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2019-10-09 16:50:19 -0300

    Adding Cart Component

---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Header from '../../components/Header/Header'
+
+class Cart extends Component {
+    render() {
+        return (
+            <div>
+                <Header></Header>
+            </div>
+        )
+    }
+}
+
+export default Cart
