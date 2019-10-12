714078396aff248708386d4db1df3035be1512d4
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2019-10-12 15:20:17 -0300

    Creating Cart Component

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import Header from '../../components/Header/Header'
+import './Cart.scss'
 
 class Cart extends Component {
     render() {
         return (
-            <div>
+            <div className='Cart'>
                 <Header></Header>
+                <h1>Produtos</h1>
+                <div className='products-container'>
+                    <div className='product'>
+                    </div>
+                </div>
             </div>
         )
     }
