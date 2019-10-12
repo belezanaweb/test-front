2bf81bec9de3fa30902257424e631b2c7f57b89d
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2019-10-12 16:28:24 -0300

    Adding Theme file

---
+++ src/theme/index.js
@@ -0,0 +1,8 @@
+export default {
+    primary: '#ff7800',
+    transition: '0.3s',
+    bold: 700,
+    inactiveColor: '#ccc',
+    boxShadow: '1px 1px 5px 0 rgba(0, 0, 29, 0.22)',
+    bgColor: '#fff'
+}
