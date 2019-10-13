b975ea95afae2af09c76e30138190a9a15ee2606
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2019-10-12 22:25:05 -0300

    creating Button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+import theme from '../../theme'
+
+const button = styled.button`
+  height: 4.615rem;
+  border-radius: ${theme.borderRadius};
+  background-color: ${theme.buttonColor};
+  color: ${theme.buttonTextColor};
+  border: 0;
+  box-shadow: inset 0 -3px 0 0 ${theme.buttonSecundaryColor}, 0 2px 4px 0 rgba(0,0,0,0.25);
+  width: 100%;
+  font-size: 1.538rem;
+  font-weight: ${theme.fontWeightBold};
+  line-height: 1.846rem;
+  letter-spacing: 0.5px;
+  text-align: center;
+  text-transform: uppercase;
+  margin-bottom: 7.231rem;
+
+  &:hover {
+    background-color: ${theme.buttonSecundaryColor};
+  }
+`
+
+export default button
