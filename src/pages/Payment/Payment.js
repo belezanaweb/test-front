435fd99a4b6aa8f976f1c5c9301138038fc0f1b1
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2019-10-13 17:38:58 -0300

    Creating Payment Page

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import CheckoutPage from 'components/CheckoutPage'
+import Input from 'components/Input'
+import Col from 'components/Col'
 
 const Payment = () => {
   return (
-    <Payment>
+    <CheckoutPage
+      title='Cartão de Crédito'
+      nextStep='/confirm'
+      textButton='Finalizar pedido'
+    >
+      <Col col='16'>
+        <Input label='Número do cartão:' placeholder='____.____.____.____' />
+      </Col>
 
-    </Payment>
+      <Col col='16'>
+        <Input label='Nome do Titular:' placeholder='Como no cartão' />
+      </Col>
+
+      <Col col='8'>
+        <Input label='Validade (mês/ano):' placeholder='__/____' />
+      </Col>
+      <Col col='6' offset='1'>
+        <Input label='CVV:' placeholder='___' />
+      </Col>
+
+
+    </CheckoutPage>
   )
 }
 
