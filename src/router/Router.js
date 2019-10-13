3054509e77998f8965f66e72f1021f7faf0fece1
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2019-10-13 12:49:33 -0300

    Consuming redux store

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,18 +1,23 @@
 import React, { lazy } from 'react'
+import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
+
 import ScrollToTop from './ScrollToTop'
 import LazyRoute from './LazyRoute'
+import store from 'store'
 
 /* Pages */
 const Cart = lazy(() => import('../pages/Cart'))
 
 export default () => (
-  <Router>
-    <ScrollToTop>
-      <Switch>
-        <LazyRoute path="/cart" component={Cart} />
-        <LazyRoute path="*" component={Cart} />
-      </Switch>
-    </ScrollToTop>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <ScrollToTop>
+        <Switch>
+          <LazyRoute path="/cart" component={Cart} />
+          <LazyRoute path="*" component={Cart} />
+        </Switch>
+      </ScrollToTop>
+    </Router>
+  </Provider>
 )
