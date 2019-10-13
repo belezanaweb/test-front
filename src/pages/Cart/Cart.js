fa29d493a44cd751f83ea44fe84e3318d9e4242b
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2019-10-12 22:26:52 -0300

    Upgrading Cart component

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,20 +1,72 @@
 import React, { Component } from 'react'
 import Header from '../../components/Header'
-import { Title, CartPage, ProductContainer } from './style'
+import Button from '../../components/Button'
+import Image from '../../components/ImageWrapper'
+import {
+  Title,
+  CartPage,
+  ProductList,
+  ProductDetails,
+  Product,
+  CartCheckout,
+  CheckoutInfo
+} from './style'
 
 class Cart extends Component {
-    render() {
-        return (
-            <CartPage>
-                <Header></Header>
-                <Title>Produtos</Title>
-                <ProductContainer>
-                    <div className='product'>
-                    </div>
-                </ProductContainer>
-            </CartPage>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Header></Header>
+        <CartPage>
+          <Title>Produtos</Title>
+          <ProductList>
+            <Product>
+              <Image alt='Product' src='/product.test.png' width={65} height={65}></Image>
+              <ProductDetails>
+                <p>L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium</p>
+                <span>R$ 225,90</span>
+              </ProductDetails>
+            </Product>
+            <Product>
+              <Image alt='Product' src='/product.test.png' width={65} height={65}></Image>
+              <ProductDetails>
+                <p>L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium</p>
+                <span>R$ 225,90</span>
+              </ProductDetails>
+            </Product>
+            <Product>
+              <Image alt='Product' src='/product.test.png' width={65} height={65}></Image>
+              <ProductDetails>
+                <p>L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium</p>
+                <span>R$ 225,90</span>
+              </ProductDetails>
+            </Product>
+          </ProductList>
+
+          <CartCheckout>
+            <CheckoutInfo>
+              <span>Produto</span>
+              <span>R$ 624,80</span>
+            </CheckoutInfo>
+            <CheckoutInfo>
+              <span>Frete</span>
+              <span>R$ 5,30</span>
+            </CheckoutInfo>
+            <CheckoutInfo primary>
+              <span>Desconto</span>
+              <span>- R$ 30,00</span>
+            </CheckoutInfo>
+            <CheckoutInfo>
+              <span>Total</span>
+              <span>R$100,00</span>
+            </CheckoutInfo>
+          </CartCheckout>
+
+          <Button>Seguir para o pagamento</Button>
+        </CartPage>
+      </>
+    )
+  }
 }
 
 export default Cart
--- src/pages/Cart/style.js
+++ src/pages/Cart/style.js
@@ -2,29 +2,79 @@ import styled from 'styled-components'
 import theme from '../../theme'
 
 export const Title = styled.h1`
-  padding: 0 1.308rem;
+  padding: 0 0.8462rem;
   text-transform: uppercase;
-  color: #ccc;
+  color: #999;
+  font-weight: ${theme.fontWeightBold};
   font-size: 1.077rem;
-  margin-top: 0.7692rem;
   margin-bottom: 0.3846rem;
 `
 
 export const CartPage = styled.div`
+  padding: 0 0.6923rem;
+  padding-top: 0.7692rem;
   background-color: ${theme.secundaryBgColor};
 `
 
-export const ProductContainer = styled.div`
-    box-shadow: ${theme.boxShadow};
-    border-radius: ${theme.borderRadius};
-    background-color: ${theme.bgColor};
-    margin: 0 0.6923rem;
-    padding: 0.9231rem;
+export const ProductList = styled.div`
+  box-shadow: ${theme.boxShadow};
+  border-radius: ${theme.borderRadius};
+  background-color: ${theme.bgColor};
+  padding: 0.9231rem;
+`
 
-    .product {
-      min-height: 5.625rem;
-      border-radius: ${theme.borderRadius};
-      border: ${theme.border};
-      margin-bottom: 0.9375rem;
-    }
+export const Product = styled.div`
+  display: flex;
+  padding: 0.8462rem 0.7692rem 1.077rem 0.7692rem;
+  min-height: 5.625rem;
+  border-radius: ${theme.borderRadius};
+  border: ${theme.border()};
+  margin-bottom: 1.1539rem;
+`
+
+export const ProductDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 0.8462rem;
+  justify-content: space-between;
+
+  p {
+    margin-top: 0.07692rem;
+    color: ${theme.textColor};
+    line-height: 1.231rem;
+  }
+
+  span {
+    font-weight: ${theme.fontWeightBold};
+    color: ${theme.textColor};
+    font-size: 1.077rem;
+    line-height: 1.308rem;
+    text-align: right;
+  }
+`
+
+export const CartCheckout = styled.div`
+  padding: 1.154rem 1rem 1rem 1.077rem;
+  margin: 1.538rem 0;
+  box-sizing: border-box;
+  border-radius: ${theme.borderRadius};
+  border: ${theme.border('#ccc')};
+`
+
+export const CheckoutInfo = styled.div`
+  display: flex;
+  text-transform: uppercase;
+  justify-content: space-between;
+  color: ${props => props.primary ? theme.primary : theme.textColor};
+  font-size: 1.077rem;
+  line-height: 1.308rem;
+
+  &:not(:first-child) {
+    margin-top: ${props => props.primary ? '0.6154rem' : '0.6923rem'};
+  }
+
+  &:last-child {
+    margin-top: 1.231rem;
+    font-weight: ${theme.fontWeightBold}
+  }
 `
