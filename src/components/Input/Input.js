f7c0d358c057f909a75ceeb9f1bf841d08687521
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2019-10-13 22:55:11 -0300

    Improving Input for show errors and focus

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,13 +1,41 @@
-import React from 'react'
-import { StyledInput, StyledLabel } from './style'
+import React, { useState } from 'react'
+import { string } from 'prop-types'
+import { StyledInput, StyledLabel, StyledLabelError } from './style'
 
-const Input = ({ label, ...props }) => {
+
+const Input = ({ label, onChange, ...props }) => {
+  const [hasError, setError] = useState(false)
+
+  function handleChange(e) {
+    onChange(e)
+    setError(false)
+  }
+
+  function onInvalid() {
+    setError(true)
+  }
+
   return (
     <>
       <StyledLabel>{label}</StyledLabel>
-      <StyledInput {...props} />
+      <StyledInput
+        onInvalid={onInvalid}
+        {...props}
+        onChange={handleChange}
+        hasError={hasError}
+      />
+      <StyledLabelError hasError={hasError}>Campo inválido</StyledLabelError>
     </>
   )
+}
+
+Input.propTypes = {
+  value: string,
+  label: string.isRequired,
+}
+
+Input.defaultProps = {
+  value: ''
 }
 
 export default Input
