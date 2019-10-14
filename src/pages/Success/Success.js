a8b3bb5fa06d74f9f04b06a836015cb37508f53f
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2019-10-14 07:04:46 -0300

    Adding success message in final page

--- src/pages/Success/Success.js
+++ src/pages/Success/Success.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+
 import CheckoutPage from 'components/CheckoutPage'
-import { useForm } from 'utils/form.utils'
 import Container from 'components/Container'
 import CartProductList from 'components/CartProductList'
-import { StyledText } from './style'
-import { maskCardNumber } from 'utils/format.utils'
+import { StyledText, StyledSuccess, StyledSuccessText, StyledImage } from './style'
+import { format, form } from 'utils'
 
 const Success = props => {
-  const { inputs } = useForm('payment')
+  const { inputs } = form.useForm('payment')
   const { data } = useSelector(state => state.cart)
 
   useEffect(() => {
@@ -21,14 +21,19 @@ const Success = props => {
     <CheckoutPage
       noFooterButton
     >
+      <StyledSuccess>
+        <StyledImage alt='success' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACkAAAAeCAYAAACxHzfjAAABmUlEQVRYR82XzXHCMBBGP3USOoFKErrQLeSmLkIngUqgE2VWI3nkZWVJi8HShRnGNo/9ebs2GPR4iw8AvwCuZlBGeAuf2IaE9BZ/APbDQnobUvyVZfgwVCQFwKNxOA8D6S1OAL6zCP4YF77DEJDehvRSmtOZAIeA9DY0CDVKOmfjcMyts2kkowtvGdDFOBy4FreGnFwY0urk8tsMkrsQwM443KXhsglkSTWl6fd2SEE1wYVL4/mtkEsuHAJScOGDaorpjp76DCtRJezajanFhYuR9BbkKdrd6MxMr4Xi9+VrF4C7cdj1PNsIddKchpYfYkEourBakwLoJUaVPtVHXLscup85dbcASnBVPZT+Qa8Lq5FMFwgdqKpTrWqaZR5Bqdun9b2noWprl6Z2ijIX0kVTgbpfnK/048+4sDndgjr4tkyABPowxlrXrlUjmdUpBxXrlLlQpZrmmpQujGmk948k/Rloz9r1kkhmESVAAs1fN1Pa56+gCheqa7IQVSn96VK1V1eFjF3cVKea1Er3qPdJpptV5z0H/Qcrnp+5p2JMmAAAAABJRU5ErkJggg==' />
+      </StyledSuccess>
+      <StyledSuccessText>Compra efetuada com sucesso</StyledSuccessText>
+
       <Container title='Pagamento'>
-        <StyledText>{maskCardNumber(inputs.cardNumber)}</StyledText>
+        <StyledText>{format.maskCardNumber(inputs.cardNumber)}</StyledText>
         <StyledText>{inputs.name}</StyledText>
         <StyledText>{inputs.expirationDate}</StyledText>
       </Container>
       <CartProductList />
 
-    </CheckoutPage>
+    </CheckoutPage >
   )
 }
 
