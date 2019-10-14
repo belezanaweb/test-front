9bc59a173d4a7341e7e9d30fed5552ff53ad6452
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2019-10-14 03:25:10 -0300

    Adjusting style

--- src/components/Container/style.js
+++ src/components/Container/style.js
@@ -5,5 +5,6 @@ export const StyledContainer = styled.div`
   box-shadow: ${theme.boxShadow};
   border-radius: ${theme.borderRadius};
   background-color: ${theme.bgColor};
+  margin-bottom: 1.538rem;
   padding: 0.7692rem;
 `
