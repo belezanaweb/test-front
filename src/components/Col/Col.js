dcfddfbc85291b3276de184247083911c314c5cc
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2019-10-13 22:49:48 -0300

    fixing layout

--- src/components/Col/Col.js
+++ src/components/Col/Col.js
@@ -4,7 +4,7 @@ const Col = styled.div`
   display: inline-block;
   width: ${props => `calc(100%/(16/${props.col}))`};
   margin-left: ${props => `calc(100%/(16/${props.offset}))`};
-  margin-bottom: 1.923rem;
+  margin-bottom: 0.8462rem;
 `
 
 export default Col
