29843b4ca13f71ea31d7e3593c63ad7f72b8fc90
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2019-10-13 22:56:36 -0300

    Adding validation to inputs in Payments page

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -2,29 +2,80 @@ import React from 'react'
 import CheckoutPage from 'components/CheckoutPage'
 import Input from 'components/Input'
 import Col from 'components/Col'
+import { format } from 'utils'
+import { useForm } from 'utils/form.utils'
+import { pay } from 'services/cart.service'
+import { to } from 'utils/async.utils'
 
-const Payment = () => {
+const Payment = props => {
+  const { handleInputChange, inputs } = useForm('payment')
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const [error] = await to(pay())
+    if (!error) {
+      props.history.push('/confirm')
+    }
+  }
+
   return (
     <CheckoutPage
       title='Cartão de Crédito'
-      nextStep='/confirm'
       textButton='Finalizar pedido'
+      onSubmit={handleSubmit}
     >
       <Col col='16'>
-        <Input label='Número do cartão:' placeholder='____.____.____.____' />
+        <Input
+          label='Número do cartão:'
+          placeholder='____.____.____.____'
+          inputMode='numeric'
+          onChange={handleInputChange}
+          name='cardNumber'
+          maxLength={19}
+          pattern='.{19,}'
+          value={format.cardNumber(inputs.cardNumber)}
+          required
+        />
       </Col>
 
       <Col col='16'>
-        <Input label='Nome do Titular:' placeholder='Como no cartão' />
+        <Input
+          label='Nome do Titular:'
+          placeholder='Como no cartão'
+          name='name'
+          onChange={handleInputChange}
+          value={inputs.name}
+          required
+        />
       </Col>
 
       <Col col='8'>
-        <Input label='Validade (mês/ano):' placeholder='__/____' />
-      </Col>
-      <Col col='6' offset='1'>
-        <Input label='CVV:' placeholder='___' />
+        <Input
+          label='Validade (mês/ano):'
+          placeholder='__/____'
+          inputMode='numeric'
+          name='expirationDate'
+          maxLength={7}
+          pattern='.{7,}'
+          value={format.cardExpiration(inputs.expirationDate)}
+          onChange={handleInputChange}
+          required
+        />
       </Col>
 
+      <Col col='7' offset='1'>
+        <Input
+          label='CVV:'
+          placeholder='___'
+          inputMode='numeric'
+          maxLength={3}
+          pattern='.{3,}'
+          name='cvv'
+          onChange={handleInputChange}
+          value={inputs.cvv}
+          required
+        />
+      </Col>
 
     </CheckoutPage>
   )
