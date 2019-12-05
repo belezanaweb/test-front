f36ef20687ff7924e98a079162488272a1a97b36
Luiz "darknessxk" Felipe <memphis@darknessxk.com>
2019-12-05 20:29:45 -0300

    Api support

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -4,21 +4,19 @@ import './ProductList.css';
 
 class ProductList extends Component {
   render() {
-    // Default List is initialized as an empty Array
-    let productList = [];
-
-    if(this.props.products !== undefined) {
-      if(Array.isArray(this.props.products)) {
-        for(let i = 0; i < this.props.products.length; i ++) {
-          productList.push(<Product key={i} name={this.props.products[i]} price={(Math.random() * 999 + 1)} />)
-        }
-      }
-    }
+    let productList = this.props.products.items === undefined ? [] : this.props.products.items;
 
     return(<section className="product-list">
       <p className="product-list-title">Produtos</p>
       <div className="product-list-container">
-        {productList}
+        {productList.map((productInfo) => (
+          <Product
+            key={productInfo.product.sku}
+            name={productInfo.product.name}
+            image={productInfo.product.imageObjects[0].small}
+            price={productInfo.product.priceSpecification.price}
+          />
+        ))}
       </div>
     </section>);
   }
