cb74c8d796bfc3439ff755d0f894e8aa022eaca4
Luiz "darknessxk" Felipe <memphis@darknessxk.com>
2019-12-05 20:28:55 -0300

    Api added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import Header from './Components/Header';
 import ProductList from './Components/ProductList'
 
 class App extends Component {
+  state = {
+    apiResult: []
+  }
+
+  componentDidMount() {
+    fetch('http://www.mocky.io/v2/5b15c4923100004a006f3c07')
+      .then(response => response.json())
+      .then(info => {
+        this.setState({
+          apiResult: info
+        })
+      }).catch(alert);
+  }
+
   render() {
-
-    let productList = [];
-    for(let i = 0; i < 3; i ++) {
-      productList.push("L'Oreal Professionnel Expert Absolut Repair Cortex Lipidium");
-    }
-
     return (
       <div className="App">
         <Header selected="cart" />
-        <ProductList products={productList} />
+        <ProductList products={this.state.apiResult === undefined ? [] : this.state.apiResult} />
       </div>
     );
   }
