18bb7bddcd4758153a85ca284011604ba759f493
Luiz "darknessxk" Felipe <memphis@darknessxk.com>
2019-12-05 19:48:39 -0300

    Product CSS Align correction

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -5,7 +5,9 @@ import placeHolderImage from '../Assets/Images/placeholder64x64.png';
 class Product extends Component {
   render() {
     return(<div className="product-item">
-      <img className="product-item-image" src={this.props.image === undefined ? placeHolderImage : this.props.image} alt={this.props.name} />
+      <div className="product-item-image">
+        <img src={this.props.image === undefined ? placeHolderImage : this.props.image} alt={this.props.name} />
+      </div>
       <div className="product-item-info">
         <p className="product-item-name">{this.props.name === undefined ? 'Product without name' : this.props.name}</p>
         <span className="product-item-price">R$ {this.props.price === undefined ? '0,0' : this.props.price.toFixed(2).replace('.', ',')}</span>
