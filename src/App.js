f26044cfccc1c99b518ad687c8674bad28b603b8
Luiz "darknessxk" Felipe <memphis@darknessxk.com>
2019-12-05 15:28:07 -0300

    Header first version

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './SharedComponents.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <nav className="App-navbar">
+            <ul>
+              <li className="selected">sacola</li>
+              <li>pagamento</li>
+              <li>confirmação</li>
+            </ul>
+          </nav>
         </header>
+
+        <section>
+          Content
+        </section>
       </div>
     );
   }
