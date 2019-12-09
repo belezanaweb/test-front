5661cdd47d318123dcd434e2b94c43f9093a1f05
digitalbat <marcelo.bock@gmail.com>
2019-12-09 14:06:31 -0300

    Install Dependencies

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <img src={logo} className="App-logo" alt="logo" />
         </header>
       </div>
     );
