c02efed5be761afea69e655f61b15367adbcdf41
Franklin Javier <franklinjalves@gmail.com>
2020-01-23 13:02:35 -0300

    refactor: update dependencies

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+  </div>
+)
 
-export default App;
+export default App
