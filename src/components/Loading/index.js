370c713decdc330d3fd660c7b571cc295e626fbe
collumba <collumba@users.noreply.github.com>
2020-02-20 00:37:53 -0300

    [FEAT] created loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Loading = () => {
+    return (
+        <div className='loading-page'>Carregando...</div>
+    )
+}
+
+export default Loading;
