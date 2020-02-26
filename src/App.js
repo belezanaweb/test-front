a9a2d09f15606d11edb7a1bdd69c30e05effc5b3
Luan Pereira <pereiralv2010@gmail.com>
2020-02-26 19:00:53 -0300

    style: fix eslint errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 const App = () => (
   <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
+    <img src={logo} className="App-logo" alt="logo" />
   </div>
-)
+);
 
-export default App
+export default App;
