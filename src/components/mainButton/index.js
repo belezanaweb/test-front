2ce10a5a59afeaad63108fce436a83ab73e865d8
Natália Acedo <n.alves.acedo@gmail.com>
2020-07-15 10:43:39 -0300

    Added mainButton component

---
+++ src/components/mainButton/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import {StyledButton} from "./style"
+
+export default function MainButton (props) {
+    return(
+        <StyledButton> {props.text} </StyledButton> 
+    )
+}
---
+++ src/components/mainButton/style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components' 
+
+export const StyledButton = styled.button`
+    width: 100%;
+    height: 60px;
+    border-radius: 3px;
+    background-color: #FF6C00;
+    box-shadow: inset 0 -3px 0 0 #D45A00, 0 2px 4px 0 rgba(0,0,0,0.25); ;
+    color: #FFF;
+    font-size: 20px;
+    font-weight: 700;
+    letter-spacing: 0.5px;
+    text-transform: uppercase;
+
+    &:hover {
+    background-color: #D45A00;
+  }
+` 
