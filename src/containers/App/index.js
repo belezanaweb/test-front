9122cef56d14efcde07e6c79e800811491649986
Nauara Melo <nauaramelo@gmail.com>
2020-07-16 20:55:37 -0300

    Add app container

---
+++ src/containers/App/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import Router from "../Router";
+import { createBrowserHistory } from "history";
+import { createStore, applyMiddleware, compose } from "redux";
+import { generateReducers } from "../../reducers";
+import { routerMiddleware } from "connected-react-router";
+import TopBar from '../../components/TopBar'
+
+export const history = createBrowserHistory();
+
+const middlewares = [
+  applyMiddleware(routerMiddleware(history), thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
+];
+
+const store = createStore(generateReducers(history), compose(...middlewares));
+
+export const App = () => (
+  <Provider store={store}>
+    <TopBar />
+    <Router history={history} />
+  </Provider>
+);
+
+
+export default App;
