8c25de4bca28b8e0ac0cb84cef10b8d4cef084a6
Natália Acedo <n.alves.acedo@gmail.com>
2020-07-17 16:31:56 -0300

    Added action setCreditCard and inicialState cardInfo in reducers/cart.js

--- src/actions/cart.js
+++ src/actions/cart.js
@@ -2,6 +2,15 @@ import axios from "axios";
 
 const baseUrl = "http://www.mocky.io/v2/5b15c4923100004a006f3c07"
 
+export const setCreditCard = (cardInfo) => {
+    return {
+        type: "SET_CREDIT_CARD",
+        payload: {
+            cardInfo
+        }
+    }
+}
+
 export const setCart = (cart) => {
     return {
         type: "SET_CART",
--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,11 +1,18 @@
 const inicialState = {
-    cart: null
+    cart: null,
+    cardInfo: null
 }
 
 const carts = (state = inicialState, action) => {
     switch(action.type) {
         case "SET_CART": {
             return {...state, cart: action.payload.cart}
+        }
+
+        case "SET_CREDIT_CARD": {
+            console.log("REDUCER")
+            console.log("PAYLOAD",action.payload.cardInfo)
+            return {...state, cardInfo: action.payload.cardInfo}
         }
 
         default:
