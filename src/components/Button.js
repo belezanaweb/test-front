8a462fd8155f062df462ddbe17adae55119b6fec
Nauara Melo <nauaramelo@gmail.com>
2020-07-17 00:21:00 -0300

    styling adjustment

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,10 +8,12 @@ const Buttons = styled.button`
     background-color: #FF6C00;
     box-shadow: inset 0 -3px 0 0 #D45A00, 0 2px 4px 0 rgba(0,0,0,0.25);
     margin-top: 3.5vh;
+    :hover {
+        background-color: #D45A00
+    }
 `
 const Name = styled.div`
     height: 24px;
-    /* width: 339px; */
     color: #FFF;
     font-family: "Helvetica Neue";
     font-size: 20px;
@@ -25,7 +27,7 @@ const Button = (props) => {
     return (
         <Buttons>
             <Name>
-                {props.children}
+                {props.name}
             </Name>
         </Buttons>
     )
