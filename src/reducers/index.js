38d0b82a92d949fc39f492dd4304bd7f7598a417
Nauara Melo <nauaramelo@gmail.com>
2020-07-17 00:20:29 -0300

    inserting reducer cart in the index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
+import cart from './cart'
 
 export const generateReducers = history =>
   combineReducers({
-    router: connectRouter(history)
+    router: connectRouter(history),
+    cart
   });
