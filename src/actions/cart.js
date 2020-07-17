84f8c180f2de97c81a77075458a34853498f9ca4
Nauara Melo <nauaramelo@gmail.com>
2020-07-16 23:54:51 -0300

    endpoint integration

---
+++ src/actions/cart.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const baseUrl = "http://www.mocky.io/v2/5b15c4923100004a006f3c07"
+
+export const setCart = (cart) => {
+    return {
+        type: "SET_CART",
+        payload: {
+            cart
+        }
+    }
+}
+
+export const listCart = () => async (dispatch) => {
+    try {
+        const response = await axios.get(`${baseUrl}`)
+
+        dispatch(setCart(response.data))
+
+    } catch (error) {
+        alert('Por favor, tentar novamente.')
+    }
+}
