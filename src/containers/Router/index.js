87397bbb5c1f643577e6a1b14c34fb705ff0e9bc
Nauara Melo <nauaramelo@gmail.com>
2020-07-18 03:33:58 -0300

    Add payment route

--- src/containers/Router/index.js
+++ src/containers/Router/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import { ConnectedRouter } from "connected-react-router";
 import { Switch, Route } from "react-router-dom";
 import CartPage from "../Cart";
+import PaymentPage from "../Payment";
+import ConfirmationPage from "../Confirmation";
 
 export const routes = {
-  root: "/"
+  root: "/",
+  payment: "/payment",
 };
 
 function Router(props) {
   return (
     <ConnectedRouter history={props.history}>
       <Switch>
-         <Route exact path={routes.root} component={CartPage} />
+        <Route exact path={routes.root} component={CartPage} />
+        <Route exact path={routes.payment} component={PaymentPage} />
       </Switch>
     </ConnectedRouter>
   );
