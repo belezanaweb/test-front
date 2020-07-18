ace7ec821580b4bbee701b79d4042a7cecf96c75
Nauara Melo <nauaramelo@gmail.com>
2020-07-18 08:25:17 -0300

    Including reducer menu in the index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
-import cart from './cart'
+import cart from "./cart";
+import menu from "./menu";
 
 export const generateReducers = history =>
   combineReducers({
     router: connectRouter(history),
-    cart
+    cart,
+    menu
   });
