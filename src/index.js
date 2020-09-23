30299f213a0b714273e5b8595b8261d81d61b17b
Hugo Marcelo <hugo.silva@matera.com>
2020-09-23 14:59:03 -0300

    feat: config styled components and styled normalize

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React, { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+
+import './index.css';
+import App from './App';
+import theme from './styles/theme';
+import * as serviceWorker from './serviceWorker';
 
 const Strict = () => (
   <StrictMode>
-    <App />
+    <App></App>
+    <Normalize />
+    <ThemeProvider theme={theme}></ThemeProvider>
   </StrictMode>
-)
+);
 
-ReactDOM.render(<Strict />, document.getElementById('root'))
+ReactDOM.render(<Strict />, document.getElementById('root'));
 
-serviceWorker.unregister()
+serviceWorker.unregister();
---
+++ src/styles/theme.js
@@ -0,0 +1,23 @@
+const colors = {
+  primary: '#ff6c00',
+  primaryHover: '#d45a00',
+  white: '#fff',
+  gallery: '#eee',
+  silver: '#ccc'
+};
+
+const widths = {
+  container: '998px'
+};
+
+const fonts = {
+  primary: {
+    family: 'Helvetica Neue'
+  },
+  weights: {
+    regular: 400,
+    bold: 700
+  }
+};
+
+export default { colors, fonts, widths };
