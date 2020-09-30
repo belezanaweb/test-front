da91f0bfce3b01dc2ed2c8daedf00d292c7463d2
Milene Taborda <milenetaborda1@outlook.com>
2020-09-29 22:18:40 -0300

    feat: creation mock cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -2,25 +2,31 @@ import React from 'react';
 
 export default function Cart () {
   return (
-    <table>
-    <thead>
-      <tr>
-        <th>Produtos</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>L'Oréal</td>
-      </tr>
-      <tr>
-        <td>Good Girl</td>
-      </tr>
-    </tbody>
-    <tfoot>
-      <tr>
-        <td>Senscience</td>
-      </tr>
-    </tfoot>
-  </table>
+      <table>
+      <thead>
+        <tr>
+          <th>Produtos</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>L'Oréal</td>
+        </tr>
+        <tr>
+          <td>Good Girl</td>
+        </tr>
+      </tbody>
+
+      <tfoot>
+        <tr>
+          <td>produtos</td>
+          <td>624,80</td>
+        </tr>
+        <tr>
+          <td>frete</td>
+          <td>5,90</td>
+        </tr>
+      </tfoot>
+    </table>
   )
 }
