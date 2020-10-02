533bf8c4745d942abd46843e02791dc124ab9c04
Igor Souza <igor.souza.webmaster@gmail.com>
2020-10-02 16:26:03 -0300

    feat: add typescript

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const App = () => (
-  <div className="App" />
-)
-
-export default App
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const Strict = () => (
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+ReactDOM.render(<Strict />, document.getElementById('root'));
