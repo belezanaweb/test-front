8abd093ae2d40e710a423c3b1f445fb47f3d09cf
skempfer <shana.kempfer88@gmail.com>
2020-10-02 00:30:54 -0300

    feat: tests in the payment method page file

--- src/pages/PaymentMethodPage/paymentMethodPage.test.js
+++ src/pages/PaymentMethodPage/paymentMethodPage.test.js
@@ -1,17 +1,17 @@
 import React from "react";
-// import { shallow } from "enzyme";
-// import PaymentMethodPage from "./index.js";
+import { shallow } from "enzyme";
+import PaymentMethodPage from "./index.js";
 
-// const renderedPage = shallow(<PaymentMethodPage/>)
+const renderedPage = shallow(<PaymentMethodPage/>)
 
-// describe('PaymentMethodPage', () => {
-//     it('o payment method page está sendo corretamente renderizado', () => {                     
-//         expect(renderedPage.exists()).toBeTruthy();
-//         expect(renderedPage).toMatchSnapshot();
-//     });
-//     it('O Botão de confirmação está renderizando e clicando', () => {
-//         expect(renderedPage.find("ButtonConfirmation")).toBeTruthy();
-//     });
+describe('PaymentMethodPage', () => {
+    it('o payment method page está sendo corretamente renderizado', () => {                     
+        expect(renderedPage.exists()).toBeTruthy();
+        expect(renderedPage).toMatchSnapshot();
+    });
+    it('O Botão de confirmação está renderizando e clicando', () => {
+        expect(renderedPage.find("ButtonConfirmation")).toBeTruthy();
+    });
 
 
-// })
+})
