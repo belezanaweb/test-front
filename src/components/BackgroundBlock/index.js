2044d0f8c82ab74e1b0f10f6633c13c227384ad2
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-10-01 21:40:43 -0300

    Created BackgroundBlock component

--- src/components/BackgroundBlock/index.js
+++ src/components/BackgroundBlock/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { BoxWrapper, Content, Title } from './style'
 
-const BackgroundBlock = () => (
-    <div></div>
+const BackgroundBlock = ({ title, boxContent }) => {
+
+  return (
+    <BoxWrapper>
+      <Title>{title}</Title>
+      <Content>{boxContent}</Content>
+    </BoxWrapper>
   )
-  
-  export default BackgroundBlock
+}
+
+export default BackgroundBlock
---
+++ src/components/BackgroundBlock/style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const BoxWrapper = styled.div`
+  width: 100%;
+  display: grid;
+  gap: 5px;
+`
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 600px;
+  padding: 10px;
+  background-color: white;
+  box-shadow: 1px 1px 5px 0 rgba(0,0,29,0.22);
+  border-radius: 3px;
+`
+
+export const Title = styled.div`
+  padding: 0 10px;
+  color: #999;
+  font-weight: bold;
+`
