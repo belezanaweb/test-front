70680ca36689fcc4e7c95d793abd56e502d30cea
Igor Souza <igor.souza.webmaster@gmail.com>
2020-10-02 18:50:40 -0300

    chore: redirect to cart in root path

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Cart from 'pages/Cart';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
+      <Route exact path="/">
+        <Redirect to="/cart" />
+      </Route>
       <Route exact path="/cart" component={Cart} />
     </Switch>
   </BrowserRouter>
