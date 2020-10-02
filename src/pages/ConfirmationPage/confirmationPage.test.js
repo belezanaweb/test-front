2656121b3a1d6e3fc41a547284090a147791200e
skempfer <shana.kempfer88@gmail.com>
2020-10-02 00:32:42 -0300

    feat: tests on the confirmation page file

--- src/pages/ConfirmationPage/confirmationPage.test.js
+++ src/pages/ConfirmationPage/confirmationPage.test.js
@@ -1,25 +1,25 @@
-// import React from "react";
-// import { shallow } from "enzyme";
-// import ConfirmationPage from "./index.js";
+import React from "react";
+import { shallow } from "enzyme";
+import ConfirmationPage from "./index.js";
 
-// const mocky = {
-//     "number": "1234.1234.1234.1234",
-//     "name": "Ada Lovelace",
-//     "date": "12/2027"
-// }
-// const cardNumber = mocky.number
-// const userName = mocky.name
-// const expirationDate = mocky.date
+const mocky = {
+    "number": "1234.1234.1234.1234",
+    "name": "Ada Lovelace",
+    "date": "12/2027"
+}
+const cardNumber = mocky.number
+const userName = mocky.name
+const expirationDate = mocky.date
 
-// //const renderedPage = shallow(< ConfirmationPage />)
+//const renderedPage = shallow(< ConfirmationPage />)
 
-// describe('ConfirmationPage', () => {
-//     it('o confirmation page está sendo corretamente renderizado', () => {                     
-//         //expect(renderedPage.exists()).toBeTruthy();
-//         //expect(renderedPage).toMatchSnapshot();
+describe('ConfirmationPage', () => {
+    it('o confirmation page está sendo corretamente renderizado', () => {                     
+        //expect(renderedPage.exists()).toBeTruthy();
+        //expect(renderedPage).toMatchSnapshot();
 
-//         //PRECISA FAZER O MOCK DO CARTÃO DE CRÉDITO
-//     });
+        //PRECISA FAZER O MOCK DO CARTÃO DE CRÉDITO
+    });
   
 
-// })
+})
