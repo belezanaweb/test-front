3b2c14dc89e59c6950a0278a890abd47d5b6819d
skempfer <shana.kempfer88@gmail.com>
2020-10-02 00:29:06 -0300

    feat: tests on the cart page file

--- src/pages/CartPage/cartPage.test.js
+++ src/pages/CartPage/cartPage.test.js
@@ -1,22 +1,22 @@
-// import React from "react";
-// import { shallow } from "enzyme";
-// import CartPage from "./index.js";
+import React from "react";
+import { shallow } from "enzyme";
+import CartPage from "./index.js";
 
-// const renderedPage = shallow(<CartPage/>)
+const renderedPage = shallow(<CartPage/>)
 
-// describe('CartPage', () => {
-//     it('o Cart page está sendo corretamente renderizado', () => {                     
-//         expect(renderedPage.exists()).toBeTruthy();
-//         expect(renderedPage).toMatchSnapshot();
-//     });
-//     it('o Products container renderizado corretamente', () => {
-//         expect(renderedPage.find("ProductsContainer")).toBeTruthy();
-//     });
-//     it('O Botão de confirmação está renderizando e clicando', () => {
-//         expect(renderedPage.find("ButtonConfirmation")).toBeTruthy();
-//     });
-//     it('o botão de confirmação está recebendo click', () => {
-//         expect(renderedPage.find("ButtonConfirmation"));
-//     });
+describe('CartPage', () => {
+    it('o Cart page está sendo corretamente renderizado', () => {                     
+        expect(renderedPage.exists()).toBeTruthy();
+        expect(renderedPage).toMatchSnapshot();
+    });
+    it('o Products container renderizado corretamente', () => {
+        expect(renderedPage.find("ProductsContainer")).toBeTruthy();
+    });
+    it('O Botão de confirmação está renderizando e clicando', () => {
+        expect(renderedPage.find("ButtonConfirmation")).toBeTruthy();
+    });
+    it('o botão de confirmação está recebendo click', () => {
+        expect(renderedPage.find("ButtonConfirmation"));
+    });
 
-// })
+})
