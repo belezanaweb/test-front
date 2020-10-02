9975e85ad852e876e41117437b8f78346b978117
Igor Souza <igor.souza.webmaster@gmail.com>
2020-10-02 16:22:20 -0300

    chore: prepare basic project structure for development

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 const App = () => (
-  <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-  </div>
+  <div className="App" />
 )
 
 export default App
