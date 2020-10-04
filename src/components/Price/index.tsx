3c4867ff5b594ba6bcf9eca43b2757fafa1e9a05
Igor Souza <igor.souza.webmaster@gmail.com>
2020-10-04 19:07:29 -0300

    feat: render fetched price data

--- src/components/Price/index.tsx
+++ src/components/Price/index.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { ResumeContext } from 'context/resume';
+
+import { usePrice } from 'hooks/price';
 
 import { Container, ValueInfo } from './styles';
 
 const Price: React.FC = () => {
+  const { cartResume } = useContext(ResumeContext);
+  const { formatPrice } = usePrice();
+
   return (
     <Container>
-      <ValueInfo>
-        <span>PRODUTOS</span>
-        <span>R$ 624,80</span>
-      </ValueInfo>
-      <ValueInfo style={{ marginTop: 9 }}>
-        <span>FRETE</span>
-        <span>R$ 5,30</span>
-      </ValueInfo>
-      <ValueInfo style={{ marginTop: 8, color: 'var(--color-orange)' }}>
-        <span>DESCONTO</span>
-        <span>- R$ 30,00</span>
-      </ValueInfo>
-      <ValueInfo style={{ marginTop: 16, fontWeight: 700 }}>
-        <span>TOTAL</span>
-        <span>R$ 600,10</span>
-      </ValueInfo>
+      {cartResume && (
+        <>
+          <ValueInfo>
+            <span>PRODUTOS</span>
+              <span>R$ {formatPrice(cartResume.subTotal)}</span>
+          </ValueInfo>
+          <ValueInfo style={{ marginTop: 9 }}>
+            <span>FRETE</span>
+            <span>R$ {formatPrice(cartResume.shippingTotal)}</span>
+          </ValueInfo>
+          <ValueInfo style={{ marginTop: 8, color: 'var(--color-orange)' }}>
+            <span>DESCONTO</span>
+            <span>- R$ {formatPrice(cartResume.discount)}</span>
+          </ValueInfo>
+          <ValueInfo style={{ marginTop: 16, fontWeight: 700 }}>
+            <span>TOTAL</span>
+            <span>R$ {formatPrice(cartResume.total)}</span>
+          </ValueInfo>
+        </>
+      )}
     </Container>
   );
 };
