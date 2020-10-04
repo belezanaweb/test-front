2635fcfab86ffc3e2974272288ec9e1f952eb662
Melissa Tammy <mel.tammy2@gmail.com>
2020-10-04 19:00:28 -0300

    Add: PaymentData component

---
+++ src/components/PaymentData/index.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { Container } from './styles';
+import CardContext from '../../contexts/CardContext';
+
+function PaymentData(props) {
+    const cardContext = useContext(CardContext);
+
+    return (
+        <Container>
+            <p>{cardContext.card.number}</p>
+            <p>{cardContext.card.name}</p>
+            <p>{cardContext.card.validity}</p>
+        </Container>
+    );
+}
+export default PaymentData;
---
+++ src/components/PaymentData/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: fit-content;
+  width: 100%;
+  padding: 3vw;
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+  background-color: #FFF;
+  border-radius: 3px;
+  box-shadow: 1px 1px 5px 0 rgba(0,0,29,0.22);
+`
+
