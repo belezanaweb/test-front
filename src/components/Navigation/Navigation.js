d20cbc307e6ccb4deb77b26466e0260a07cf892f
Francisco Kahil <codexico@gmail.com>
2020-10-05 22:16:58 -0300

    Add styles to Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,37 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components/macro'
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  border-radius: 3px;
+  background-color: #fff;
+  box-shadow: 1px 1px 5px 0 rgba(0, 0, 29, 0.22);
+  padding: 4px 6px 4px 10px;
+  margin-bottom: 10px;
+`
+
+const StyledLink = styled(NavLink)`
+  color: #ccc;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-size: 13px;
+  font-weight: 700;
+  text-transform: uppercase;
+  padding: 10px 10px 7px 10px;
+  text-decoration: none;
+  &.active {
+    color: #ff7800;
+  }
+`
 
 function Navigation() {
   return (
-    <nav>
-      <Link to="/">Sacola</Link>
-      <Link to="/pagamento">Pagamento</Link>
-      <Link to="/confirmacao">Confirmação</Link>
-    </nav>
+    <StyledNav>
+      <StyledLink to="/">Sacola</StyledLink>
+      <StyledLink to="/pagamento">Pagamento</StyledLink>
+      <StyledLink to="/confirmacao">Confirmação</StyledLink>
+    </StyledNav>
   )
 }
 
--- src/components/Navigation/Navigation.test.js
+++ src/components/Navigation/Navigation.test.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 
 import Navigation from './Navigation'
 
 describe('Navigation Component', () => {
   test('renders links', async () => {
-    const { asFragment } = render(<Navigation />)
+    const { asFragment } = render(<Navigation />, { wrapper: MemoryRouter })
 
     expect(asFragment()).toMatchSnapshot()
   })
