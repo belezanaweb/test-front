ce946c7594e738231e26e46596a140380a83801c
Igor Souza <igor.souza.webmaster@gmail.com>
2020-10-05 22:00:02 -0300

    style: improve desktop experience

--- src/components/Price/styles.ts
+++ src/components/Price/styles.ts
@@ -5,6 +5,13 @@ export const Container = styled.div`
   border: 1px solid #CCC;
   padding: 15px 13px 13px 14px;
   margin: 20px 0 20px 0;
+
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+    height: 92%;
+  }
 `;
 
 export const ValueInfo = styled.div`
@@ -13,5 +20,11 @@ export const ValueInfo = styled.div`
 
   span {
     font-size: 14px;
+  }
+
+  @media screen and (min-width: 768px) {
+    &:last-child {
+      margin-top: auto !important;
+    }
   }
 `;
