288e7f9ef2bc3ac43e1be931883cdd0dfcb58209
Diego <di__e__go@hotmail.com>
2020-10-07 01:01:04 -0300

    function handleSubmit created on PaymentPage
    sh: q: command not found

--- src/components/PaymentPage/index.js
+++ src/components/PaymentPage/index.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
-import ConfirmPage from '../ConfirmPage'
+import { useHistory } from 'react-router-dom'
 
 function PaymentPage() {
   const infoCart = JSON.parse(window.localStorage.getItem('infoCart'))
+  const history = useHistory()
+
   const [cardNumber, setCardNumber] = useState()
   const [userName, setUserName] = useState()
   const [cardValidate, setCardValidate] = useState()
   const [cvv, setCvv] = useState()
+
+  const infoUser = {
+    number: cardNumber,
+    user: userName,
+    card: cardValidate,
+    cvv: cvv
+  }
 
   const handleInputCardNumber = (event) => {
     setCardNumber(event.target.value)
@@ -24,11 +33,14 @@ function PaymentPage() {
     setCvv(event.target.value)
   }
 
-  const infoUser = {
-    number: cardNumber,
-    user: userName,
-    card: cardValidate,
-    cvv: cvv
+  const goToConfirm = () => {
+    history.push('/confirm')
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    localStorage.setItem('infoUser', JSON.stringify(infoUser))
+    goToConfirm()
   }
 
   console.log(infoUser)
@@ -37,7 +49,7 @@ function PaymentPage() {
     <div>
       <div>CARTÃO DE CRÉDITO</div>
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           Numero do Cartão:
           <input
             type="text"
@@ -70,14 +82,15 @@ function PaymentPage() {
             value={cvv}
             onChange={handleInputCvv}
           ></input>
+          <hr></hr>
+          <div>
+            <p>Produtos: {infoCart.subTotal.toFixed(2)}</p>
+            <p>Frete: {infoCart.shippingTotal.toFixed(2)}</p>
+            <p>Desconto: {infoCart.discount.toFixed(2)}</p>
+            <p>Total: {infoCart.total.toFixed(2)}</p>
+          </div>
+          <button type="submit">FINALIZAR O PEDIDO</button>
         </form>
-      </div>
-      <hr></hr>
-      <div>
-        <p>Produtos: {infoCart.subTotal.toFixed(2)}</p>
-        <p>Frete: {infoCart.shippingTotal.toFixed(2)}</p>
-        <p>Desconto: {infoCart.discount.toFixed(2)}</p>
-        <p>Total: {infoCart.total.toFixed(2)}</p>
       </div>
     </div>
   )
