aecc57f971f12eb3a0ade9caa1ab1accd46a903a
Nauara Melo <nauaramelo@gmail.com>
2020-10-09 03:54:58 -0300

    refactor: refactor app and eslint

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = () => (
-  <div className="App">
-    <img className="App-logo" alt="logo" />
-  </div>
-);
-
-export default App;
---
+++ src/styles/global.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Global = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+  }
+  :root {
+    background-color: #EEE;
+    }
+`;
+
+export default Global;
