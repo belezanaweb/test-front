f9f3d027fdc3ecc3a8a126732f771c702ce84be7
Nauara Melo <nauaramelo@gmail.com>
2020-10-09 06:03:03 -0300

    refactor: add new props to padding

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 import { Title, Box, Container } from './style';
 
 const Card = (props) => {
-  const { title, heightBox, children } = props;
+  const {
+    title, heightBox, children, paddingBox,
+  } = props;
 
   return (
     <Container>
       <Title>
         {title}
       </Title>
-      <Box height={heightBox}>
+      <Box height={heightBox} padding={paddingBox}>
         {children}
       </Box>
     </Container>
@@ -21,6 +23,11 @@ Card.propTypes = {
   title: PropTypes.string.isRequired,
   heightBox: PropTypes.number.isRequired,
   children: PropTypes.node.isRequired,
+  paddingBox: PropTypes.string,
+};
+
+Card.defaultProps = {
+  paddingBox: '',
 };
 
 export default Card;
