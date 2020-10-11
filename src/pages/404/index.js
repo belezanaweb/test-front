693808032d654e0d2a5daa39e470b026de95b188
thiago <thiago.w3c@gmail.com>
2020-10-11 15:43:51 -0300

    notFound page route add

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { ReactComponent as Logo } from 'assets/img/logo.svg';
+import { Link} from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    flexDirection: 'row',
     height: '100vh',
     '& h1':{
-        fontSize: '3rem'
+        fontSize: '3rem',
+        marginLeft: theme.spacing(4)
     }
   },
 }));
 
 export default function StickyFooter() {
   const classes = useStyles();
-
   return (
     <div className={classes.root}>
+        <Link to="/" title="Voltar para home"><Logo/></Link>
         <Typography component="h1">
           Página não encontrada !
         </Typography>
