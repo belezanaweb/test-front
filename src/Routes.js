ba106bfbc2c4c8bf38d85ef687a364e49f7a5468
thiago <thiago.w3c@gmail.com>
2020-10-11 15:18:14 -0300

    created other routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import {BrowserRouter , Switch , Route} from 'react-router-dom'
-import Home from './ShoppingCart'
+import ShoppingCart from './ShoppingCart'
+import Payment from './ShoppingCart'
+import Confirmation from './ShoppingCart'
 
 const Routes = () =>{
     return(
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/" component={ShoppingCart}></Route>
+                <Route exact path="/pagamento" component={Payment}></Route>
+                <Route exact path="/confirmacao" component={Confirmation}></Route>
             </Switch>
         </BrowserRouter>
     )
