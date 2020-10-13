9be6617bbf3c9658c877b8f7e84ab217e6499e1f
MatheusDardengo <mds.1996@hotmail.com>
2020-10-13 01:07:59 -0300

    Add App.js it contains the code to persist the json file and all the Route parts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import Header from './Header';
+import Sacola from './Sacola';
+import Pagamento from './Pagamento';
+import Confirmacao from './Confirmacao';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const App = () => (
-  <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-  </div>
-)
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { data: [] };
+  }
 
-export default App
+  componentDidMount() {
+    try {
+      fetch('http://www.mocky.io/v2/5b15c4923100004a006f3c07')
+        .then((res) => res.json())
+        .then((json) => this.setState({ data: json }));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="sacola" element={<Sacola />} />
+            <Route path="pagamento" element={<Pagamento />} />
+            <Route path="confirmacao" element={<Confirmacao />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
