590c3f1045ca15caf6a97e1e99328555d6d185cf
Rodrigo Godoy <contato@rodrigogodoy.net>
2020-10-20 13:17:44 -0300

    confirmacao finished

--- src/screens/Confirmacao/index.js
+++ src/screens/Confirmacao/index.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react'
 
+import './Confirmacao.scss'
+
 // components
 import Title from '../../components/Title'
 import ProductList from '../../components/ProductList'
 import PricesList from '../../components/PricesList'
 
-//API
+// API
 import Checkout from '../../api/checkout'
+
+// images
+import successIcon from '../../assets/images/icons/success-icon.svg'
 
 const checkout = new Checkout()
 
@@ -27,6 +32,29 @@ const Confirmacao = () => {
     <div className="container">
 
       <div className="container__content">
+
+        <div className="success">
+          <img className="success__icon" src={ successIcon } alt="Sucesso" />
+          <h1 className="success__title">compra efetuada com sucesso</h1>
+        </div>
+
+        <Title title="Pagamento" />
+
+        <div className="card">
+
+          <div className="card-information">
+            <span className="card-information__text">
+              { window.localStorage.getItem('cardNumber') }
+            </span>
+            <span className="card-information__text">
+              { window.localStorage.getItem('name') }
+            </span>
+            <span className="card-information__text">
+              { window.localStorage.getItem('date') }
+            </span>
+          </div>
+
+        </div>
 
         <Title title="Produtos" />
 
