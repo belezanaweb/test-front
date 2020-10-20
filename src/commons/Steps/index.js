1eb10cf2941b48c8aa53b8a92e619d15019f5861
Rodrigo Godoy <contato@rodrigogodoy.net>
2020-10-20 13:24:47 -0300

    steps actives

--- src/commons/Steps/index.js
+++ src/commons/Steps/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 import './Steps.scss'
 
 const Steps = () => {
+
+  const location = useLocation();
+  console.log(location.pathname);
   return (
     <header className='steps'>
-      <span className='steps__item'>Sacola</span>
-      <span className='steps__item'>Pagamento</span>
-      <span className='steps__item'>Confirmação</span>
+      <span className={ location.pathname === '/' ? 'steps__item steps__item--active' : 'steps__item' } >Sacola</span>
+      <span className={ location.pathname === '/pagamento' ? 'steps__item steps__item--active' : 'steps__item' }>Pagamento</span>
+      <span className={ location.pathname === '/confirmacao' ? 'steps__item steps__item--active' : 'steps__item' }>Confirmação</span>
     </header>
   )
 }
