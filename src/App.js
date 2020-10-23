1c6a3af85718ee33dec37002feb3b0108a91b74a
Franklin Javier <franklinjalves@gmail.com>
2020-10-23 00:49:01 -0300

    chore: husky, lint-staged and prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
-const App = () => (
-  <div className="App">
-    <img src={logo} className="App-logo" alt="logo" />
-    Beleza Na Web
-  </div>
-)
+const App = () => <div className="App">Boa sorte! 🚀</div>
 
 export default App
