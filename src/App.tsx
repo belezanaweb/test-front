6242b78bdb275c28c33b52ca55ce84b1d645c5aa
Rodrigo Pereira <rodrigo.pereira@ingresso.com>
2020-10-27 17:58:41 -0300

    feat: add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
 
 import Routes from './routes'
 import store from './redux/store'
+import theme from './theme'
 
 function App() {
   return (
     <Provider store={store}>
       <React.StrictMode>
-        <Router>
-          <Routes />
-        </Router>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <Routes />
+          </Router>
+        </ThemeProvider>
       </React.StrictMode>
     </Provider>
   )
---
+++ src/theme/index.ts
@@ -0,0 +1,13 @@
+export default {
+  font: 'Helvetica Neue, Helvetica, Arial, sans-serif',
+  colors: {
+    primary: '#FF6C00',
+    primaryLighten: '#FF7800',
+    primaryDarken: '#D45A00',
+    secondary: '#212122',
+    secondaryLighten: '#999999',
+    auxiliary: '#E7E7E7',
+    auxiliaryLighten: '#EEEEEE',
+    auxiliaryDarken: '#CCCCCC'
+  }
+}
