547ed9abb10fa60d9c1dd8cdc80306282615ff70
Rodrigo Pereira <rodrigo.pereira@ingresso.com>
2020-10-27 04:13:39 -0300

    feat: change cart and payment pages to test redux actions

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,3 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-export const Cart = () => <h1>Cart</h1>
+import { RootState } from '../../redux/store'
+import { fetchCartProducts } from '../../redux/ducks/cart'
+
+export const Cart = () => {
+  const dispatch = useDispatch()
+  const cart = useSelector((state: RootState) => state.cart)
+
+  useEffect(() => {
+    dispatch(fetchCartProducts())
+  }, [dispatch])
+
+  return (
+    <>
+      <h1>Cart</h1>
+      {cart?.data?.items?.map((item) => {
+        return <p key={item.product.sku}>{item.product.name}</p>
+      })}
+    </>
+  )
+}
--- src/pages/Payment/Payment.tsx
+++ src/pages/Payment/Payment.tsx
@@ -1,3 +1,26 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
-export const Payment = () => <h1>Payment</h1>
+import { setData } from '../../redux/ducks/creditCard'
+
+export const Payment = () => {
+  const dispatch = useDispatch()
+
+  const handleSubmit = () => {
+    dispatch(
+      setData({
+        holder: 'Fulano de Tal',
+        number: 92737321837183,
+        expirationDate: new Date().getTime(),
+        cvv: 234
+      })
+    )
+  }
+
+  return (
+    <>
+      <h1>Payment</h1>
+      <button onClick={() => handleSubmit()}>Finalizar pedido</button>
+    </>
+  )
+}
