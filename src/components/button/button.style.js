3e9a5893bb8bd19a8dac57a7e10118ef210251ab
vinicius aragao <disanti2010@gmail.com>
2020-10-29 20:03:23 -0300

    feat: font helvetica neue
    Adding font Helvetica Neue on project

--- src/components/button/button.style.js
+++ src/components/button/button.style.js
@@ -6,6 +6,7 @@ const ButtonContainer = styled.button`
   background-color: #ff6c00;
   box-shadow: inset 0 -3px 0 0 #d45a00, 0 2px 4px 0 rgba(0, 0, 0, 0.25);
   padding: 18px;
+  margin-bottom: 12px;
   border: none;
   outline: none;
   transition: 0.3s background-color;
--- src/core/App.test.js
+++ src/core/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import store from '../store'
 import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/beleza/i)
-  expect(linkElement).toBeInTheDocument()
+test('renders breadcrumb correctly', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+  const breadCrumb = getByText('sacola')
+
+  expect(breadCrumb).toBeInTheDocument()
 })
