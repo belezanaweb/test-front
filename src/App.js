2753662eb03409828206cbd5706af42b1e100ec4
Phil Gruneich <git@philgruneich.com>
2020-11-01 16:14:55 -0300

    feat: ajustes gerais e assets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React from 'react'
-import './App.css'
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { PaymentStore } from './store/PaymentStore'
+import { CartStore } from './store/CartStore'
 
-const App = () => <div className="App">Boa sorte! 🚀</div>
+const Cart = lazy(() => import('./containers/Cart/Cart'))
+const Payment = lazy(() => import('./containers/Payment/Payment'))
+const Confirmation = lazy(() => import('./containers/Confirmation/Confirmation'))
+
+const App = () => (
+  <PaymentStore>
+    <CartStore>
+      <Router>
+        <Suspense fallback={<div>Carregando...</div>}>
+          <Switch>
+            <Route path="/pagamento" component={Payment} />
+            <Route path="/confirmacao" component={Confirmation} />
+            <Route exact path="/" component={Cart} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </CartStore>
+  </PaymentStore>
+)
 
 export default App
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,5 @@
 import '@testing-library/jest-dom/extend-expect'
+import 'jest-styled-components'
+import fetchMock from 'jest-fetch-mock'
+
+fetchMock.enableMocks()
