eee16d7f32bebb6102e8468f92aae853a032afa4
hemersonvianna <hemerson.lourenco@gmail.com>
2020-12-16 10:50:42 -0300

    feat: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,28 @@
+module.exports = {
+  stories: ['../src/components/**/*.stories.tsx'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [
+          ['react-app', {
+            flow: false,
+            typescript: true
+          }]
+        ],
+      },
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-viewport',
+    '@storybook/addon-knobs',
+    '@storybook/addon-actions'
+  ]
+}
---
+++ .storybook/preview.js
@@ -0,0 +1,77 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { ThemeProvider } from 'styled-components';
+import { StoryContext, StoryGetter, StoryWrapper } from '@storybook/addons';
+
+const globals = {
+  colors: {
+    dark: '#333',
+    light: '#fff'
+  }
+}
+
+export const darkTheme = {
+  name: 'dark',
+  background: globals.colors.black,
+  globals
+}
+
+export const lightTheme = {
+  name: 'light',
+  background: globals.colors.white,
+  globals
+}
+
+const getTheme = (theme) => {
+  if (theme == 'dark') {
+    return darkTheme;
+  } else {
+    return lightTheme;
+  }
+}
+
+export const globalTypes = {
+  theme: {
+    name: 'Tema',
+    description: 'Tema global para os componentes',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: [
+        { value: 'dark', title: 'dark', icon: 'circle' },
+        { value: 'light', title: 'light' }
+      ]
+    },
+  },
+};
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: globals.colors.light,
+      },
+      {
+        name: 'dark',
+        value: globals.colors.dark,
+      }
+    ]
+  },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+    defaultViewport: 'iphone5',
+  }
+}
+
+const withThemeProvider = (Story,context) => {
+  const theme = getTheme(context.globals.theme);
+  return (
+    <ThemeProvider theme={theme}>
+      <Story {...context} />
+    </ThemeProvider>
+  )
+}
+
+export const decorators = [withThemeProvider];
