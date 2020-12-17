b6a2e84338937470dad2e48099e06db49c0cd1a9
hemersonvianna <hemerson.lourenco@gmail.com>
2020-12-17 11:06:45 -0300

    feat: update syntax in button component

--- src/shared/components/button/button.stories.tsx
+++ src/shared/components/button/button.stories.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story } from '@storybook/react/types-6-0'
+import { Story } from '@storybook/react/types-6-0';
 
-import { IButtonProps } from './button.interface'
-import { Button } from './button.component'
+import { IButtonProps } from './button.interface';
+import { Button } from './button.component';
 
 export default {
   title: 'Button',
-  component: Button
-}
+  component: Button,
+};
 
-const Template: Story<IButtonProps> = (args) => <Button {...args} />
+const Template: Story<IButtonProps> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({})
+export const Primary = Template.bind({});
 Primary.args = {
-  children: 'Button'
-}
+  children: 'Button',
+};
 
-export const Large = Template.bind({})
+export const Large = Template.bind({});
 Large.args = {
   size: 'large',
-  children: 'Button'
-}
+  children: 'Button',
+};
