154d299b212384e178a2ce071ea9461352b50bad
Carlos Henrique Rabelo de Oliveira <carloshenriquerabelodeoliveira@MBP-de-Carlos.box>
2020-12-19 10:31:01 -0300

    chore: update deps

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/beleza/i)
+  const linkElement = getByText(/Boa sorte!/i)
   expect(linkElement).toBeInTheDocument()
 })
