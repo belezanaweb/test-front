bd48e2928856d7db444daf9771b89363c2c06436
brunoamadormdc <brunoamadormdc@gmail.com>
2021-01-15 14:28:36 -0300

    Removing unused files

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
 import Reducer from './Products/index'
 import FormReducer from './Form/index'
-import StepReducer from './Steps/index'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
 export default combineReducers({
   Products: Reducer,
   toastr: toastrReducer,
-  FormReducer,
-  StepReducer
+  FormReducer
 })
