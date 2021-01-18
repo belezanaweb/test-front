7bdf85a8375f5226ed0e8d01bd12d5bee0f02610
Diego Martins <diego.santos@ingresso.com>
2021-01-18 00:46:49 -0300

    style(input): creates Input styled component

---
+++ src/ui/components/Input/styled.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components/macro'
+import InputMask from 'react-input-mask'
+
+export const Input = styled.input<{error?: boolean}>`
+  background-color: ${({ theme }) => theme.colors.baseLight};
+  border-radius: 0.3rem;
+  border: 0.1rem solid ${({ theme }) => theme.colors.secondary};
+  box-shadow: inset 0 0.1rem 0.2rem 0 rgba(0, 0, 0, 0.2);
+  padding: 1rem;
+  color: ${({ theme }) => theme.colors.baseDark};
+  transition: border 0.2s ease-out;
+  width: 100%;
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.secondaryLight};
+  }
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.colors.focus};
+    outline: 0;
+  }
+  ${({ theme, error }) => error && `border: 1px solid ${theme.colors.error};`}
+`
+
+export const MaskedInput = styled(InputMask)<{error?: boolean}>`
+  background-color: ${({ theme }) => theme.colors.baseLight};
+  border-radius: 0.3rem;
+  border: 0.1rem solid ${({ theme }) => theme.colors.secondary};
+  box-shadow: inset 0 0.1rem 0.2rem 0 rgba(0, 0, 0, 0.2);
+  padding: 1rem;
+  color: ${({ theme }) => theme.colors.baseDark};
+  transition: border 0.2s ease-out;
+  width: 100%;
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.secondaryLight};
+  }
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.colors.focus};
+    outline: 0;
+  }
+  ${({ theme, error }) => error && `border: 1px solid ${theme.colors.error};`}
+`
