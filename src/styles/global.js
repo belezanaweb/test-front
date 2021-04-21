fc35566a1ea6f675040cffed9076f0003342eb9a
felpgoes <felipe.goess@hotmail.com>
2021-04-21 18:34:18 -0300

    feat: setting global style

---
+++ src/styles/global.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+import theme from "./theme";
+
+export default createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+}
+
+body{
+    background: ${theme.mainBackgroundColor};
+    font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;
+    -webkit-font-smoothing: antialiased;
+    min-height: 100%;
+
+    @media (min-width: 1000px){
+        min-height: 100vh;
+    }
+}
+
+`;
---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+  mainColor: "#FF7800",
+  mainBackgroundColor: "#EEE",
+  containerBackground: "#FFF",
+  btnBackgroundColor: "#FF6C00",
+  btnTextColor: "#FFF",
+  btnShadowColor: "#D45A00",
+  inputDefaultBorderColor: "#E7E7E7",
+  inputDefaultPlaceholderColor: "#E0E7EE",
+  inputDefaultValueColor: "#212121",
+  inputFocusBorderColor: "#A43287",
+  inputErrorDefaultColor: "#F30",
+  textTitleColor: "#212122",
+  textColor: "#777",
+};
