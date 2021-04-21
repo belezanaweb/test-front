ebaa4ef159bbe884a367f736c38a6484f86c7899
felpgoes <felipe.goess@hotmail.com>
2021-04-21 18:37:11 -0300

    feat: adding reactotron config

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,11 @@
+import Reactotron from 'reactotron-react-js'
+import { reactotronRedux } from 'reactotron-redux'
+import reactotronSaga from 'reactotron-redux-saga'
+
+if (process.env.NODE_ENV === 'development') {
+  const tron = Reactotron.configure().use(reactotronRedux()).use(reactotronSaga()).connect()
+
+  tron.clear()
+
+  console.tron = tron
+}
