c442a0680134d2cba477b3468d701923323c7cb2
Decxus <samuell.marques1@gmail.com>
2021-04-22 16:44:34 -0300

    Add page routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+import * as serviceWorker from './serviceWorker'
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
+import './css/index.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Cart from './pages/Cart'
+import Payment from './pages/Payment'
+import Confirmation from './pages/Confirmation'
 
 const Strict = () => (
   <StrictMode>
-    <App />
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/">
+            <Cart />
+          </Route>
+          <Route path="/payment">
+            <Payment />
+          </Route>
+          <Route path="/cart">
+            <Confirmation />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   </StrictMode>
 )
 
