2271427c9bc625f1007ffa4c73fbdfa789172a95
felpgoes <felipe.goess@hotmail.com>
2021-04-21 23:03:25 -0300

    feat: finish cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,15 +1,14 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { Container } from './styles'
-
 import { Button } from '../../components/Button'
 import { BorderContainer } from '../../components/BorderContainer'
 import { Card } from '../../components/Card'
 import { Text } from '../../components/Text'
-import { Header } from '../../components/Header'
+import { Item } from '../../components/Item'
 import { ProductHeader } from '../../components/ProductHeader'
 import { Creators as CartActions } from '../../store/ducks/cart'
+import { Container, ContainerWrapper, ProductWrapper } from './styles'
 
 export const Cart = () => {
   const dispatch = useDispatch()
@@ -20,43 +19,64 @@ export const Cart = () => {
   }, [])
 
   return (
-    <>
-      <Header>
-        <Text type="headerSeleted">SACOLA</Text>
-        <Text type="headerDefault">PAGAMENTO</Text>
-        <Text type="headerDefault">CONFIRMAÇÃO</Text>
-      </Header>
+    <Container>
+      <ProductWrapper>
+        <Text type="cardTextLabel">PRODUTOS</Text>
+        <Card margin="2%">
+          {cartById.items &&
+            cartById.items.map((item) => (
+              <BorderContainer color="#EEE">
+                <Item
+                  key={item.product.sku}
+                  item={{
+                    label: item.product.name,
+                    imageUrl: item.product.imageObjects[0].thumbnail,
+                    price: item.product.priceSpecification.price
+                      .toFixed(2)
+                      .toString()
+                      .replace('.', ',')
+                  }}
+                />
+              </BorderContainer>
+            ))}
+        </Card>
+      </ProductWrapper>
+      <ContainerWrapper>
+        <BorderContainer color="#CCC">
+          <ProductHeader>
+            <Text type="cartPriceValue">PRODUTOS</Text>
+            <Text type="cartPriceValue">
+              R$ {cartById.subTotal && cartById.subTotal.toFixed(2).toString().replace('.', ',')}
+            </Text>
+          </ProductHeader>
 
-      <Text type="cardTextLabel">PRODUTOS</Text>
-      <Card margin="2%">
-        <BorderContainer color="#EEE" />
-      </Card>
+          <ProductHeader>
+            <Text type="cartPriceValue">FRETE</Text>
+            <Text type="cartPriceValue">
+              R${' '}
+              {cartById.shippingTotal &&
+                cartById.shippingTotal.toFixed(2).toString().replace('.', ',')}
+            </Text>
+          </ProductHeader>
 
-      <BorderContainer color="#CCC">
-        <ProductHeader>
-          <Text type="cartPriceValue">PRODUTOS</Text>
-          <Text type="cartPriceValue">R$ {cartById.subTotal}</Text>
-        </ProductHeader>
+          <ProductHeader>
+            <Text type="cartPriceDiscount">DESCONTO</Text>
+            <Text type="cartPriceDiscount">
+              {' '}
+              - R$ {cartById.discount && cartById.discount.toFixed(2).toString().replace('.', ',')}
+            </Text>
+          </ProductHeader>
 
-        <ProductHeader>
-          <Text type="cartPriceValue">FRETE</Text>
-          <Text type="cartPriceValue">R$ {cartById.shippingTotal}</Text>
-        </ProductHeader>
+          <ProductHeader>
+            <Text type="cartPriceTotal">TOTAL</Text>
+            <Text type="cartPriceTotal">
+              R$ {cartById.total && cartById.total.toFixed(2).toString().replace('.', ',')}
+            </Text>
+          </ProductHeader>
+        </BorderContainer>
 
-        <ProductHeader>
-          <Text type="cartPriceDiscount">DESCONTO</Text>
-          <Text type="cartPriceDiscount"> - R$ {cartById.discount}</Text>
-        </ProductHeader>
-
-        <ProductHeader>
-          <Text type="cartPriceTotal">TOTAL</Text>
-          <Text type="cartPriceTotal">R$ {cartById.total}</Text>
-        </ProductHeader>
-      </BorderContainer>
-
-      <Container>
         <Button value="SEGUIR PARA O PAGAMENTO" />
-      </Container>
-    </>
+      </ContainerWrapper>
+    </Container>
   )
 }
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,5 +1,23 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  width: 100%;
+  @media (min-width: 1000px) {
+    display: grid;
+    grid-template-columns: 50% 50%;
+    grid-template-rows: 16% 42% 42%;
+  }
+`
+
+export const ProductWrapper = styled.div`
+  @media (min-width: 1000px) {
+    grid-column: 1 / 2;
+    margin: 5%;
+  }
+`
+
+export const ContainerWrapper = styled.div`
+  @media (min-width: 1000px) {
+    height: 40rem;
+    padding: 10%;
+  }
 `
