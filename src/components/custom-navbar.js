fa674489bf3614e8171240b2707c3f7b3531f56e
Decxus <samuell.marques1@gmail.com>
2021-04-22 16:37:55 -0300

    Add activeStatus on navbar

--- src/components/custom-navbar.js
+++ src/components/custom-navbar.js
@@ -2,19 +2,24 @@ import React, { Component } from 'react'
 import { Navbar } from '../css/styles'
 
 class CustomNavBar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      current_page: ''
+    }
+  }
+
   render() {
+    let classCart = this.props.current_page === 'cart' ? 'active' : ''
+    let classPayment = this.props.current_page === 'payment' ? 'active' : ''
+    let classConfirmation = this.props.current_page === 'confirmation' ? 'active' : ''
+
     return (
       <div>
         <Navbar>
-          <li>
-            <a href="#">Sacola</a>
-          </li>
-          <li>
-            <a href="#">Pagamento</a>
-          </li>
-          <li>
-            <a href="#">Confirmação</a>
-          </li>
+          <li className={classCart}>Sacola</li>
+          <li className={classPayment}>Pagamento</li>
+          <li className={classConfirmation}>Confirmação</li>
         </Navbar>
       </div>
     )
