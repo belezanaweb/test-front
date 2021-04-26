ef2299bbc34932c2ad2101cc5da32e74b8b9e4ad
Decxus <samuell.marques1@gmail.com>
2021-04-26 18:01:51 -0300

    Add Id proprerty

--- src/components/custom-button.js
+++ src/components/custom-button.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Button, Margin } from '../css/styles'
 
-class CustomButton extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      title: ''
-    }
-  }
-
-  render() {
-    return (
-      <Margin>
-        <Button>{this.props.title}</Button>
-      </Margin>
-    )
-  }
+export default function CustomButton({ id, label, disabled }) {
+  return (
+    <Margin>
+      <Button id={id} disabled={disabled}>
+        {' '}
+        {label}{' '}
+      </Button>
+    </Margin>
+  )
 }
-
-export default CustomButton
