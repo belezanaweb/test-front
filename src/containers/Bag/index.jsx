ebb73917f6c225526aed9c4565a3707385949559
Lucas Bomfim <lucasliko@hotmail.com>
2021-05-19 18:25:47 -0300

    feat: starting Bag Container

---
+++ src/containers/Bag/index.jsx
@@ -0,0 +1,8 @@
+import { Wrapper } from 'components/Wrapper'
+import React from 'react'
+
+const Bag = () => {
+  return <Wrapper></Wrapper>
+}
+
+export default Bag
---
+++ src/containers/Bag/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+import * as T from 'constants/styles/typography'
+import * as C from 'constants/styles/colors'
+
+const BagContainer = styled.div`
+  /* ... */
+`
+
+export default A
