aff793a6004629548112fc8f248d1acc8b6cbfe4
Lucas Bomfim <lucasliko@hotmail.com>
2021-05-19 23:02:07 -0300

    refactor: slice data

--- src/constants/config/index.js
+++ src/constants/config/index.js
@@ -1,1 +1,2 @@
-export const REACT_APP_BASE_URL = 'http://www.mocky.io/v2/5b15c4923100004a006f3c07'
+export const BASE_URL = 'http://www.mocky.io/v2/'
+export const BAG_ID = '5b15c4923100004a006f3c07'
