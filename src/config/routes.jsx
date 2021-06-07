10a847a6f0945e0a2dbfa8708e28796753bf26e9
Fernando de Bona <fbonareis@gmail.com>
2021-06-06 23:58:26 -0300

    feat: add code splitting routes

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'
 
-import { Cart, Payment, PaymentConfirmation } from '../pages'
+const Cart = React.lazy(() => import('../pages/Cart'))
+const Payment = React.lazy(() => import('../pages/Payment'))
+const PaymentConfirmation = React.lazy(() => import('../pages/PaymentConfirmation'))
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact>
-        <NavLink to="/cart">Ir para o Checkout</NavLink>
-      </Route>
+      <React.Suspense fallback={<span>Loading...</span>}>
+        <Route path="/" exact>
+          <NavLink to="/cart">Ir para o Checkout</NavLink>
+        </Route>
 
-      <Route path="/cart">
-        <Cart />
-      </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
 
-      <Route path="/payment">
-        <Payment />
-      </Route>
+        <Route path="/payment">
+          <Payment />
+        </Route>
 
-      <Route path="/confirmation">
-        <PaymentConfirmation />
-      </Route>
+        <Route path="/confirmation">
+          <PaymentConfirmation />
+        </Route>
+      </React.Suspense>
     </Switch>
   </BrowserRouter>
 )
