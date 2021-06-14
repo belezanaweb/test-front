6d967f0ca3c92068a7782c8bdb98d6554898628e
tarondaniel <marcosvtd+tarondaniel@gmail.com>
2021-06-14 12:24:32 -0300

    refactor: desktop styles in Confirmation page

--- src/pages/Confirmation/styles.js
+++ src/pages/Confirmation/styles.js
@@ -1,34 +1,20 @@
 import styled from 'styled-components'
 import { device } from '../../styles/breakpoints'
 
-export const Confirmation = styled.div`
+export const Confirmation = styled.section`
   display: flex;
   flex-direction: column;
   gap: var(--spacing-4);
-  padding-top: var(--spacing-2);
+  padding: var(--spacing-2) 0;
+  max-width: 500px;
+  margin: 0 auto;
 
   @media ${device.tablet} {
     padding-top: var(--spacing-4);
   }
 
   @media ${device.desktop} {
-    display: block;
-
-    > *:first-child {
-      width: 100%;
-      margin-bottom: var(--spacing-4);
-    }
-
-    > *:nth-child(3) {
-      float: right;
-    }
-
-    > * {
-      width: 40%;
-      height: 100%;
-      float: left;
-      width: calc(50% - var(--spacing-4));
-      margin: var(--spacing-2);
-    }
+    padding-top: var(--spacing-6);
+    gap: var(--spacing-6);
   }
 `
