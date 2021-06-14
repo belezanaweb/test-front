a3641253ad84f7fd71144b06cf0f664f4a7c3fc3
tarondaniel <marcosvtd+tarondaniel@gmail.com>
2021-06-14 02:01:12 -0300

    feat: add Loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const Loading = () => (
+  <S.Loading data-testid="loading">
+    <S.LoadingSpinner />
+  </S.Loading>
+)
+
+export default Loading
