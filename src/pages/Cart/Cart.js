3aec2ed34394d335590c5f2448653be161086c32
tarondaniel <marcosvtd+tarondaniel@gmail.com>
2021-06-14 02:04:10 -0300

    feat: add Cart page

---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+import formatPrice from '../../helpers/formatPrice'
+import CheckoutResume from '../../components/CheckoutResume'
+import ProductList from '../../components/ProductList'
+import Button from '../../components/Button'
+import { useCart } from '../../hooks/useCart'
+
+import * as S from './styles'
+
+const Cart = () => {
+  const { productList, shippingTotal, total, subTotal, discount } = useCart()
+  const history = useHistory()
+
+  const handleSubmit = () => history.push('/checkout')
+
+  return (
+    <S.Cart>
+      <ProductList productList={productList} />
+      <CheckoutResume
+        productsPrice={formatPrice(subTotal)}
+        freightPrice={formatPrice(shippingTotal)}
+        discountPrice={`- ${formatPrice(discount)}`}
+        totalPrice={formatPrice(total)}
+      />
+      <Button onClick={handleSubmit}>SEGUIR PARA O PAGAMENTO</Button>
+    </S.Cart>
+  )
+}
+
+export default Cart
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+import { device } from '../../styles/breakpoints'
+
+export const Cart = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-4);
+  padding-top: var(--spacing-2);
+
+  @media ${device.tablet} {
+    padding-top: var(--spacing-4);
+  }
+
+  @media ${device.desktop} {
+    display: block;
+
+    > *:first-child {
+      width: 60%;
+      margin: 0 auto;
+      float: left;
+    }
+
+    > * {
+      width: calc(40% - var(--spacing-8));
+      float: right;
+      margin: var(--spacing-4);
+    }
+
+    > button {
+      margin-top: 0;
+    }
+  }
+`
