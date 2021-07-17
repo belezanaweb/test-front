f58ab777fb6788013a5157416dc60a6db19b5d71
Gustavo <gustavomaurocamacho@hotmail.com>
2021-07-17 20:41:42 -0300

    Enhanced responsiveness of CardForm and Navbar

--- src/commons/title.js
+++ src/commons/title.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './title.css'
 
-const Title = ({ text, classname }) => <span className={'title ' + classname}>{text}</span>
+const Title = ({ text, classname = '' }) => <span className={'title ' + classname}>{text}</span>
 
 export { Title }
