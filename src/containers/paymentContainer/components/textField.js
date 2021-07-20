d03eac30abca023b007a2326fb825bec38157020
Gustavo <gustavomaurocamacho@hotmail.com>
2021-07-20 18:40:25 -0300

    Added invalid field message on card form

--- src/containers/paymentContainer/components/textField.js
+++ src/containers/paymentContainer/components/textField.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './textField.css';
 import InputMask from 'react-input-mask';
 import classNames from 'classnames';
 
-const TextField = ({ id, className, label, onChange, value, mask, placeholder, isValid }) => (
-  <div className="textField" mask={mask}>
-    <label htmlFor={id} className="label">
-      {label}
-    </label>
-    <InputMask
-      id={id}
-      data-testid={id}
-      className={classNames(
-        'input',
-        className,
-        { invalidInput: isValid === false },
-        { validInput: isValid === true }
-      )}
-      placeholder={placeholder}
+const TextField = ({ id, className, label, onChange, value, mask, placeholder, isValid }) => {
+  const [hasFocus, setHasFocus] = useState(false);
+
+  return (
+    <div
+      className={classNames('textField', isValid === false && !hasFocus ? 'invalid' : null)}
       mask={mask}
-      onChange={onChange}
-      value={value}
-      alwaysShowMask={true}
-    />
-  </div>
-);
+    >
+      <label htmlFor={id} className="label">
+        {label}
+      </label>
+      <InputMask
+        id={id}
+        data-testid={id}
+        className={classNames('input', className, { invalidInput: isValid === false })}
+        placeholder={placeholder}
+        mask={mask}
+        onChange={onChange}
+        value={value}
+        alwaysShowMask={true}
+        onFocus={() => setHasFocus(true)}
+        onBlur={() => setHasFocus(false)}
+      />
+      {isValid === false && !hasFocus && <span className="invalidMessage">Invalid Field</span>}
+    </div>
+  );
+};
 
 export { TextField };
