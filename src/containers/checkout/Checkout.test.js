a258329216565ee86965c07006a184ec6b6128d3
niltonxp2 <nitinhoxd@gmail.com>
2021-09-07 22:30:28 -0300

    reafctor: remove unused imports

--- src/containers/checkout/Checkout.test.js
+++ src/containers/checkout/Checkout.test.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import Checkout from '.'
-import useFetch from '../../hooks/useFetch'
 
 describe('Checkout component', () => {
   it('Should render loading', async () => {
