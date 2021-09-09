95f3f6b8801ad9c912bde8630833eb5660a70810
Diórgenes Eugênio <diorgeneseugenio@gmail.com>
2021-09-09 15:35:23 -0300

    feat: add resume and button on payment page

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
+import ResumeValues from '@/components/resume/ResumeValues'
+import Button from '@/components/Button'
+
+import { StepPathname } from '@/entities/Step'
+
 function Payment (): React.ReactElement {
-  return <div>Payment</div>
+  return (
+    <>
+      <ResumeValues />
+      <Button text="Finalizar pedido" href={StepPathname.Confirmacao} />
+    </>
+  )
 }
 
 export default Payment
