eb56c59252edff60c7a71f9530f98a83c2d4958e
Rafa Silveira <oi@rafasilveira.dev>
2021-09-22 12:58:23 -0300

    fix(request): https

--- src/requests/get-orders.request.ts
+++ src/requests/get-orders.request.ts
@@ -1,7 +1,7 @@
 import useSWR, { SWRResponse } from 'swr'
 import { IOrder } from './models/order.model'
 
-const endpoint = 'http://www.mocky.io/v2/5b15c4923100004a006f3c07'
+const endpoint = 'https://www.mocky.io/v2/5b15c4923100004a006f3c07'
 
 const getOrders: () => Promise<IOrder> = () => fetch(endpoint).then((data) => data.json())
 
