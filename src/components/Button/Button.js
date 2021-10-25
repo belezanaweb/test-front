e409adc5d21fbdbf306bc25df166a2452beef787
João Füzessy <joaofuzessy@MacBook-Air-de-Joao.local>
2021-10-25 12:20:08 -0300

    Button Component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { createUseStyles } from 'react-jss'
 
-const Button = () => {
+const useStyles = createUseStyles({
+  defaultButton: {
+    height: '60px',
+    width: '320px',
+    borderRadius: '3px',
+    backgroundColor: '#FF6C00',
+    boxShadow: 'inset 0 -3px 0 0 #D45A00, 0 2px 4px 0 rgba(0,0,0,0.25)',
+    border: 'none',
+    '&:hover': {
+      backgroundColor: '#D45A00'
+    }
+  },
+  textButton: {
+    height: '24px',
+    width: '318.12px',
+    color: '#FFF',
+    fontFamily: 'Helvetica Neue',
+    fontSize: '20px',
+    fontWeight: '700',
+    letterSpacing: '0.5px',
+    lineHeight: '24px',
+    textAlign: 'center',
+    textTransform: 'uppercase'
+  }
+})
+
+const Button = (props) => {
+  const { text, click } = props
+  const classes = useStyles()
+
   return (
     <div>
-      <button></button>
+      <button className={classes.defaultButton} onClick={click}>
+        <span className={classes.textButton}>{text}</span>
+      </button>
     </div>
   )
 }
--- src/routes/Cart/Cart.js
+++ src/routes/Cart/Cart.js
@@ -1,16 +1,27 @@
 import React, { useEffect, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
 import { CartContext } from '../../components/Context/Context'
+import Button from '../../components/Button/Button'
+import Card from '../../components/Card/Card'
+import OrderSummary from '../../components/OrderSummary/OrderSummary'
 
 const Cart = () => {
   const { cartProductsData } = useContext(CartContext)
+  const history = useHistory()
 
   useEffect(() => {
     console.log(cartProductsData)
   }, [cartProductsData])
 
+  const handleClick = () => {
+    history.push('/payment')
+  }
+
   return (
     <div data-testid="Cart">
-      <p>Cart</p>
+      <Card></Card>
+      <OrderSummary></OrderSummary>
+      <Button text="Seguir para o pagamento" click={handleClick} />
     </div>
   )
 }
