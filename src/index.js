5d03055f934634807b87e74fcb7e2c1859844881
renatogual <renatogual@hotmail.com>
2022-01-12 08:35:46 -0300

    refactor: deleted unnecessary import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
 import * as serviceWorker from './serviceWorker'
+
+import App from './App'
 
 const Strict = () => (
   <StrictMode>
