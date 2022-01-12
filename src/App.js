2b540a9cec625788f1c71749b2bfdbb1c7acba4c
renatogual <renatogual@hotmail.com>
2022-01-12 08:37:40 -0300

    feat: add routes config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-const App = () => <div className="App">Boa sorte! 🚀</div>
+import { Header } from './components'
+import { Bag } from './pages/Bag'
+
+import GlobalStyle from './styles/globalStyle'
+
+const App = () => (
+  <BrowserRouter>
+    <GlobalStyle />
+    <Header />
+    <Routes>
+      <Route path="/" element={<Bag />} />
+    </Routes>
+  </BrowserRouter>
+)
 
 export default App
---
+++ src/pages/Bag.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { Container } from '../styles/globalStyle'
+
+export function Bag() {
+  return <Container>Bag</Container>
+}
