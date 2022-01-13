1484fb14d38d887def87373238763d9e7def94de
renatogual <renatogual@hotmail.com>
2022-01-12 21:14:28 -0300

    feat: add active style in header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 import { HeaderContainer, HeaderContent, HeaderText } from './Header.elements'
 
 export default function Header() {
+  const { pathname } = useLocation()
+
+  const walletIsActive = pathname === '/wallet'
+  const paymentIsActive = pathname === '/payment'
+  const confirmationIsActive = pathname === '/confirmation'
+
   return (
     <HeaderContainer>
       <HeaderContent>
-        <HeaderText>SACOLA</HeaderText>
-        <HeaderText>PAGAMENTO</HeaderText>
-        <HeaderText>CONFIRMAÇÃO</HeaderText>
+        <HeaderText active={walletIsActive}>SACOLA</HeaderText>
+        <HeaderText active={paymentIsActive}>PAGAMENTO</HeaderText>
+        <HeaderText active={confirmationIsActive}>CONFIRMAÇÃO</HeaderText>
       </HeaderContent>
     </HeaderContainer>
   )
