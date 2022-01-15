d2850204d714eb30e7c063d77cef922fe5f41f61
Chandee <chandemoraes@hotmail.com>
2022-01-14 21:23:30 -0300

    feat: new page pagamento

--- src/component/Button/Button.js
+++ src/component/Button/Button.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import * as S from './Button.styles'
 
-const Button = ({ text, onClick }) => {
-  return <S.StyledButton onClick={onClick}>{text}</S.StyledButton>
+const Button = ({ text, onClick, ...props }) => {
+  return (
+    <S.StyledButton onClick={onClick} {...props}>
+      {text}
+    </S.StyledButton>
+  )
 }
 
 export default Button
