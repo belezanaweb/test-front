811e8cec3516726f3ec9a07fba087d74af53c9f1
jonathanfsilva <jonathansilva259@gmail.com>
2022-01-16 15:20:04 -0300

    refactor: add dinamic imports in routerConfig

--- src/navigation/RouterConfig.tsx
+++ src/navigation/RouterConfig.tsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { CART_PATH, PAYMENT_PATH, PAYMENT_SUCCESS_PATH } from '../constants/paths'
 
 import PurchaseContextProvider from '../contexts/PurchaseContext'
 
-import CartPage from '../pages/cart/Cart'
-import PaymentPage from '../pages/payment/Payment'
-import PaymentSuccess from '../pages/paymentSuccess/PaymentSuccess'
+const CartPage = lazy(() => import('../pages/cart/Cart'))
+const PaymentPage = lazy(() => import('../pages/payment/Payment'))
+const PaymentSuccess = lazy(() => import('../pages/paymentSuccess/PaymentSuccess'))
 
 function RouterConfig(): JSX.Element {
   return (
     <PurchaseContextProvider>
-      <Routes>
-        <Route path={CART_PATH} element={<CartPage />} />
-        <Route path={PAYMENT_PATH} element={<PaymentPage />} />
-        <Route path={PAYMENT_SUCCESS_PATH} element={<PaymentSuccess />} />
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path={CART_PATH} element={<CartPage />} />
+          <Route path={PAYMENT_PATH} element={<PaymentPage />} />
+          <Route path={PAYMENT_SUCCESS_PATH} element={<PaymentSuccess />} />
 
-        <Route path="*" element={<Navigate replace to={CART_PATH} />} />
-      </Routes>
+          <Route path="*" element={<Navigate replace to={CART_PATH} />} />
+        </Routes>
+      </Suspense>
     </PurchaseContextProvider>
   )
 }
