11ba8c74b4c5150eaeb680be9ac0519d3853daf7
Chandee <chandemoraes@hotmail.com>
2022-01-18 20:45:10 -0300

    feat: add split code

--- src/component/ContainerPage/ContainerPage.styles.js
+++ src/component/ContainerPage/ContainerPage.styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   padding: 10px;
   background-color: #eee;
-  height: 100vh;
+  min-height: 100vh;
 `
 
 export const InnerContainer = styled.div`
--- src/route/MyRouter.js
+++ src/route/MyRouter.js
@@ -1,19 +1,22 @@
-import * as React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Header from '../component/Header/Header'
-import Confirmacao from '../pages/Confirmacao'
-import Pagamento from '../pages/Pagamento'
-import Sacola from '../pages/Sacola'
+
+const Sacola = lazy(() => import('../pages/Sacola'))
+const Pagamento = lazy(() => import('../pages/Pagamento'))
+const Confirmacao = lazy(() => import('../pages/Confirmacao'))
 
 export default function MyRouter() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="sacola" element={<Sacola />} />
-        <Route path="pagamento" element={<Pagamento />} />
-        <Route path="confirmacao" element={<Confirmacao />} />
-      </Routes>
+      <Suspense fallback={<div />}>
+        <Routes>
+          <Route path="sacola" element={<Sacola />} />
+          <Route path="pagamento" element={<Pagamento />} />
+          <Route path="confirmacao" element={<Confirmacao />} />
+        </Routes>
+      </Suspense>
     </>
   )
 }
