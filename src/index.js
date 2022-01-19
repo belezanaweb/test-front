e6a516b944581591dbeeb2678c1e104753d06573
simoneguimaraes <simoneguimaraes11@gmail.com>
2022-01-18 22:37:48 -0300

    created a cart page and imported on app file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { GlobalStyle } from './assets/styles/global'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
+    <GlobalStyle />
   </BrowserRouter>,
+
   document.getElementById('root')
 )
