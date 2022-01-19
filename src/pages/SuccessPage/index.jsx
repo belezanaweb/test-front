e17b14984630a6fc73ad4d40737452c68b424ebc
simoneguimaraes <simoneguimaraes11@gmail.com>
2022-01-19 14:52:44 -0300

    inserted payment summary on success page

--- src/pages/SuccessPage/index.jsx
+++ src/pages/SuccessPage/index.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
 import { useCheckoutContext } from '../../contexts/Checkout'
+import PaymentConfirmation from '../../components/PaymentConfirmation'
+import CardContainer from '../../components/CardContainer'
 
 const SuccessPage = () => {
-  const { isLoading, prices, products, isValidated, paymentInfo } = useCheckoutContext()
+  const { paymentInfo } = useCheckoutContext()
+  const { cardName, cardNumber, cardDate } = paymentInfo
+  const successPageText = {
+    containerPaymentTitle: 'Pagamento',
+    containerProductsTitle: 'Produtos'
+  }
+
+  const maskCreditCardNumber = (number) => {
+    return '****.****.****.' + number.substring(15)
+  }
 
   return (
     <>
       <Navbar step={2} />
       <div>
         <PaymentConfirmation status={'success'} />
+        <CardContainer title={successPageText.containerPaymentTitle}>
+          <div>{maskCreditCardNumber(cardNumber)}</div>
+          <div>{cardName}</div>
+          <div>{cardDate}</div>
+        </CardContainer>
       </div>
     </>
   )
