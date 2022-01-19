db8f07f750ad5312d4514e746e0bcde2650fcb25
simoneguimaraes <simoneguimaraes11@gmail.com>
2022-01-19 13:14:35 -0300

    validation on  payment page

--- src/pages/Payment/index.jsx
+++ src/pages/Payment/index.jsx
@@ -1,17 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { useCheckoutContext } from '../../contexts/Checkout'
 import Navbar from '../../components/Navbar'
 import Button from '../../components/Button'
 import Inputs from '../../components/Inputs'
+import ProductList from '../../components/ProductList'
 
 const PaymentPage = () => {
-  const { isLoading } = useCheckoutContext()
+  const [data, setData] = useState({
+    cardNumber: {
+      value: '',
+      valid: true
+    },
+    cardName: {
+      value: '',
+      valid: true
+    },
+    cardDate: {
+      value: '',
+      valid: true
+    },
+    cardCvv: {
+      value: '',
+      valid: true
+    }
+  })
+
+  const {
+    isLoading,
+    prices,
+    isConfirmed,
+    setIsConfirmed,
+    setPaymentInfo,
+    paymentInfo
+  } = useCheckoutContext()
+
+  /* dados do cartão de crédito */
 
   const creditCardData = {
+    containerCreditTitle: 'Cartão de crédito',
+    buttonText: 'Finalizar o pedido',
     cardNumber: {
       placeholder: '____.____.____.____',
       mask: '9999.9999.9999.9999',
       label: 'Número do cartão:'
+    },
+    cardCvv: {
+      placeholder: '___',
+      mask: '999',
+      label: 'CVV:'
     },
     cardName: {
       placeholder: 'Como no cartão',
@@ -21,15 +57,105 @@ const PaymentPage = () => {
       placeholder: '__/____',
       mask: '99/9999',
       label: 'Validade (mês/ano):'
-    },
-    cardCvv: {
-      placeholder: '___',
-      mask: '999',
-      label: 'CVV:'
-    },
-    containerCreditTitle: 'Cartão de crédito',
-    buttonText: 'Finalizar o pedido'
+    }
   }
+
+  const { cardNumber, cardName, cardDate, cardCvv } = creditCardData
+
+  /* validação do cartão de crédito */
+
+  const switchValidation = useCallback((type, value, bol) => {
+    setData((data) => ({
+      ...data,
+      [type]: {
+        value: value,
+        valid: bol
+      }
+    }))
+  }, [])
+
+  const handleChange = useCallback(
+    (type, value) => {
+      const targetValue = value.target.value
+      const newValue = targetValue.replaceAll('.', '')
+
+      const valid = require('card-validator')
+      const valCardNumber = (number) => {
+        const numberValidation = valid.number(number)
+
+        if (numberValidation.isValid) {
+          return true
+        } else {
+          return false
+        }
+      }
+
+      const valCardName = (name) => {
+        const cardholderName = valid.cardholderName(name)
+
+        if (cardholderName.isValid && name.length > 1) {
+          return true
+        } else {
+          return false
+        }
+      }
+
+      const valCardDate = (date) => {
+        const expirationDate = valid.expirationDate(date)
+
+        if (expirationDate.isValid) {
+          return true
+        } else {
+          return false
+        }
+      }
+
+      const valCvv = (cvv) => {
+        const cvvValitation = valid.cvv(cvv)
+
+        if (cvvValitation.isValid) {
+          return true
+        } else {
+          return false
+        }
+      }
+
+      switch (type) {
+        case 'cardNumber':
+          switchValidation(type, targetValue, valCardNumber(newValue))
+          break
+        case 'cardName':
+          switchValidation(type, targetValue, valCardName(targetValue))
+          break
+        case 'cardDate':
+          switchValidation(type, targetValue, valCardDate(targetValue))
+          break
+        case 'cardCvv':
+          switchValidation(type, targetValue, valCvv(targetValue))
+          break
+        default:
+          return
+      }
+
+      setPaymentInfo({
+        ...paymentInfo,
+        [type]: targetValue
+      })
+    },
+    [paymentInfo, setPaymentInfo, switchValidation]
+  )
+
+  useEffect(() => {
+    const { cardNumber, cardDate, cardName, cardCvv } = data
+    const items = [cardNumber, cardDate, cardName, cardCvv]
+
+    const checkout = items.every((currentInput) => currentInput.value !== '' && currentInput.valid)
+    if (checkout) {
+      setIsConfirmed(true)
+    } else {
+      setIsConfirmed(false)
+    }
+  })
 
   return (
     <>
@@ -39,17 +165,37 @@ const PaymentPage = () => {
         <div>
           <>
             <div>
-              <Inputs />
-              <Inputs />
-              <Inputs />
-              <Inputs />
+              <Inputs
+                {...cardNumber}
+                value={data.cardNumber.value}
+                onChange={(event) => handleChange('cardNumber', event)}
+                valid={data.cardNumber.valid}
+              />
+              <Inputs
+                {...cardName}
+                value={data.cardName.value}
+                onChange={(event) => handleChange('cardName', event)}
+                valid={data.cardName.valid}
+              />
+              <Inputs
+                {...cardDate}
+                value={data.cardDate.value}
+                onChange={(event) => handleChange('cardDate', event)}
+                valid={data.cardDate.valid}
+              />
+              <Inputs
+                {...cardCvv}
+                value={data.cardCvv.value}
+                onChange={(event) => handleChange('cardCvv,', event)}
+                valid={data.cardCvv.valid}
+              />
             </div>
-
+            <ProductList prices={prices} />
             <Button
               text={creditCardData.buttonText}
               step="2"
               path="/success"
-              disabled={!isValidated}
+              disabled={!isConfirmed}
             />
           </>
         </div>
