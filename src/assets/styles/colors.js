5cc865aecc318d61dfdabcdd69c6f65379b5bacd
Adler Coelho <adler.coelho.santos@icloud.com>
2022-02-03 08:35:03 -0300

    feat: creating style dor Button component and init map of colors

---
+++ src/assets/styles/colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+  button: {
+    main: '#ff6c00',
+    text: '#fff',
+    hover: '#ff6c00'
+  }
+}
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import * as CSS from './style'
 
 export default function Button(props) {
-  return <button onClick={props.onClick}>{props.children}</button>
+  return <CSS.Button onClick={props.onClick}>{props.children}</CSS.Button>
 }
