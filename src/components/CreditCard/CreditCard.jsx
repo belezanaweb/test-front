a835509760e151e306b83cbad0d0b126272f00f9
Adler Coelho <adler.coelho.santos@icloud.com>
2022-02-05 13:34:09 -0300

    fix: changing input type for number

--- src/components/CreditCard/CreditCard.jsx
+++ src/components/CreditCard/CreditCard.jsx
@@ -19,6 +19,7 @@ export default function CreditCard() {
           mask={creditCardMask}
           placeholder={creditCardPlace}
           name="credit-card-number"
+          type="number"
         ></Input>
       </CSS.Column>
 
