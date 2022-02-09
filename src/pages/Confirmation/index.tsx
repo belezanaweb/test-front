35bb54b03da2a8950eb6e404f02ba6de3eeffef1
dxwebster <adrianalimafm@gmail.com>
2022-02-09 19:40:29 -0300

    feat: get cartInfo in localstorage in confirmation page

--- src/pages/Confirmation/index.tsx
+++ src/pages/Confirmation/index.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import { RootStateOrAny, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
 
 import SumInfo from '../../components/SumInfo';
 import ItemsList from '../../components/ItemsList';
 
 import { Container } from './styles';
+import { getFromLocalStorage } from '../../helpers/local-storage';
+import { cartRequest, cartSuccess } from '../../store/modules/cart/actions';
 
 export default function Confirmation() {
   const { cart } = useSelector((state: RootStateOrAny) => state.cart);
+  const [cartItems, setCartItems] = useState(null);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const cartFromStorage = getFromLocalStorage('@BelezaNaWeb: cart');
+
+    if (cartFromStorage) {
+      setCartItems(cartFromStorage);
+      dispatch(cartSuccess(cartFromStorage));
+    } else {
+      dispatch(cartRequest());
+    }
+  }, []);
+
+  useEffect(() => {
+    if (cart?.items?.length > 0) setCartItems(cart);
+  }, [cart?.items]);
 
   return (
     <Container>
-      {cart && <ItemsList cart={cart} />}
+      {cartItems && <ItemsList cart={cartItems} />}
       <SumInfo />
     </Container>
   );
