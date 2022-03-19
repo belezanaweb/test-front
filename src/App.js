a9dffdd3973c76c75d0ae234961ee1a7033b6fc5
Rafael Bamberg <raafaelbamberg@gmail.com>
2022-03-19 05:35:09 -0300

    Create page routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Bag from './pages/Bag/Bag'
+import Payment from './pages/Payment/Payment'
+import Sucess from './pages/Sucess/Sucess'
 
-const App = () => <div className="App">Boa</div>
-
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Bag />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/sucess" element={<Sucess />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
