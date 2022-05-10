f509c281eed0639c9b84203a5a3579c0b50e8877
lfilipe09 <81202572+lfilipe09@users.noreply.github.com>
2022-05-09 21:22:26 -0300

    feat: typing api's return

---
+++ src/types.ts
@@ -0,0 +1,37 @@
+export interface CartItems {
+  id: string;
+  items: Item[];
+  subTotal: number;
+  shippingTotal: number;
+  discount: number;
+  total: number;
+}
+
+export interface Item {
+  quantity: number;
+  product: Product;
+}
+
+export interface Product {
+  sku: number;
+  name: string;
+  imageObjects: Image[];
+  priceSpecification: {
+    sku: number,
+    price: number,
+    originalPrice: number,
+    maxPrice: number,
+    percent: number,
+    discount: number
+  };
+}
+
+export interface Image {
+  featured: boolean;
+  thumbnail: string;
+  small: string;
+  medium: string;
+  large: string;
+  extraLarge: string;
+  valid: boolean;
+}
