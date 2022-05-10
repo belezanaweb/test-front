e6c1a14d0af8bf64adf621bc0284097c856fce81
lfilipe09 <81202572+lfilipe09@users.noreply.github.com>
2022-05-09 23:00:38 -0300

    fix: change prettier file to fit generic typescript

--- src/hooks/useCartDetails.tsx
+++ src/hooks/useCartDetails.tsx
@@ -4,7 +4,7 @@ import { api } from '../services/api'
 import { CartDetails } from '../types'
 
 interface CartDetailsProviderProps {
-  children: ReactNode;
+  children: ReactNode
 }
 
 const cartDetailsInitialValues = {
@@ -16,17 +16,19 @@ const cartDetailsInitialValues = {
   total: 0
 }
 
-const CartDetailsContext = createContext < CartDetails > cartDetailsInitialValues
+const CartDetailsContext = createContext<CartDetails>(cartDetailsInitialValues)
 
 export function CartDetailsProvider({ children }: CartDetailsProviderProps) {
-  const [cartDetails, setCartDetails] = useState < CartDetails > cartDetailsInitialValues
+  const [cartDetails, setCartDetails] = useState<CartDetails>(
+    cartDetailsInitialValues
+  )
   const [mounted, setMounted] = useState(false)
 
   const getCartDetails = async () => {
     try {
-      api.get <
-        CartDetails >
-        'http://www.mocky.io/v2/5b15c4923100004a006f3c07'.then((cart) => {
+      api
+        .get<CartDetails>('http://www.mocky.io/v2/5b15c4923100004a006f3c07')
+        .then((cart) => {
           setCartDetails(cart.data)
           setMounted(true)
         })
@@ -39,7 +41,11 @@ export function CartDetailsProvider({ children }: CartDetailsProviderProps) {
     getCartDetails()
   }
 
-  return <CartDetailsContext.Provider value={cartDetails}>{children}</CartDetailsContext.Provider>
+  return (
+    <CartDetailsContext.Provider value={cartDetails}>
+      {children}
+    </CartDetailsContext.Provider>
+  )
 }
 
 export function useCartDetails(): CartDetails {
