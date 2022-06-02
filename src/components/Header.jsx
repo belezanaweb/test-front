25855634231a7efed3ed14606dfe75311f550c24
Alex Alves <alxalvs0121@gmail.com>
2022-06-02 09:17:35 -0300

    [checkout] add header css file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { PageHeader, Col, Row } from 'antd'
+import './Header.css'
 
 const Header = () => (
   <PageHeader>
     <Row>
       <Col>
-        <Link to="/cart">SACOLA</Link>
+        <Link className="link" to="/cart">
+          SACOLA
+        </Link>
       </Col>
       <Col>PAGAMENTO</Col>
       <Col>CONFIRMAÇÃO</Col>
