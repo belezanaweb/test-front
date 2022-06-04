2caae644c3fa753cb2472a0e86c7a86fe40d674c
Alex Alves <alxalvs0121@gmail.com>
2022-06-04 17:08:37 -0300

    [checkout] add loading symbol while fetching data

--- src/routes/cart/Cart.jsx
+++ src/routes/cart/Cart.jsx
@@ -1,31 +1,29 @@
 import React, { useEffect } from 'react'
 import useSWR from 'swr'
 import URL from '../../utils/constants.js'
-import { Col, Row } from 'antd'
-import { useSelector, useDispatch } from 'react-redux'
-import { setTotal } from '../../reducer/Cart'
+import { Col, Row, Spin } from 'antd'
 import Header from '../../components/Header.jsx'
 import './Cart.css'
 
 const Cart = () => {
   const { data: result, error } = useSWR(URL)
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    if (result) {
-      dispatch(setTotal(result.total))
-    }
-  }, [result])
 
   if (error) return <h1>Something went wrong!</h1>
-  if (!result) return <h1>Loading...</h1>
 
   return (
     <>
       <Header />
       <Row className="cartBody">
         <Row className="productCard">
-          <Col className="productLabel">PRODUTOS</Col>
+          {!result ? (
+            <Row className="loading">
+              <Col>
+                <Spin />
+              </Col>
+            </Row>
+          ) : (
+            <Col className="productLabel">PRODUTOS</Col>
+          )}
         </Row>
       </Row>
     </>
