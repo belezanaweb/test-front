7322da681a1efa83ac4d95b628507ce90ce5ba31
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-06-12 17:15:04 -0300

    Fix nav items' fonts and make it responsive

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -6,11 +6,25 @@ export const Nav = styled.nav`
   max-width: 720px;
   height: 100%;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
+  margin: 0 auto;
+
+  @media (min-width: 720px) {
+    a {
+      font-size: 1.2rem;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    a {
+      font-size: 1.3rem;
+    }
+  }
 `
 export const CustomLink = styled(Link)`
-  font-size: 1.204vw;
+  font-size: 0.81rem;
   color: #ccc;
   text-transform: uppercase;
+  text-decoration: none;
 `
