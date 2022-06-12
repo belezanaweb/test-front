1923a8ae686c0b91cb7c3fea32014d7ed24cbbbd
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-06-12 17:45:10 -0300

    Create Products component

---
+++ src/components/Products/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from './styles'
+import { Product } from '../Product'
+
+export const Products = () => {
+  return (
+    <Container>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+    </Container>
+  )
+}
---
+++ src/components/Products/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  width: 100%;
+  padding: 13px;
+  background-color: #fff;
+`
