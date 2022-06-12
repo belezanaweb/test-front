265396b110a242be0cf96a6c07dbc118d663a4cc
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-06-12 19:49:42 -0300

    Create Confirmation page's general structure

--- src/pages/Confirmation/index.jsx
+++ src/pages/Confirmation/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import { Container } from './styles'
+import { Products } from '../../components/Products'
+import { Summary } from '../../components/Summary'
+import { PaymentInformation } from '../../components/PaymentInformation'
 
 export const Confirmation = () => {
-  return <div>Confirmation</div>
+  return (
+    <Container>
+      <PaymentInformation />
+      <Products />
+      <Summary />
+    </Container>
+  )
 }
