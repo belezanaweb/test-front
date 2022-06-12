08202b4b5a3f3ef5ba87c97b26340b868a968cc2
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-06-12 19:00:52 -0300

    Add styling to Summary

--- src/components/Summary/index.jsx
+++ src/components/Summary/index.jsx
@@ -1,5 +1,25 @@
 import React from 'react'
+import { Container, div, ShippingCost, Discount, Total } from './styles'
 
-export const Summary = () => {
-  return <div>Summary</div>
+export const Summary = ({ pricesTotal, shippingCost, discount, total }) => {
+  return (
+    <Container>
+      <div>
+        <p>Produtos</p>
+        <p>RS{pricesTotal}</p>
+      </div>
+      <div>
+        <p>Frete</p>
+        <p>R${shippingCost}</p>
+      </div>
+      <Discount>
+        <p>Desconto</p>
+        <p>R${discount}</p>
+      </Discount>
+      <Total>
+        <p>Total</p>
+        <p>R${total}</p>
+      </Total>
+    </Container>
+  )
 }
---
+++ src/components/Summary/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  width: 100%;
+  border: 1px solid #d4d4d4;
+  padding: 15px;
+
+  div {
+    width: 100%;
+    font-size: 0.875rem;
+    margin-bottom: 8px;
+    color: #212122;
+    display: flex;
+    justify-content: space-between;
+
+    &:nth-child(3) {
+      margin-bottom: 16px;
+    }
+  }
+`
+
+export const Discount = styled.div`
+  p {
+    color: #ff7800;
+  }
+`
+export const Total = styled.div`
+  p {
+    font-weight: 800;
+  }
+`
