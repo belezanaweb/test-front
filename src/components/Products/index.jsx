d4166af9271bf65b92842ed5c5cf2d3e021e644a
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-06-12 18:30:05 -0300

    Add section header to products

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Container } from './styles'
 import { Product } from '../Product'
+import { SectionHeader } from '../SectionHeader'
 
 export const Products = () => {
   return (
-    <Container>
-      <Product></Product>
-      <Product></Product>
-      <Product></Product>
-    </Container>
+    <>
+      <SectionHeader text={'Produtos'} />
+      <Container>
+        <Product></Product>
+        <Product></Product>
+        <Product></Product>
+      </Container>
+    </>
   )
 }
