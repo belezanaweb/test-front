5b58f88a9b56046d5442d05dc0f4aabf44827743
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-06-12 18:29:43 -0300

    Create SectionHeader component

---
+++ src/components/SectionHeader/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Container } from './styles'
+
+export const SectionHeader = ({ text }) => {
+  return <Container>{text}</Container>
+}
---
+++ src/components/SectionHeader/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.p`
+  padding: 0 10px;
+  font-size: 0.875rem;
+  font-weight: 800;
+  color: #999;
+  margin-bottom: 5px;
+  text-transform: uppercase;
+`
