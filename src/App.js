25995d8190b75679dbc3b24bd1311ff54ca59417
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-06-12 17:13:48 -0300

    Create app's general structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import './App.css'
-
-const App = () => <div className="App">Boa sorte! 🚀</div>
+import { Route, Routes } from 'react-router-dom'
+import { PageContainer } from './components/PageContainer'
+import { Header } from './components/Header'
+import { Cart } from './pages/Cart/'
+import { Payment } from './pages/Payment'
+import { Confirmation } from './pages/Confirmation'
+const App = () => {
+  return (
+    <PageContainer>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Cart />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
+    </PageContainer>
+  )
+}
 
 export default App
