a0f75cb9451e3f4ea44b6788db08d3e2e66cc74c
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-06-12 19:50:11 -0300

    Create Payment page's general structure

--- src/pages/Payment/index.jsx
+++ src/pages/Payment/index.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { Container } from './styles'
+import { Products } from '../../components/Products'
+import { Summary } from '../../components/Summary'
+import { ActionButton } from '../../components/ActionButton'
+import { PaymentInformation } from '../../components/PaymentInformation'
 
 export const Payment = () => {
-  return <div>Payment</div>
+  return (
+    <Container>
+      <Products />
+      <Summary />
+      <ActionButton actionText={'Finalizar o pedido'} />
+    </Container>
+  )
 }
---
+++ src/pages/Payment/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  width: 100%;
+  padding: 10px;
+`
