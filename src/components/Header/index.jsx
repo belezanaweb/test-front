acb9e317a3f11c5cc30908b3d8430a22b295d9d0
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-06-12 14:52:54 -0300

    Create Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Container } from './styles'
+
+export const Header = () => {
+  return <Container></Container>
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  width: 100%;
+  height: 40px;
+  background-color: #fff;
+`
