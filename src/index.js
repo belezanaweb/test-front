ca5edf28d51350ff0468eeb2fc6c694faa48a02c
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-06-13 22:01:50 -0300

    Create store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom'
 import './global.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 const Strict = () => (
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 )
 
