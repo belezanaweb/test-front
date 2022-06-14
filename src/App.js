aa26521bfc1610b0ff5db6084ea0f8bac3339e91
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-06-14 08:57:41 -0300

    Implement data fetching

--- src/App.js
+++ src/App.js
@@ -2,11 +2,34 @@ import React from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { PageContainer } from './components/PageContainer'
-import { Header } from './components/Header'
 import { Cart } from './pages/Cart/'
 import { Payment } from './pages/Payment'
 import { Confirmation } from './pages/Confirmation'
+import { useEffect } from 'react'
+import { api } from './services/api'
+import { useDispatch } from 'react-redux'
+import { loadProducts } from './redux/slices/productsSlice'
+import { loadSummaryInfo } from './redux/slices/summarySlice'
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const res = await api.get()
+      dispatch(loadProducts(res.data.items))
+      console.log(res)
+      dispatch(
+        loadSummaryInfo({
+          subTotal: res.data.subTotal,
+          discount: res.data.discount,
+          shippingCost: res.data.shippingTotal,
+          total: res.data.total
+        })
+      )
+    }
+
+    getProducts()
+  }, [dispatch])
   return (
     <PageContainer>
       <Routes>
