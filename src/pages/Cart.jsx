dd93205a279a2ce45a285a971d90bcf3f8559b8f
Gabriel Arruda <gabriel@juriscorrespondente.com.br>
2022-06-17 19:18:06 -0300

    Feat: Add Axios and get cart data

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,16 +2,39 @@ import { Header } from "../components/Header";
 import { Content } from "../components/Content";
 import { ProductCard } from "../components/ProductCard"
 
+import axios from "axios"
+import { useEffect, useState } from "react";
+
 export function Cart() {
+
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://www.mocky.io/v2/5b15c4923100004a006f3c07')
+      .then((response) => {
+        setCart(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
+
   return (
     <>
       <Header activeItem='sacola'></Header>
-      <Content title='Produtos' type='white'>
-        <ProductCard />
-      </Content>
-      <Content>
-        Lorem Ipsum
-      </Content>
+      {cart.items?.length > 0 &&  (
+        <>
+          <Content title='Produtos' type='white'>
+            {cart.items.map((item) => (
+              <ProductCard key={item.product.sku} product={{name: item.product.name, image: item.product.imageObjects[0].thumbnail, price: item.product.priceSpecification.price}}/>
+            ))}
+          </Content>
+          <Content>
+            Lorem Ipsum
+          </Content>
+        </>
+      )}
     </>
   )
 }
