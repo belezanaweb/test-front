9bcd4a0210ceab6be3c5d05eda2bfc86b75f1489
Vinícius Di Santi Cascales Aragao <vinicius@MacBook-Air-de-Vinicius.local>
2022-07-05 19:14:44 -0300

    feat(button component): creating button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from 'react'
+import { ButtonBox } from './Button.style'
+
+interface ButtonProps {
+  children: ReactNode;
+}
+
+export const Button: React.FC<ButtonProps> = ({ children }: ButtonProps) => (
+  <ButtonBox>{children}</ButtonBox>
+)
