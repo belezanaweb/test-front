495c290b4be4168765c72c26f815b062066a1135
Vinícius Di Santi Cascales Aragao <vinicius@MacBook-Air-de-Vinicius.local>
2022-07-06 13:20:46 -0300

    feat(theme): adding new variables values

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -4,10 +4,12 @@ const defaultTheme: DefaultTheme = {
   borderRadius: '3px',
 
   fonts: {
+    label: '12px',
     header: '13px',
-    title: '14px',
     text: '13px',
+    title: '14px',
     highlightText: '14px',
+    input: '16px',
     button: '20px'
   },
 
@@ -16,10 +18,14 @@ const defaultTheme: DefaultTheme = {
     secondary: '#999',
     accent: '#FF6C00',
     darkAccent: '#D45A00',
-    disabled: '#CCC',
+    ultraLightGray: '#E0E7EE',
+    lightGray: '#CCC',
+    mediumGray: '#E7E7E7',
     gray: '#EEE',
-    black: '#212122',
-    white: '#FFF'
+    black: '#212121',
+    white: '#FFF',
+    purple: '#A43287',
+    red: '#F30'
   },
 
   shadows: {
