9ba83df5b035b7fae959329c0f38c2257a066be1
Vinícius Di Santi Cascales Aragao <vinicius@MacBook-Air-de-Vinicius.local>
2022-07-05 21:29:53 -0300

    feat(app component): adding global components to its page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { Header } from './components/atoms'
 import GlobalStyle from './global/styles/style'
+import { defaultTheme } from './global/styles/theme'
 import { Bag } from './pages/bag'
 
 export const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <Bag />
+    <ThemeProvider theme={defaultTheme}>
+      <Header
+        items={[
+          {
+            label: 'sacola'
+          },
+          {
+            label: 'pagamento'
+          },
+          {
+            label: 'confirmação'
+          }
+        ]}
+      />
+      <Bag />
+    </ThemeProvider>
   </>
 )
