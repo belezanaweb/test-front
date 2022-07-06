58430f501892f83bfaa8dbe195dab2f6aa5c95e2
Vinícius Di Santi Cascales Aragao <vinicius@MacBook-Air-de-Vinicius.local>
2022-07-06 13:18:42 -0300

    feat(button component): adjusting button component to receive new props

--- src/components/atoms/button/Button.style.ts
+++ src/components/atoms/button/Button.style.ts
@@ -13,7 +13,10 @@ export const ButtonBox = styled.button(
   width: 100%;
   box-shadow: inset 0 -3px 0 0 #D45A00, 0 2px 4px 0 rgba(0,0,0,0.25), ${theme.shadows.paper};
   transition: .2s;
-  &:hover {
+  &:disabled {
+    opacity: 0.5;
+  }
+  &:not(:disabled):hover {
     background-color: ${theme.colors.darkAccent};
     box-shadow: none;
     cursor: pointer;
--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -4,8 +4,17 @@ import { ButtonBox } from './Button.style'
 interface ButtonProps {
   children: ReactNode
   onClick: () => void
+  type?: 'button' | 'submit' | 'reset'
+  disabled?: boolean
 }
 
-export const Button: React.FC<ButtonProps> = ({ children, onClick }: ButtonProps) => (
-  <ButtonBox onClick={onClick}>{children}</ButtonBox>
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  type = 'button',
+  disabled
+}: ButtonProps) => (
+  <ButtonBox disabled={disabled} type={type} onClick={onClick}>
+    {children}
+  </ButtonBox>
 )
