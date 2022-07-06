9024623411ff26118a75bc23e7f52c481dd2b1db
Vinícius Di Santi Cascales Aragao <vinicius@MacBook-Air-de-Vinicius.local>
2022-07-06 19:44:13 -0300

    refactor(paymentform): adjusting placeholders and adding grid area name

--- src/components/molecules/paymentForm/PaymentForm.style.ts
+++ src/components/molecules/paymentForm/PaymentForm.style.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+export const Container = styled.section`
+  grid-area: payment-form;
+`
+
 export const RowFields = styled.div`
   display: flex;
   gap: 20px;
--- src/components/molecules/paymentForm/PaymentForm.tsx
+++ src/components/molecules/paymentForm/PaymentForm.tsx
@@ -3,7 +3,7 @@ import { DeepRequired, FieldErrorsImpl, UseFormRegister } from 'react-hook-form'
 import { FormMessages } from '../../../constants/formMessages'
 import { formatCreditCard, formatCvv, formatValidDate } from '../../../utils/mask'
 import { Input, Paper } from '../../atoms'
-import { RowFields } from './PaymentForm.style'
+import { Container, RowFields } from './PaymentForm.style'
 
 export interface FormData {
   cardNumber: string | undefined
@@ -18,66 +18,68 @@ interface PaymentFormProps {
 }
 
 export const PaymentForm = ({ register, errors }: PaymentFormProps) => (
-  <Paper title="cartão de crédito">
-    <Input
-      label="Número do Cartão:"
-      placeholder="_ _ _ _._ _ _ _._ _ _ _._ _ _ _"
-      error={errors.cardNumber?.message}
-      {...register('cardNumber', {
-        required: FormMessages.required,
-        pattern: {
-          value: /^[0-9]{4}\.[0-9]{4}\.[0-9]{4}\.[0-9]{4}$/i,
-          message: FormMessages.invalid
-        }
-      })}
-      onChange={(event) => {
-        const { target } = event
-        target.value = formatCreditCard(target.value)
-      }}
-    />
-
-    <Input
-      label="Nome do Titular:"
-      placeholder="Como no cartão"
-      error={errors.cardName?.message}
-      {...register('cardName', { required: FormMessages.required })}
-    />
-
-    <RowFields>
+  <Container>
+    <Paper title="cartão de crédito" paddingBottom={false}>
       <Input
-        label="Validade (mês/ano):"
-        placeholder="_ _/_ _ _ _"
-        error={errors.date?.message}
-        {...register('date', {
+        label="Número do Cartão:"
+        placeholder="____.____.____.____"
+        error={errors.cardNumber?.message}
+        {...register('cardNumber', {
           required: FormMessages.required,
           pattern: {
-            value: /^[0-9]{2,}\/[0-9]{4,}/i,
+            value: /^[0-9]{4}\.[0-9]{4}\.[0-9]{4}\.[0-9]{4}$/i,
             message: FormMessages.invalid
           }
         })}
         onChange={(event) => {
           const { target } = event
-          target.value = formatValidDate(target.value)
+          target.value = formatCreditCard(target.value)
         }}
       />
 
       <Input
-        label="CVV:"
-        placeholder="_ _ _"
-        minWidth="140px"
-        error={errors.cvv?.message}
-        {...register('cvv', {
-          required: FormMessages.required,
-          pattern: {
-            value: /^[0-9]{3}$/i,
-            message: FormMessages.invalid
-          }
-        })}
-        onChange={(event) => {
-          const { target } = event
-          target.value = formatCvv(target.value)
-        }}
+        label="Nome do Titular:"
+        placeholder="Como no cartão"
+        error={errors.cardName?.message}
+        {...register('cardName', { required: FormMessages.required })}
       />
-    </RowFields>
-  </Paper>
+
+      <RowFields>
+        <Input
+          label="Validade (mês/ano):"
+          placeholder="__/____"
+          error={errors.date?.message}
+          {...register('date', {
+            required: FormMessages.required,
+            pattern: {
+              value: /^[0-9]{2,}\/[0-9]{4,}/i,
+              message: FormMessages.invalid
+            }
+          })}
+          onChange={(event) => {
+            const { target } = event
+            target.value = formatValidDate(target.value)
+          }}
+        />
+
+        <Input
+          label="CVV:"
+          placeholder="___"
+          minWidth="140px"
+          error={errors.cvv?.message}
+          {...register('cvv', {
+            required: FormMessages.required,
+            pattern: {
+              value: /^[0-9]{3}$/i,
+              message: FormMessages.invalid
+            }
+          })}
+          onChange={(event) => {
+            const { target } = event
+            target.value = formatCvv(target.value)
+          }}
+        />
+      </RowFields>
+    </Paper>
+  </Container>
 )
