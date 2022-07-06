39b386c685c247ca1b21d90f04f19944be6bc01b
Vinícius Di Santi Cascales Aragao <vinicius@MacBook-Air-de-Vinicius.local>
2022-07-06 19:41:02 -0300

    fix(button and title): adjusting line height values

--- src/components/atoms/button/Button.style.ts
+++ src/components/atoms/button/Button.style.ts
@@ -2,24 +2,25 @@ import styled from 'styled-components'
 
 export const ButtonBox = styled.button(
   ({ theme }) => `
-  font-size: ${theme.fonts.button};
-  color: ${theme.colors.white};
-  background-color: ${theme.colors.accent};
-  border: none;
-  border-radius: ${theme.borderRadius};
-  font-weight: 700;
-  text-transform: uppercase;
-  padding: 18px 12px;
-  width: 100%;
-  box-shadow: inset 0 -3px 0 0 #D45A00, 0 2px 4px 0 rgba(0,0,0,0.25), ${theme.shadows.paper};
-  transition: .2s;
-  &:disabled {
-    opacity: 0.5;
-  }
-  &:not(:disabled):hover {
-    background-color: ${theme.colors.darkAccent};
-    box-shadow: none;
-    cursor: pointer;
-  }
-`
+    font-size: ${theme.fonts.button};
+    color: ${theme.colors.white};
+    background-color: ${theme.colors.accent};
+    border: none;
+    border-radius: ${theme.borderRadius};
+    font-weight: 700;
+    text-transform: uppercase;
+    padding: 18px 12px;
+    width: 100%;
+    box-shadow: inset 0 -3px 0 0 #D45A00, 0 2px 4px 0 rgba(0,0,0,0.25), ${theme.shadows.paper};
+    transition: .2s;
+    line-height: 24px;
+    &:disabled {
+      opacity: 0.5;
+    }
+    &:not(:disabled):hover {
+      background-color: ${theme.colors.darkAccent};
+      box-shadow: none;
+      cursor: pointer;
+    }
+  `
 )
--- src/components/atoms/title/Title.style.ts
+++ src/components/atoms/title/Title.style.ts
@@ -2,10 +2,11 @@ import styled from 'styled-components'
 
 export const Heading = styled.h1(
   ({ theme }) => `
-  font-size: ${theme.fonts.title};
-  color: ${theme.colors.secondary};
-  font-weight: 700;
-  text-transform: uppercase;
-  margin: 0px 0px 5px;
-`
+    font-size: ${theme.fonts.title};
+    color: ${theme.colors.secondary};
+    font-weight: 700;
+    text-transform: uppercase;
+    margin: 0px 0px 5px;
+    line-height: 17px;
+  `
 )
