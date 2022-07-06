957651bf4be43cc64700ee44295ed535fcb8c069
Vinícius Di Santi Cascales Aragao <vinicius@MacBook-Air-de-Vinicius.local>
2022-07-06 19:39:36 -0300

    feat(bagdetails): adding button component to it

--- src/components/molecules/bagDetails/BagDetails.style.ts
+++ src/components/molecules/bagDetails/BagDetails.style.ts
@@ -5,23 +5,32 @@ interface BagDetailsItem {
   bold: boolean
 }
 
+export const Container = styled.section`
+  grid-area: bag-details;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+
 export const BagDetailsList = styled.ul`
   margin: 0;
   list-style: none;
   padding: 0;
 `
 
-export const BagDetailsItem = styled.li<BagDetailsItem>(
-  ({ highlight, bold, theme }) => `
-display: flex;
-width: 100%;
-justify-content: space-between;
-color: ${highlight ? theme.colors.accent : theme.colors.primary};
-font-weight: ${bold ? '700' : 'normal'};
-font-size: ${theme.fonts.highlightText};
-margin-bottom: 9px;
-`
-)
+export const BagDetailsItem = styled.li<BagDetailsItem>(({ highlight, bold, theme }) => {
+  const checkHighlightMarginBottom = highlight ? '16px' : '9px'
+  return `
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    color: ${highlight ? theme.colors.accent : theme.colors.primary};
+    font-weight: ${bold ? '700' : 'normal'};
+    font-size: ${theme.fonts.highlightText};
+    margin-bottom: ${bold ? '1px' : checkHighlightMarginBottom};
+    line-height: 17px;
+  `
+})
 
 export const BagDetailsLabel = styled.p`
   text-transform: uppercase;
--- src/components/molecules/bagDetails/BagDetails.tsx
+++ src/components/molecules/bagDetails/BagDetails.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import { formatValueToBRL } from '../../../utils/currency'
-import { Paper } from '../../atoms'
+import { Button, Paper } from '../../atoms'
 import {
   BagDetailsItem,
   BagDetailsLabel,
   BagDetailsList,
-  BagDetailsValue
+  BagDetailsValue,
+  Container
 } from './BagDetails.style'
+
+type ButtonData = {
+  label: string
+  action: () => void
+  disabled?: boolean
+}
 
 interface BagDetailsProps {
   bag: ProductBag
+  buttonData?: ButtonData
 }
 
-export const BagDetails = ({ bag }: BagDetailsProps) => {
+export const BagDetails = ({ bag, buttonData }: BagDetailsProps) => {
   const details = [
     { label: 'produtos', value: bag.subTotal },
     { label: 'frete', value: bag.shippingTotal },
@@ -21,21 +29,28 @@ export const BagDetails = ({ bag }: BagDetailsProps) => {
   ]
 
   return (
-    <Paper isSolid={false}>
-      <BagDetailsList>
-        {details.map(({ label, value }) => {
-          const isDiscount = label === 'desconto'
-          const isTotal = label === 'total'
-          return (
-            <BagDetailsItem key={label} highlight={isDiscount} bold={isTotal}>
-              <BagDetailsLabel>{label}</BagDetailsLabel>
-              <BagDetailsValue>{`${isDiscount ? '- ' : ''} ${formatValueToBRL(
-                value
-              )}`}</BagDetailsValue>
-            </BagDetailsItem>
-          )
-        })}
-      </BagDetailsList>
-    </Paper>
+    <Container>
+      <Paper isSolid={false}>
+        <BagDetailsList>
+          {details.map(({ label, value }) => {
+            const isDiscount = label === 'desconto'
+            const isTotal = label === 'total'
+            return (
+              <BagDetailsItem key={label} highlight={isDiscount} bold={isTotal}>
+                <BagDetailsLabel>{label}</BagDetailsLabel>
+                <BagDetailsValue>{`${isDiscount ? '- ' : ''} ${formatValueToBRL(
+                  value
+                )}`}</BagDetailsValue>
+              </BagDetailsItem>
+            )
+          })}
+        </BagDetailsList>
+      </Paper>
+      {buttonData && (
+        <Button onClick={buttonData.action} disabled={buttonData.disabled}>
+          {buttonData.label}
+        </Button>
+      )}
+    </Container>
   )
 }
