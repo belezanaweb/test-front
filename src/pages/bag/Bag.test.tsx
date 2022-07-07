3b5d2080a95f00d4b7b1d567a54e8741c2a5670b
Vinícius Di Santi Cascales Aragao <vinicius@MacBook-Air-de-Vinicius.local>
2022-07-06 22:56:25 -0300

    test(bag page): removing unused test

--- src/pages/bag/Bag.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import { Bag } from './Bag'
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Bag />)
-  const linkElement = getByText(/sacola/i)
-  expect(linkElement).toBeTruthy()
-})
