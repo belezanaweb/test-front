3f2c409ede715be0298f13701acb9f1e16028acf
Vinícius Di Santi Cascales Aragao <vinicius@MacBook-Air-de-Vinicius.local>
2022-07-07 10:31:28 -0300

    refactor(global css): adjusting font family and adding favicon

--- src/global/styles/style.ts
+++ src/global/styles/style.ts
@@ -1,20 +1,16 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Helvetica Neue';
-    src: local('Helvetica Neue'), url(../../assets/fonts/HelveticaNeue.ttf) format('truetype'),
-      url(../../assets/fonts/HelveticaNeue.woff) format('woff'),
-      url(../../assets/fonts/HelveticaNeue.eot?#iefix) format('embedded-opentype');
-  }
-
   body {
     margin: 0;
     padding: 0;
-    font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
+    background-color: #eee;
+  }
+
+  * {
+    font-family: Helvetica-Neue, HelveticaNeue, Helvetica, Arial, 'Lucida Grande', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    background-color: #eee;
   }
 
   #root {
