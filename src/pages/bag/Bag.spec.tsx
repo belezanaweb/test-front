cbb6276146300feb30df82ff103b4c42637e00df
Vinícius Di Santi Cascales Aragao <vinicius@MacBook-Air-de-Vinicius.local>
2022-07-06 23:31:32 -0300

    test(bag test): adding bag tests

---
+++ src/pages/bag/Bag.spec.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { setupServer } from 'msw/node'
+import { waitFor } from '@testing-library/react'
+import { rest } from 'msw'
+import * as router from 'react-router'
+import { render, fireEvent } from '../../test-utils'
+import { Bag } from './Bag'
+import { bagResponseMock } from '../../../mock/bagResponse'
+import { belezaNaWebApi } from '../../api'
+
+const navigate = jest.fn()
+
+beforeEach(() => {
+  jest.spyOn(router, 'useNavigate').mockImplementation(() => navigate)
+})
+
+const baseURL = belezaNaWebApi.getUri()
+
+const server = setupServer(
+  rest.get(`${baseURL}5b15c4923100004a006f3c07`, (req, res, ctx) =>
+    res(ctx.status(200), ctx.json(bagResponseMock))
+  )
+)
+
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+
+const renderComponent = () => render(<Bag />)
+
+describe('Bag', () => {
+  test('renders correctly', async () => {
+    const { getByText } = renderComponent()
+    await waitFor(() => getByText(/L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium/i))
+    getByText(/225,90 BRL/i)
+
+    getByText(/Good Girl Carolina Herrera Eau de Parfum - Perfume Feminino 30ml/i)
+    getByText(/299,00 BRL/i)
+
+    getByText(/Senscience Inner Restore Intensif - Máscara Capilar 50ml/i)
+    getByText(/99,90 BRL/i)
+  })
+
+  test('goes to payment page correctly', async () => {
+    const { getByText, getByRole } = renderComponent()
+    await waitFor(() => getByText(/L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium/i))
+
+    const button = getByRole('button')
+    await fireEvent.click(button)
+
+    expect(navigate).toHaveBeenCalledWith('/payment')
+  })
+})
--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -2,13 +2,22 @@ import React, { ComponentType, ReactElement, ReactNode } from 'react'
 import { render, RenderOptions } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './global/styles/theme'
+import { HeaderProvider } from './contexts/HeaderContext'
+import { BagProvider } from './contexts/BagContext'
+import { PaymentFormProvider } from './contexts/PaymentFormContext'
 
 interface AllTheProvidersProps {
   children: ReactNode
 }
 
 const AllTheProviders = ({ children }: AllTheProvidersProps) => (
-  <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+  <HeaderProvider>
+    <BagProvider>
+      <PaymentFormProvider>
+        <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+      </PaymentFormProvider>
+    </BagProvider>
+  </HeaderProvider>
 )
 
 const customRender = (ui: ReactElement, options?: RenderOptions) =>
