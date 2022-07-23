e55d6f331995d2157c21d548b43aa728c713ccdd
Ian Silva <iansilva6@yahoo.com.br>
2022-07-23 11:17:42 -0300

    feat: create success page

--- src/pages/Success/index.jsx
+++ src/pages/Success/index.jsx
@@ -1,5 +1,33 @@
 import React from 'react'
+import loadable from '@loadable/component'
+import { Section } from './style'
+import { DataContext } from '../../context'
+
+const Header = loadable(() => import('../../components/Header'), {
+  resolveComponent: (components) => components.Header
+})
+
+const CartItems = loadable(() => import('../../components/CartItems'), {
+  resolveComponent: (components) => components.CartItems
+})
+
+const CartPrice = loadable(() => import('../../components/CartPrice'), {
+  resolveComponent: (components) => components.CartPrice
+})
 
 export const Success = () => {
-  return <></>
+  const [products, setProducts] = React.useContext(DataContext)
+
+  return (
+    <>
+      <Header />
+      <Section>
+        <CartItems products={products.items} />
+      </Section>
+      <Section>
+        <CartPrice cartPrice={products} />
+      </Section>
+      <br />
+    </>
+  )
 }
---
+++ src/pages/Success/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Section = styled.section`
+  width: 100%;
+  @media (min-width: 1024px) {
+    width: 48%;
+    margin: 0 auto;
+  }
+`
