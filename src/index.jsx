3b052c475ab8389e3cd285731aaf3764ede35c73
Ian Silva <iansilva6@yahoo.com.br>
2022-07-24 09:38:19 -0300

    fix: remove comments

--- src/index.jsx
+++ src/index.jsx
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom'
 import GlobalStyle from './assets/styles/global'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-// import { Provider } from 'react-redux'
-// import store from './redux/store'
 import * as serviceWorker from './serviceWorker'
 
 const Strict = () => (
   <StrictMode>
-    {/* <Provider store={store}> */}
     <GlobalStyle />
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    {/* </Provider> */}
   </StrictMode>
 )
 
