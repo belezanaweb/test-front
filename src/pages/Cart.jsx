b192bfce72f426ad572c82e7a679c3dee2f266d9
João Coqueiro <jaaum.p95@gmail.com>
2022-09-20 16:46:44 -0300

    Faz esboço do carrinho

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default class Cart extends React.Component {
   state = {
-    cartItems: []
+    cartItems: [],
+    totalPrice: ''
   }
 
   componentDidMount() {
@@ -13,23 +14,35 @@ export default class Cart extends React.Component {
     const url = 'http://www.mocky.io/v2/5b15c4923100004a006f3c07'
     const request = await fetch(url)
     const response = await request.json()
-    const { items } = response
+    const { items, subTotal, shippingTotal, discount, total } = response
     this.setState({
-      cartItems: items
+      cartItems: items,
+      subTotal,
+      shippingTotal,
+      discount,
+      total
     })
   }
   render() {
-    const { cartItems } = this.state
+    const { cartItems, subTotal, shippingTotal, discount, total } = this.state
     return (
       <>
+        <p>Produtos</p>
         {cartItems.map(({ product }) => (
-          // Box onde os produtos serão alencados
-          <div>
+          <div key={product.sku}>
+            {/* Box onde os produtos serão alencados */}
             <p>{product.name}</p>
-            <p>{product.priceSpecification.price}</p>
-            <img src={product.imageObjects[0].medium} alt="products" />
+            <img src={product.imageObjects[0].small} alt="products" />
+            <p>R$ {product.priceSpecification.price}</p>
           </div>
         ))}
+        <div>
+          {/* Box onde o valor total vai ficar */}
+          <p>Produtos: R$ {subTotal}</p>
+          <p>Frete: R$ {shippingTotal}</p>
+          <p>Desconto: R$ {discount}</p>
+          <p>Total: R$ {total}</p>
+        </div>
       </>
     )
   }
