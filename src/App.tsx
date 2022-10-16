27ce967883ef4b1b34e577cb4c7dba7e3de666b6
Rodrigo Quintas <rodrigo.santos.quintas@gmail.com>
2022-10-16 19:17:01 -0300

    lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,42 @@ import Header from './components/organisms/Header/Header'
 import Container from './components/organisms/Container/Container'
 import GlobalStyle from './styles/global'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Cart from './pages/Cart/Cart'
-import Payment from './pages/Payment/Payment'
-import Confirmation from './pages/Confirmation/Confirmation'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './styles/theme'
 import { HeaderContextProvider } from './contexts/HeaderContext'
 import { CartContextProvider } from './contexts/CartContext'
 import { PaymentContextProvider } from './contexts/PaymentContext'
+import { lazy, Suspense } from 'react'
+import Spinner from './components/organisms/Spinner/Spinner'
 
-export default function App() {
+const Cart = lazy(() => import('./pages/Cart/Cart'))
+const Payment = lazy(() => import('./pages/Payment/Payment'))
+const Confirmation = lazy(() => import('./pages/Confirmation/Confirmation'))
+
+export default function App(): JSX.Element {
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <HeaderContextProvider>
-          <CartContextProvider>
-            <PaymentContextProvider>
+      <HeaderContextProvider>
+        <CartContextProvider>
+          <PaymentContextProvider>
+            <Suspense fallback={<Spinner />}>
               <Router>
-                <Header />
-                <Container>
-                  <Routes>
-                    <Route path="/" element={<Cart />}></Route>
-                    <Route path="/payment" element={<Payment />}></Route>
-                    <Route path="/confirmation" element={<Confirmation />}></Route>
-                  </Routes>
-                </Container>
+                <ThemeProvider theme={theme}>
+                  <Header />
+                  <Container>
+                    <Routes>
+                      <Route path="/" element={<Cart />}></Route>
+                      <Route path="/payment" element={<Payment />}></Route>
+                      <Route path="/confirmation" element={<Confirmation />}></Route>
+                    </Routes>
+                  </Container>
+                </ThemeProvider>
               </Router>
-            </PaymentContextProvider>
-          </CartContextProvider>
-        </HeaderContextProvider>
-      </ThemeProvider>
+            </Suspense>
+          </PaymentContextProvider>
+        </CartContextProvider>
+      </HeaderContextProvider>
     </>
   )
 }
