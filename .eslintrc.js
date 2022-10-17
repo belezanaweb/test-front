93481b04251dc9c55c9e0ec3e442db9921a69ab3
Rodrigo Quintas <rodrigo.santos.quintas@gmail.com>
2022-10-16 22:26:18 -0300

    remove comments to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,10 +17,8 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    // suppress errors for missing 'import React' in files
     'react/react-in-jsx-scope': 'off',
     "react/prop-types": "off",
-    // allow jsx syntax in js files (for next.js project)
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }] //should add ".ts" if typescript project
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }]
   }
 }
