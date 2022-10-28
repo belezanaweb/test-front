8686e15424cc79d58645f0896f98fee091f501ff
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-10-28 13:55:03 -0300

    connected to the API first time

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Api from '../components/Components'
 
 function List() {
   return (
     <div>
       <p>testando2</p>
       <h1>Login</h1>
+      <p>{Api.urlJson}</p>
     </div>
   )
 }
