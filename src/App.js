0aa28603a1c7987fdb73349a4bc7a1015f1b1d2e
Gustavo Kunde <gustavokunde@gmail.com>
2022-10-29 17:53:45 -0300

    added providers to be able to use cart and windowsize contexts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import { CartProvider } from "./contexts/cart";
+import { DeviceSizeProvider } from "./contexts/deviceSize";
+import ShoppingCart from "./pages/ShoppingCart";
 
 const App = () => (
-  <CartProvider>
-    <div />
-  </CartProvider>
+  <DeviceSizeProvider>
+    <CartProvider>
+      <ShoppingCart />
+    </CartProvider>
+  </DeviceSizeProvider>
 );
 
 export default App;
