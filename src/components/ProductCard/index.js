790068fd36e4f6178a7b457fdf682c66713bac6e
Antonio Thiele <athiele@multiplan.com.br>
2022-10-29 17:23:52 -0300

    formatCurrencyBRL

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,4 +1,7 @@
 import React from 'react'
+
+import formatCurrencyBRL from '../../utils/formatCurrencyBRL'
+
 import { Container } from './styles'
 
 const ProductCard = ({ image, name, price }) => {
@@ -6,7 +9,7 @@ const ProductCard = ({ image, name, price }) => {
     <Container>
       <img src={image} alt={name} />
       <p>{name}</p>
-      <p id="price">{price}</p>
+      <p id="price">{formatCurrencyBRL(price)}</p>
     </Container>
   )
 }
