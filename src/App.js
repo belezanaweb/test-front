cbf176a73dee2e56b801c9d52dc3f9e8357a3cae
Antonio Thiele <athiele@multiplan.com.br>
2022-10-29 11:28:30 -0300

    router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import Router from './router'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 
-const App = () => <div className="App">Boa sorte! 🚀</div>
+const App = () => (
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>
+)
 
 export default App
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Kart from '../pages/Kart'
 
-function App() {
+function Router() {
   return (
-    <Switch>
-      <Route exact path="/" component={Kart} unauthenticated />
-    </Switch>
+    <Routes>
+      <Route path="/" element={<Kart />} />
+    </Routes>
   )
 }
 
-export default App
+export default Router
