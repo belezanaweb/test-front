2c548465e74e7bc28c354a291aa247c7aba98e51
Antonio Thiele <athiele@multiplan.com.br>
2022-10-29 11:50:39 -0300

    API call

--- src/pages/Kart/index.js
+++ src/pages/Kart/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { useEffect } from 'react'
+import requestAPI from '../../utils/requestAPI'
 
 const Kart = () => {
-  console.log('cart')
+  useEffect(() => {
+    requestAPI({ url: 'http://www.mocky.io/v2/5b15c4923100004a006f3c07' }).then((resp) =>
+      console.log(resp.data)
+    )
+  }, [])
 
   return <h1>Kart</h1>
 }
