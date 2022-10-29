760a95f068e942828f08eb4ad7ed1f2f116dee85
Antonio Thiele <athiele@multiplan.com.br>
2022-10-29 14:28:55 -0300

    import navbar

--- src/pages/Kart/index.js
+++ src/pages/Kart/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import requestAPI from '../../utils/requestAPI'
 
+import NavBar from '../../components/NavBar'
+
 const Kart = () => {
   useEffect(() => {
     requestAPI({ url: 'http://www.mocky.io/v2/5b15c4923100004a006f3c07' }).then((resp) =>
@@ -10,6 +12,7 @@ const Kart = () => {
 
   return (
     <div>
+      <NavBar step={'sacola'} />
       <h1>Kart</h1>
     </div>
   )
