39122cd0bea23dda9b46620dc4479a4b0d8c64dd
Antonio Thiele <athiele@multiplan.com.br>
2022-10-29 14:26:14 -0300

    setps

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const NavBar = () => {
-  return <h1>NavBar</h1>
+const NavBar = ({ actualStep }) => {
+  const steps = ['sacola', 'pagamento', 'confirmação']
+
+  return (
+    <div>
+      {steps.map((step) => (
+        <p key={step}>{step}</p>
+      ))}
+    </div>
+  )
 }
 
 export default NavBar
