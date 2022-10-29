ba3a727d1250ef9ccea3f1fd13303b1725ddd7cc
Antonio Thiele <athiele@multiplan.com.br>
2022-10-29 19:09:02 -0300

    padding navbar

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components'
 export const Container = styled.div`
   display: flex;
   background: white;
-  padding: 12px;
+  padding: 12px 24px;
   justify-content: space-between;
   box-shadow: 5px 0 4px 1px rgba(0, 0, 29, 0.12);
 `
