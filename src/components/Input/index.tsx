54227ab6376b78bf02024fd105bca3561da9990b
Gustavo Kunde <gustavokunde@gmail.com>
2022-10-29 23:28:27 -0300

    changing textfield to react input mask

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,59 +1,78 @@
-import React from 'react'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import TextField from '@mui/material/TextField'
-import { Container } from './styles'
-import { colors } from '../../utils/style-config'
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Container, InputMask } from "./styles";
+import { colors } from "../../utils/style-config";
+import { InputBaseComponentProps } from "@mui/material";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: colors.inputFocusColor
-    }
+      main: colors.inputFocusColor,
+    },
   },
   typography: {
-    fontFamily: ['"Helvetica Neue"', 'Helvetica', 'Arial', 'sans-serif'].join(',')
+    fontFamily: ['"Helvetica Neue"', "Helvetica", "Arial", "sans-serif"].join(
+      ","
+    ),
   },
   components: {
     MuiTextField: {
       variants: [
         {
-          props: { size: 'small' },
+          props: { size: "small" },
           style: {
-            width: 320,
-            height: 45,
-            fontWeight: 700
-          }
-        }
-      ]
-    }
-  }
-})
+            maxWidth: "100%",
+            fontWeight: 700,
+          },
+        },
+      ],
+      styleOverrides: {
+        root: {
+          letterSpacing: "1.37px",
+        },
+      },
+    },
+  },
+});
 
 interface IProps {
+  as?: any;
+  name?: string;
   label: string;
+  value?: string;
+  mask?: string | RegExp[];
   placeholder?: string;
   error?: boolean;
-  helperText?: string;
+  helperText?: React.ReactNode;
+  inputComponent?: React.ElementType<InputBaseComponentProps>;
   onChange: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
 }
 
-const Input = ({ label, placeholder, error, helperText, onChange }: IProps) => {
+const Input = ({
+  name,
+  label,
+  value,
+  mask,
+  placeholder,
+  error,
+  helperText,
+  onChange,
+}: IProps) => {
   return (
     <ThemeProvider theme={theme}>
       <Container>
         <label>{label}</label>
-        <TextField
-          id="outlined-basic"
-          variant="outlined"
-          size="small"
-          error={error}
+        <InputMask
+          name={name}
+          mask={mask || ""}
           placeholder={placeholder}
-          helperText={helperText}
+          error={error}
           onChange={onChange}
         />
+        <span>{helperText}</span>
       </Container>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,5 +1,6 @@
-import styled from 'styled-components'
-import { colors } from '../../utils/style-config'
+import ReactInputMask from "react-input-mask";
+import styled from "styled-components";
+import { colors } from "../../utils/style-config";
 
 export const Container = styled.div`
   display: flex;
@@ -7,7 +8,39 @@ export const Container = styled.div`
   label {
     font-weight: 700;
     line-height: 14px;
+    font-size: 12px;
     color: ${colors.inputLabelColor};
-    margin-bottom: 2px;
+    margin-bottom: 5px;
   }
-`
+
+  span {
+    color: ${colors.errorColor};
+    font-size: 12px;
+  }
+`;
+
+export const InputMask = styled(ReactInputMask)`
+  height: 45px;
+  padding: 14px;
+  border-radius: 3px;
+  border: ${({ error }: { error?: boolean }) =>
+    error
+      ? `1px solid ${colors.errorColor}`
+      : `1px solid ${colors.inputBorderColor}`};
+
+  outline: none;
+  box-shadow: ${({ error }: { error?: boolean }) =>
+    error ? "unset" : "inset 0 1px 2px 0 rgba(0, 0, 0, 0.2)"};
+  font-size: 16px;
+  letter-spacing: 1.37px;
+
+  &:focus {
+    outline: none;
+    border: 1px solid ${colors.inputFocusColor};
+    box-shadow: unset;
+  }
+  &::placeholder {
+    color: ${({ error }: { error?: boolean }) =>
+      error ? colors.errorColor : colors.inputPlaceholder};
+  }
+`;
