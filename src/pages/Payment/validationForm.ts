ebf3e58709f1e108afe3afdca215fb9f6b34b818
Gustavo Kunde <gustavokunde@gmail.com>
2022-10-30 20:19:31 -0300

    improving yup validations on payment

--- src/pages/Payment/validationForm.ts
+++ src/pages/Payment/validationForm.ts
@@ -1,11 +1,34 @@
 import { object, string } from "yup";
 
+const securityCodeValidation = /^\d{3,}/;
+const creditCardNumberValidation = /^\d{16,}/;
+const validateDateValidation = /^\d{6,}/;
+
 export const validationSchema = object({
-  creditCardNumber: string().required("Número do cartão é obrigatório."),
+  creditCardNumber: string()
+    .test({
+      name: "cardNumber",
+      exclusive: false,
+      message: "Número do cartão inválido.",
+      params: {},
+      test: function (value: any) {
+        return creditCardNumberValidation.test(value?.replace(/[^\d]/g, ""));
+      },
+    })
+    .required("Número do cartão é obrigatório."),
   ownerName: string().required("Nome do titular é obrigatório."),
-  validateDate: string().required("Data de validade é obrigatória."),
+  validateDate: string()
+    .required("Data de validade é obrigatória.")
+    .test({
+      name: "cardNumber",
+      exclusive: false,
+      message: "Número do cartão inválido.",
+      params: {},
+      test: function (value: any) {
+        return validateDateValidation.test(value?.replace(/[^\d]/g, ""));
+      },
+    }),
   securityCode: string()
     .required("CVV é obrigatório.")
-    .min(3, "CVV inválido.")
-    .max(4, "CVV inválido."),
+    .matches(securityCodeValidation, "cvv inválido."),
 });
