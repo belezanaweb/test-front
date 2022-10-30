123769addf3cdf45ae1c77ebb2d56ca2904bd39c
Gustavo Kunde <gustavokunde@gmail.com>
2022-10-29 23:29:27 -0300

    removing unused code

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,39 +1,6 @@
 import React from "react";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Container, InputMask } from "./styles";
-import { colors } from "../../utils/style-config";
 import { InputBaseComponentProps } from "@mui/material";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: colors.inputFocusColor,
-    },
-  },
-  typography: {
-    fontFamily: ['"Helvetica Neue"', "Helvetica", "Arial", "sans-serif"].join(
-      ","
-    ),
-  },
-  components: {
-    MuiTextField: {
-      variants: [
-        {
-          props: { size: "small" },
-          style: {
-            maxWidth: "100%",
-            fontWeight: 700,
-          },
-        },
-      ],
-      styleOverrides: {
-        root: {
-          letterSpacing: "1.37px",
-        },
-      },
-    },
-  },
-});
 
 interface IProps {
   as?: any;
@@ -59,19 +26,17 @@ const Input = ({
   onChange,
 }: IProps) => {
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <label>{label}</label>
-        <InputMask
-          name={name}
-          mask={mask || ""}
-          placeholder={placeholder}
-          error={error}
-          onChange={onChange}
-        />
-        <span>{helperText}</span>
-      </Container>
-    </ThemeProvider>
+    <Container>
+      <label>{label}</label>
+      <InputMask
+        name={name}
+        mask={mask || ""}
+        placeholder={placeholder}
+        error={error}
+        onChange={onChange}
+      />
+      <span>{helperText}</span>
+    </Container>
   );
 };
 
