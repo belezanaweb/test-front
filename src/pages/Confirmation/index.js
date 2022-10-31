64bb1e7fd364f127475746f304b3a2e8bcba8152
Antonio Thiele <athiele@multiplan.com.br>
2022-10-31 14:12:26 -0300

    Confirmation desktop

--- src/pages/Confirmation/index.js
+++ src/pages/Confirmation/index.js
@@ -10,38 +10,44 @@ import Total from '../../components/Total'
 
 import Success from '../../assets/success.png'
 
-import { Container, SuccessWarper, PaymentContainer } from './styles'
+import { Root, Container, Content, Warper, SuccessWarper, PaymentContainer } from './styles'
 
 const Confirmation = () => {
   const { formData } = useCreditCard()
   const { items, totalData } = useCart()
 
   return (
-    <div>
+    <Root>
       <NavBar actualStep={'confirmation'} />
-      <SuccessWarper>
-        <img src={Success} alt={'success'} />
-        <p>compra efetuada com sucesso</p>
-      </SuccessWarper>
       <Container>
-        <h1>pagamento</h1>
-        <PaymentContainer>
-          <Card>
-            <p>{`**** . **** . **** . ${formData.number.substring(formData.number.length - 4)}`}</p>
-            <p>{formData.name}</p>
-            <p>{formData.expiry}</p>
-          </Card>
-        </PaymentContainer>
-        <h1>produtos</h1>
-        <ProductsList items={items} noPrice />
-        <Total
-          subTotal={totalData.subTotal}
-          shipping={totalData.shippingTotal}
-          discount={totalData.discount}
-          total={totalData.total}
-        />
+        <Content>
+          <SuccessWarper>
+            <img src={Success} alt={'success'} />
+            <p>compra efetuada com sucesso</p>
+          </SuccessWarper>
+          <Warper>
+            <h1>pagamento</h1>
+            <PaymentContainer>
+              <Card>
+                <p>{`**** . **** . **** . ${formData.number.substring(
+                  formData.number.length - 4
+                )}`}</p>
+                <p>{formData.name}</p>
+                <p>{formData.expiry}</p>
+              </Card>
+            </PaymentContainer>
+            <h1>produtos</h1>
+            <ProductsList items={items} noPrice />
+            <Total
+              subTotal={totalData.subTotal}
+              shipping={totalData.shippingTotal}
+              discount={totalData.discount}
+              total={totalData.total}
+            />
+          </Warper>
+        </Content>
       </Container>
-    </div>
+    </Root>
   )
 }
 
--- src/pages/Confirmation/styles.js
+++ src/pages/Confirmation/styles.js
@@ -1,6 +1,13 @@
 import styled from 'styled-components'
+import { ThemeRoot, ThemeContainer, ThemeContent } from '../../styles/theme'
 
-export const Container = styled.div`
+export const Root = styled(ThemeRoot)``
+
+export const Container = styled(ThemeContainer)``
+
+export const Content = styled(ThemeContent)``
+
+export const Warper = styled.div`
   padding: 12px;
   h1 {
     text-transform: uppercase;
