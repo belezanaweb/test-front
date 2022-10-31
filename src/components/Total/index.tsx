90ad04bff9b2ab5e9d3ed59bb8037140612f969f
Antonio Thiele <athiele@multiplan.com.br>
2022-10-31 20:35:36 -0300

    ts total

--- src/components/Total/index.tsx
+++ src/components/Total/index.tsx
@@ -4,23 +4,30 @@ import formatCurrencyBRL from '../../utils/formatCurrencyBRL'
 
 import { Container, Content } from './styles'
 
-const Total = ({ subTotal, shipping, discount, total }) => (
+interface Props {
+  subTotal: number
+  shipping: number
+  discount: number
+  total: number
+}
+
+const Total: React.FC<Props> = ({ subTotal, shipping, discount, total }) => (
   <Container>
     <Content>
       <p>produtos</p>
-      <p>{formatCurrencyBRL(parseFloat(subTotal))}</p>
+      <p>{formatCurrencyBRL(subTotal)}</p>
     </Content>
     <Content>
       <p>frete</p>
-      <p>{formatCurrencyBRL(parseFloat(shipping))}</p>
+      <p>{formatCurrencyBRL(shipping)}</p>
     </Content>
     <Content className={'orange'}>
       <p>desconto</p>
-      <p>{`- ${formatCurrencyBRL(parseFloat(discount))}`}</p>
+      <p>{`- ${formatCurrencyBRL(discount)}`}</p>
     </Content>
     <Content>
       <p>total</p>
-      <p>{formatCurrencyBRL(parseFloat(total))}</p>
+      <p>{formatCurrencyBRL(total)}</p>
     </Content>
   </Container>
 )
