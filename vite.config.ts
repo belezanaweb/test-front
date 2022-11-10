e24b0ac922da8c26786f8c04352930736b969a8c
Franklin Javier <franklinjalves@gmail.com>
2022-11-10 14:33:37 +0000

    test: better test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./src/test/setup-test.ts']
+  },
   server: {
     port: 3000
   }
