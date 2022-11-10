0bfdc4dd427488948018327facbe23113d515a28
Franklin Javier <franklinjalves@gmail.com>
2022-11-10 11:55:40 +0000

    chore: use vite instead of CRA ⚡️

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/beleza/i)
+  const linkElement = getByText(/🚀/i)
   expect(linkElement).toBeInTheDocument()
 })
