9c812f8b60ebce3a32cc86278c472204291ebd98
Hugo Renzi <hugorenzi.nave@gmail.com>
2023-01-21 17:18:54 -0300

    feat: adding main app changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { GlobalStyle } from "./theme/globals";
+import Theme from "./theme";
+import Home from "./pages/commom/Home";
+import Cart from "./pages/checkout/Cart";
+import Payment from "./pages/checkout/Payment";
+import Confirmation from "./pages/checkout/Confirmation";
+import NotFound from "./pages/error/NotFound";
 
-export default function App() {
-  return <div className="wrapper">Boa sorte! 🚀</div>
+const router = createBrowserRouter([{
+    path: "/",
+    element: <Home />
+  },{
+    path: "/checkout/cart",
+    element: <Cart />
+  }, {
+    path: "/checkout/payment",
+    element: <Payment />
+  }, {
+    path: "/checkout/confirmation",
+    element: <Confirmation />
+  }, {
+    path: '*',
+    element: <NotFound />
+  }
+]);
+
+function App() {
+  return (
+    <Theme>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </Theme>
+  );
 }
+
+export default App;
