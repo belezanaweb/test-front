9965fec2abb86ada1cc7b887be59c04287534373
James da Silva <jamesodas@gmail.com>
2023-02-06 20:55:36 -0300

    chore: add eslint to standardize the code

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
+import eslint from 'vite-plugin-eslint'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
     react(), 
     ssr(), 
+    eslint(), 
     tsconfigPaths()
   ],
   test: {
