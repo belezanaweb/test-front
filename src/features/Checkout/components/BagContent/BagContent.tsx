8ffb16d4aca53f4bb4b06b69f924e862a8af0ebf
James da Silva <jamesodas@gmail.com>
2023-02-08 20:43:09 -0300

    feat: improvement BagContent component

--- src/features/Checkout/components/BagContent/BagContent.tsx
+++ src/features/Checkout/components/BagContent/BagContent.tsx
@@ -3,25 +3,28 @@ import './style.scss'
 import { Product } from './types'
 
 interface BagContentProps {
+  title?: string
   products: Product[]
+  showPrices?: boolean
 }
 
-export function BagContent({ products }: BagContentProps) {
+export function BagContent({ title, products, showPrices = true }: BagContentProps) {
   return (
-    <section>
-      <ul className='product-list '>
-        {products?.map(productItem => {
-          const { product } = productItem
-          const { name, imageObjects, priceSpecification: { maxPrice, price } } = product
-          return(
-            <li className='product-item' key={`sku-${product.sku}`}>
-              <article>
-                <div className='product-left-box'>
-                  <span className='product-img-box'>
-                    <img src={imageObjects[0].thumbnail} alt={name} />
-                  </span>
-                  <p>{name}</p>
-                </div>
+    <ul className='product-list '>
+      {title && <h2>{title}</h2>}
+      {products?.map(productItem => {
+        const { product } = productItem
+        const { name, imageObjects, priceSpecification: { maxPrice, price } } = product
+        return(
+          <li className='product-item' key={`sku-${product.sku}`}>
+            <article>
+              <div className='product-left-box'>
+                <span className='product-img-box'>
+                  <img src={imageObjects[0].thumbnail} alt={name} />
+                </span>
+                <p>{name}</p>
+              </div>
+              {showPrices && 
                 <span className='product-price-box'>
                   { 
                     maxPrice && 
@@ -33,11 +36,11 @@ export function BagContent({ products }: BagContentProps) {
                     <Currency>{price}</Currency>
                   </span>
                 </span>
-              </article>
-            </li>
-          )
-        })}
-      </ul>
-    </section>
+              }
+            </article>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
