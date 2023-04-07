9b5f66b433939094b182c721d09dc6ddbdda4281
Ayrton <ayrtondcgomes@gmail.com>
2023-04-07 17:56:07 -0300

    fix: ThemeProvider error on type

--- src/types/styled.d.ts
+++
@@ -1,7 +0,0 @@
-import themeVariables from 'styles/theme';
-
-type ThemeInterface = typeof themeVariables
-
-declare module "styled-components" {
-  interface DefaultTheme extends ThemeInterface {}
-}
