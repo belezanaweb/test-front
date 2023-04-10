218c9542e134c2d601844e3f69878597f5d70ef6
Ayrton <ayrtondcgomes@gmail.com>
2023-04-10 17:40:05 -0300

    chore: Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import { NavLink } from 'react-router-dom'
+
+import { CustomNavLink, HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <nav>
+        <CustomNavLink to="/bag">Sacola</CustomNavLink>
+        <CustomNavLink to="/payment">Pagamento</CustomNavLink>
+        <CustomNavLink to="/confirmation">Confirmação</CustomNavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,36 @@
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  height: 37px;
+  background-color: ${(props) => props.theme.white};
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  nav {
+    display: flex;
+    flex-direction: row;
+    flex: 0 0 auto;
+  }
+`
+export const CustomNavLink = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  color: ${(props) => props.theme['gray-300']};
+  font-family: 'Arial';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 24px;
+  border-bottom: 1px solid ${(props) => props.theme['gray-300']};
+  padding: 0 8px;
+
+  &:hover {
+    border-bottom: 4px solid ${(props) => props.theme.black};
+  }
+
+  &.active {
+    border-bottom: 4px solid ${(props) => props.theme.black};
+  }
+`
