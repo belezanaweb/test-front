f0f5b20593dc24b6eda2dc194cb78b52316e4776
Ayrton <ayrtondcgomes@gmail.com>
2023-04-11 11:31:03 -0300

    chore: add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import { BrowserRouter, useNavigate } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { theme } from './styles/theme'
 
+const queryClient = new QueryClient()
+
 export function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-
-      <GlobalStyle />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
