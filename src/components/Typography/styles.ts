e8fbd498a005caa202fd6045418678fd5182983c
Ayrton <ayrtondcgomes@gmail.com>
2023-04-11 16:13:56 -0300

    chore: Typography components

---
+++ src/components/Typography/styles.ts
@@ -0,0 +1,39 @@
+import styled, { css } from 'styled-components'
+import { theme } from '../../styles/theme'
+
+export type TypographyProps = {
+  color?: keyof typeof theme.colors
+  size?: keyof typeof theme.fontSizes
+  fontFamily?: keyof typeof theme.fontFamilies
+  fontWeight?: 100 | 400 | 700,
+}
+
+export const Heading = styled('h1')<TypographyProps>`
+  ${({
+    color = 'black',
+    size = 'medium',
+    fontWeight = 700,
+    fontFamily = 'primary'
+  }) => css`
+    font-size: ${theme.fontSizes[size]};
+    color: ${theme.colors[color]};
+    font-family: ${theme.fontFamilies[fontFamily]};
+    font-weight: ${fontWeight};
+    margin-block-start: 0;
+    margin-block-end: 0;
+  `}
+`
+
+export const TextBody = styled('span')<TypographyProps>`
+  ${({
+    color = 'black',
+    size = 'small',
+    fontWeight = 400,
+    fontFamily = 'primary'
+  }) => css`
+    font-size: ${theme.fontSizes[size]};
+    color: ${theme.colors[color]};
+    font-family: ${theme.fontFamilies[fontFamily]};
+    font-weight: ${fontWeight};
+  `}
+`
