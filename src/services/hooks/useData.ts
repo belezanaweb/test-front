e7ffbcc5d03c5dca57b68ffe2ed0eee9d0bd8ce3
Ayrton <ayrtondcgomes@gmail.com>
2023-04-11 13:01:50 -0300

    refactor: improve useData hook code

--- src/services/hooks/useData.ts
+++ src/services/hooks/useData.ts
@@ -4,10 +4,12 @@ import { api } from '../api'
 export function useData() {
   return useQuery({
     queryKey: ['checkout-data'],
-    queryFn: async () => {
-      const response = await api.get('d6e9a93f-9741-4494-b81e-637a8e9b8ddd')
-
-      return response
-    }
+    queryFn: getData
   })
+}
+
+export async function getData(): Promise<any> {
+  const response = await api.get('d6e9a93f-9741-4494-b81e-637a8e9b8ddd')
+
+  return response
 }
