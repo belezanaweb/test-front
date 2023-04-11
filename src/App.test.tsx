b6ea35c4235cdc81df8114d82116b3e97b1b5d46
Ayrton <ayrtondcgomes@gmail.com>
2023-04-11 10:43:24 -0300

    fix: eslint config and react-router-dom config

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-
-import App from './App'
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/🚀/i)
-  expect(linkElement).toBeInTheDocument()
-})
