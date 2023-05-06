1b98c9030994504cf90e77940bf2e6a776f0c556
Victor Bezerra <vctr.mb@gmail.com>
2023-05-06 15:12:35 -0300

    adding react-router and query

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, Navigate, Outlet, RouterProvider } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Outlet />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/cart" />
+      },
+      {
+        path: 'cart',
+        element: <App />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
