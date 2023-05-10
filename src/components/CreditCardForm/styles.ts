05f4650d938d0d7732b0a3b5f77bfd7a6e0666f2
Lucas Gomes <lucasgomes.dky@gmail.com>
2023-05-10 20:51:56 -0300

    style: decrease the space between the input fields

--- src/components/CreditCardForm/styles.ts
+++ src/components/CreditCardForm/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 10px;
 `
 
 export const InputGroup = styled.div`
