7459a804849071bcbae43e913c98b3c7944e3074
Marlon Felix <marlon.santos.ext@dasa.com.br>
2023-05-17 08:57:40 -0300

    feat: using unstyled tab components

--- src/containers/PageCheckout/PageCheckout.tsx
+++ src/containers/PageCheckout/PageCheckout.tsx
@@ -1,13 +1,43 @@
 import { FC } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+
+import CheckoutCart from "../../components/Checkout/CheckoutCart";
+import CheckoutPayment from "../../components/Checkout/CheckoutPayment";
+import CheckoutConfirm from "../../components/Checkout/CheckoutConfirm";
+
+import { Tabs } from "../../shared"
 
 export interface PageCheckoutProps {
   className?: string;
 }
 
 const PageCheckout: FC<PageCheckoutProps> = () => {
+  const navigate = useNavigate()
+  const params = useParams()
+
+  const tabParams = (value: string) => {
+    navigate(`/${value}`);
+  }
+  
   return (
     <>
-
+      <Tabs active={params.id?.toString()} onChange={tabParams} items={[
+        {
+          key: 'cart',
+          title: 'Sacola',
+          children: <CheckoutCart />
+        },
+        {
+          key: 'payment',
+          title: 'Pagamento',
+          children: <CheckoutPayment />
+        },
+        {
+          key: 'confirm',
+          title: 'Confirmação',
+          children: <CheckoutConfirm />
+        }
+      ]} />
     </>
   );
 };
