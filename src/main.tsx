6c2b59469ab23aac039434d5b1a5893e64bf7125
Marlon Felix <marlon.santos.ext@dasa.com.br>
2023-05-17 08:53:04 -0300

    fix: indent and created variable root

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
