b59f8c1f87e697d659e25b35522679bf03b4131a
Marlon Felix <marlon.santos.ext@dasa.com.br>
2023-05-18 00:45:27 -0300

    refactor: refactor variables styled

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,14 +1,13 @@
-const Theme = {
+export const theme = {
+  borderRadius: '4px',
+  breakpoints: {
+    mobile: '768px',
+    tablet: '992px',
+    desktop: '1200px'
+  },
   colors: {
     background: "#F5F5F5",
-    primary: {
-      main: "#9222DC",
-    },
-    secundary: {
-      main: "#000000",
-    }
-  },
-  container: '1200px'
+    primary: "#9222DC",
+    secondary: "#000000",
+  }
 }
-
-export default Theme
