efd44e9d54a879e50fba7cf0293030e9d671e640
Marlon Felix <marlon.santos.ext@dasa.com.br>
2023-05-18 21:24:44 -0300

    refactor: styled product card

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,39 @@
+import { Product } from '../../stores'
+
+import {
+  ProductCardStyled,
+  ProductCardThumb,
+  ProductCardTitle,
+  ProductCardPrice,
+  ProductCardPriceFrom,
+  ProductCardPriceTo } from './ProductCard.styled'
+import { ProductCardProps } from './ProductCard.types'
+
+import { formatToBRL } from '../../utils/formatCurrency'
+
+export const ProductCard = (props: Product & ProductCardProps) => {
+  return (
+    <ProductCardStyled>
+      <ProductCardThumb className="product-thumbnail">
+        <img src={props.imageObjects[0].small} alt="" />
+      </ProductCardThumb>
+
+      <ProductCardTitle>
+        {props.name}
+      </ProductCardTitle>
+
+      {!props.hideProductPrice && (
+        <ProductCardPrice>
+          {props.priceSpecification.maxPrice > props.priceSpecification.price && (
+            <ProductCardPriceFrom>
+              {formatToBRL.format(props.priceSpecification.maxPrice)}
+            </ProductCardPriceFrom>
+          )}
+          <ProductCardPriceTo>
+            {formatToBRL.format(props.priceSpecification.price)}
+          </ProductCardPriceTo>
+        </ProductCardPrice>
+      )}
+    </ProductCardStyled>
+  )
+}
