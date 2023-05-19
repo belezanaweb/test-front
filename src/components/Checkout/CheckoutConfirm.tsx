f1d0f65b30b3c332479c343f6b08597bd567da03
Marlon Felix <marlon.santos.ext@dasa.com.br>
2023-05-18 21:29:20 -0300

    refactor: styled step checkout confirm

--- src/components/Checkout/CheckoutConfirm.tsx
+++ src/components/Checkout/CheckoutConfirm.tsx
@@ -1,9 +1,46 @@
-const CheckoutConfirm = () => {
+import { useNavigate } from 'react-router-dom'
+import { useCheckout } from '../../stores'
+
+import { Card, Button } from '../../shared'
+
+import { OrderSummary } from '../OrderSummary'
+import { ProductList } from '../ProductList'
+
+import {
+  CheckoutCardContainer,
+  CheckoutCardContent,
+  CheckoutCardDetails,
+  CheckoutCardDetailsContainer,
+  CheckoutResume,
+  CheckoutResumeTitle
+} from './Checkout.styled'
+
+export const CheckoutConfirm = () => {
+  const { checkout } = useCheckout()
+  const navigate = useNavigate()
+
   return (
-    <div>
-      confirmar
-    </div>
+    <CheckoutCardContainer>
+      <CheckoutCardContent>
+        <Card centered>
+          <CheckoutResume>
+            <CheckoutResumeTitle>Compra efetuada com sucesso</CheckoutResumeTitle>
+            <span>{`****.****.****.${checkout?.card?.number.substr(-4)}`}</span>
+            <span>{checkout?.card?.holderName}</span>
+            <span>{checkout?.card?.expiry}</span>
+          </CheckoutResume>
+        </Card>
+        <Card>
+          <ProductList title="Produtos" />
+        </Card>
+      </CheckoutCardContent>
+
+      <CheckoutCardDetails>
+        <CheckoutCardDetailsContainer>
+          <OrderSummary />
+          <Button onClick={() => navigate('/')} $block variant="dark">Voltar ao início do protótipo</Button>
+        </CheckoutCardDetailsContainer>
+      </CheckoutCardDetails>
+    </CheckoutCardContainer>
   )
 }
-
-export default CheckoutConfirm;
