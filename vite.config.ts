4d669e8805d4349afedd41798227886b68071841
Marlon Felix <marlon.santos.ext@dasa.com.br>
2023-05-19 10:25:12 -0300

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
-    globals: true,
     environment: 'happy-dom',
     setupFiles: ['./src/test/setup-test.ts']
   },
