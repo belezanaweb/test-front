d27a4b7c1fe109e0cd25c4b40eec063f2391b1ae
Marlon Felix <marlon.santos.ext@dasa.com.br>
2023-05-18 21:14:44 -0300

    chore: styled page checkout and mapping routes tab

---
+++ src/containers/PageCheckout/PageCheckout.styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const PageCheckoutContainer = styled.div`
+  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+    margin-top: 20px;
+  }
+`;
--- src/containers/PageCheckout/PageCheckout.tsx
+++ src/containers/PageCheckout/PageCheckout.tsx
@@ -1,52 +1,67 @@
-import { FC } from "react"
-import { useNavigate, useParams } from "react-router-dom"
+import { useEffect } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useCheckout } from '../../stores'
 
-import CheckoutCart from "../../components/Checkout/CheckoutCart";
-import CheckoutPayment from "../../components/Checkout/CheckoutPayment";
-import CheckoutConfirm from "../../components/Checkout/CheckoutConfirm";
+import { PageCheckoutContainer } from './PageCheckout.styled'
+
+import {
+  CheckoutCart,
+  CheckoutPayment,
+  CheckoutConfirm,
+} from '../../components'
+import { Tabs } from '../../shared'
 
 import BagIcon from '../../assets/images/masks/bag-mask.svg'
 import CardMask from '../../assets/images/masks/card-mask.svg'
 import CheckIcon from '../../assets/images/masks/check-mask.svg'
 
-import { Tabs } from "../../shared"
-
 export interface PageCheckoutProps {
-  className?: string;
+  className?: string
 }
 
-const PageCheckout: FC<PageCheckoutProps> = () => {
+export const PageCheckout: React.FC<PageCheckoutProps> = () => {
   const navigate = useNavigate()
-  const params = useParams()
+  const locate = useLocation()
+  const { checkout } = useCheckout()
 
-  const tabParams = (value: string) => {
-    navigate(`/${value}`);
+  const active = locate.pathname.toString()
+  const isCheckoutPaymentDisabled = !checkout || Object.values(checkout.card).some((value) => !value)
+
+  const items = [
+    {
+      key: '/',
+      title: 'Sacola',
+      icon: BagIcon,
+      children: <CheckoutCart onPayment={() => navigateTo('payment')} />,
+    },
+    {
+      key: '/payment',
+      title: 'Pagamento',
+      icon: CardMask,
+      children: <CheckoutPayment />,
+    },
+    {
+      key: '/confirm',
+      title: 'Confirmação',
+      icon: CheckIcon,
+      children: <CheckoutConfirm />,
+      disabled: isCheckoutPaymentDisabled,
+    },
+  ]
+
+  const navigateTo = (value: string) => {
+    navigate(value)
   }
-  
-  return (
-    <>
-      <Tabs active={params.id?.toString()} onChange={tabParams} items={[
-        {
-          key: 'cart',
-          title: 'Sacola',
-          icon: BagIcon,
-          children: <CheckoutCart />
-        },
-        {
-          key: 'payment',
-          title: 'Pagamento',
-          icon: CardMask,
-          children: <CheckoutPayment />
-        },
-        {
-          key: 'confirm',
-          title: 'Confirmação',
-          icon: CheckIcon,
-          children: <CheckoutConfirm />
-        }
-      ]} />
-    </>
-  );
-};
 
-export default PageCheckout;
+  useEffect(() => {
+    if (active === '/confirm' && isCheckoutPaymentDisabled) {
+      navigate('/')
+    }
+  }, [active])
+
+  return (
+    <PageCheckoutContainer>
+      <Tabs active={active} onChange={navigateTo} items={items} />
+    </PageCheckoutContainer>
+  )
+}
