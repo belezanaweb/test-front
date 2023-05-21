560b6f0cf41ca551dc5153d2fc1a63722b0d441b
Lucas Rozalem <lrozalem@gmail.com>
2023-05-21 17:47:44 -0300

    changing api call to services folder

--- src/contexts/Checkout.tsx
+++ src/contexts/Checkout.tsx
@@ -2,6 +2,7 @@ import { ReactNode, createContext, useEffect, useState } from 'react'
 
 import { PaymentFormData  } from '../pages/Payment';
 import { useNavigate } from 'react-router-dom';
+import { checkoutService } from '../services/checkout.service';
 
 interface Product {
   name: string;
@@ -56,32 +57,17 @@ export function CheckoutContextProvider({ children }: CheckoutContextProviderPro
 
 useEffect(()=>{
   setIsLoading(true);
-  fetch('https://run.mocky.io/v3/d6e9a93f-9741-4494-b81e-637a8e9b8ddd')
-    .then((response) => response.json())
-    .then((data) => {
-    let amountItems = 0;
-    const  cart = {
-      subTotal: data.subTotal,
-      shippingTotal: data.shippingTotal,
-      discount: data.discount,
-      total: data.total,
-      items: data.items.map((element: any) => {
-        amountItems = amountItems + element.quantity
-        return { 
-          product: {
-            sku: element.product.sku,
-            name: element.product.name,
-            image: element.product.imageObjects[0].small,
-            price: element.product.priceSpecification.price,
-            maxPrice: element.product.priceSpecification.maxPrice
-          }
-        }
-      }),
-      amountItems: amountItems,
-     }
-     setIsLoading(false);
-     return setData(cart)
-    })
+
+  async function checkout(){
+    const data = await checkoutService();
+    
+    setIsLoading(false);
+
+    setData(data)
+   }
+
+   checkout();
+  
 },[])
 
   function resetPayment() {
---
+++ src/services/checkout.service.ts
@@ -0,0 +1,33 @@
+export const checkoutService = async (): Promise<any> => {
+  const data = await fetch('https://run.mocky.io/v3/d6e9a93f-9741-4494-b81e-637a8e9b8ddd')
+    .then((response) => response.json())
+    .then((data) => {
+      let amountItems = 0
+      const cart = {
+        subTotal: data.subTotal,
+        shippingTotal: data.shippingTotal,
+        discount: data.discount,
+        total: data.total,
+        items: data.items.map((element: any) => {
+          amountItems = amountItems + element.quantity
+          return {
+            product: {
+              sku: element.product.sku,
+              name: element.product.name,
+              image: element.product.imageObjects[0].small,
+              price: element.product.priceSpecification.price,
+              maxPrice: element.product.priceSpecification.maxPrice
+            }
+          }
+        }),
+        amountItems: amountItems
+      }
+
+      return cart
+    }).catch(()=>{
+        return null
+    })
+ 
+
+  return data
+}
