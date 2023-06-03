a67aba4ecd1286d9e0b2ef96f73431ac5b5f057d
blueskiy <filipeandres@hotmail.com>
2023-06-03 20:10:58 -0300

    feat: react-query/cart service initial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
 import Text from '../components/shared/Text';
+import { fetchCartProducts } from '@/services/cart';
+import { useQuery, dehydrate, QueryClient } from '@tanstack/react-query';
 
-export default function Home() {
-  return <Text>Default</Text>;
+export default function Cart() {
+  const { data } = useQuery({
+    queryKey: ['cart'],
+    queryFn: fetchCartProducts,
+  });
+
+  return <Text>{data.subTotal}</Text>;
+}
+
+export async function getServerSideProps() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(['cart'], fetchCartProducts);
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
 }
