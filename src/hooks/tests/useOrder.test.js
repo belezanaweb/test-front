49501f6d102ef4c67f1f143eed231588ce61231f
blueskiy <filipeandres@hotmail.com>
2023-06-06 02:24:41 -0300

    chore: remove unnecessary package @testing-library/react-hooks

--- src/hooks/tests/useOrder.test.js
+++ src/hooks/tests/useOrder.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 import { normalizeCartData, normalizeProductData, useOrder } from '../useOrder';
 
 jest.mock('@tanstack/react-query', () => ({
