3dbcca87e9cd18125a5ca80b6c60526ccc0e8028
Giolvani <inavloig@gmail.com>
2023-06-11 23:55:00 -0300

    feat: configure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Cart, Confirmation, Payment, NotFound } from './pages'
+import { Layout } from './pages/layout'
 
 export default function App() {
-  return <div className="wrapper">Boa sorte! 🚀</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Cart />} />
+          <Route path="payment" element={<Payment />} />
+          <Route path="confirmation" element={<Confirmation />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
