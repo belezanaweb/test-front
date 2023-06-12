5f701a7cd743b2d77bfa2ff4fa088415280a113a
Giolvani <inavloig@gmail.com>
2023-06-12 01:03:47 -0300

    feat: :poop: load cart data from hook
    load data from hook and perform a initial display

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,15 +1,22 @@
-import { useContext, useEffect } from "react"
-import { Link } from "react-router-dom"
-import { ActionContext } from "../contexts/ActionContext"
+import { useContext, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { ActionContext } from '../contexts/ActionContext'
+import useLoadCart from '../hooks/useLoadCart'
 
 const Cart = () => {
-  const { setActionElement } = useContext(ActionContext);
+  const { setActionElement } = useContext(ActionContext)
+  const cart = useLoadCart()
 
   useEffect(() => {
     setActionElement(<Link to="payment">Seguir para o pagamento</Link>)
   }, [])
 
-  return <div>cart</div>
+  return (
+    <div>
+      {!cart && <div>carregando...</div>}
+      {cart && cart.items.map((item) => <div>{item.product.name}</div>)}
+    </div>
+  )
 }
 
 export { Cart }
