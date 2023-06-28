5999e2afe132c18e07f84bc1c76d7c7117875457
Douglas Gomes <douglasgomes.rr@gmail.com>
2023-06-27 22:03:37 -0300

    feat: create formatCurrency helper

---
+++ apps/cart/src/helpers/formatCurrency.ts
@@ -0,0 +1,8 @@
+export function formatCurrency(value: number) {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(value);
+}
