795c2e13d56bf835ea69fec02190c4e1ece644a3
francisco castro <francisco.ruf@gmail.com>
2023-07-02 15:46:11 -0300

    feat: create payment screen

---
+++ src/pages/Cart/screens/Payment/index.tsx
@@ -0,0 +1,32 @@
+import React, { useCallback } from 'react'
+import PaymentForm, { FormProps } from '../../../../components/PaymentForm'
+import PaymentResume from '../../../../components/PaymentResume'
+import useFormStore from '../../../../store/useFormStore'
+import useNavigateStore from '../../../../store/useNavigateStore'
+import useBagStore from '../../../../store/useBagStore'
+
+const Payment = () => {
+  const addForm = useFormStore((state) => state.addForm)
+  const bag = useBagStore((state) => state.bag)
+  const navigate = useNavigateStore((state) => state.navigate)
+
+  const handleSubmitForm = useCallback((data: FormProps) => {
+    addForm(data)
+    navigate('Confirmação')
+  }, [])
+
+  return (
+    <PaymentForm onSubmit={handleSubmitForm}>
+      <PaymentResume
+        buttonLable="Finalizar pedido"
+        discount={bag.discount}
+        itemsQuantity={bag.items.length}
+        shippingTotal={bag.shippingTotal}
+        subTotal={bag.subTotal}
+        total={bag.total}
+      />
+    </PaymentForm>
+  )
+}
+
+export default Payment
