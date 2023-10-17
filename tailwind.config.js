2870b6f90a0110ebdf594da547afc5e1a763a5cc
Adriel Gama <adrielgama@gmail.com>
2023-10-17 09:22:12 -0300

    feat: config eslint, tailwind, typescript lint

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purple: '#9222DC',
+        'secondary-dark-purple': '#560789',
+        'secondary-yellow': '#F2ED3B',
+        'secondary-orange': '#FF570D',
+        'secondary-ocean-blue': '#66D1E3',
+        'background-beauty-week': '#191919',
+        black01: '#1D1D1F',
+        black02: '#313033',
+        black03: '#3A393D',
+        black04: '#4E4D51',
+        black05: '#8C8996',
+        'PRO-dark-purple': '#2F1642',
+        'PRO-light-grey': '#D4CFD4',
+      },
+      backgroundColor: {
+        purple: '#9222DC',
+        'secondary-dark-purple': '#560789',
+        'secondary-yellow': '#F2ED3B',
+        'secondary-orange': '#FF570D',
+        'secondary-ocean-blue': '#66D1E3',
+        'background-beauty-week': '#191919',
+        black01: '#1D1D1F',
+        black02: '#313033',
+        black03: '#3A393D',
+        black04: '#4E4D51',
+        black05: '#8C8996',
+        'light-grey': '#F5F5F5',
+        'PRO-dark-purple': '#2F1642',
+        'PRO-light-grey': '#D4CFD4',
+      },
+    },
+  },
+  plugins: [],
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
     environment: 'happy-dom',
-    setupFiles: ['./src/test/setup-test.ts']
+    setupFiles: ['./src/test/setup-test.ts'],
   },
   server: {
-    port: 3000
-  }
+    port: 3000,
+  },
 })
